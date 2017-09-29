9d842e5990117aa2f28ad9fc2f8b1e3e1f334448
Leandro <leandro.tramma@gmail.com>
2017-09-29 12:40:25 +0100

    RDMPDEV-689 Nice prototype working, but we need a better way to set credentials for the real thing.

---
+++ DataLoad/Applications/RDMPAutomationService/WebdavAutodowloader/Options.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace WebdavAutodowloader
+{
+    public class Options
+    {
+        [Option('f', "folder", Required = true, HelpText = "Remote folder to watch for new files")]
+        public string RemoteFolder { get; set; }
+
+        [Option('p', "password", Required = false, DefaultValue = "pippolo", HelpText = "Password used for zip files")]
+        public string ZipPassword { get; set; }
+    }
+}
---
+++ DataLoad/Applications/RDMPAutomationService/WebdavAutodowloader/WebdavReleaseEngineSettings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using CommandLine;
+using WebdavAutodowloader.Properties;
+
+namespace WebdavAutodowloader
+{
+    public class WebdavReleaseEngineSettings
+    {
+        [Option('f', "folder", Required = false, DefaultValue = "Leandro", HelpText = "Remote folder to watch for new files")]
+        public string RemoteFolder { get; set; }
+
+        [Option('p', "password", Required = false, DefaultValue = "pippolo", HelpText = "Password used for zip files")]
+        public string ZipPassword { get; set; }
+
+        [Option('d', "destination", Required = false, DefaultValue = "C:\\TEMP", HelpText = "Local folder to decompress files in")]
+        public string LocalDestination { get; set; }
+
+        public string Endpoint { get; set; }
+
+        public string BasePath { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public WebdavReleaseEngineSettings()
+        {
+            //RemoteFolder = remoteFolder;
+            //ZipPassword = zipPassword;
+            Endpoint = Settings.Default.Endpoint;
+            BasePath = Settings.Default.BasePath;
+            Username = Settings.Default.Username;
+            Password = Encoding.ASCII.GetString(Convert.FromBase64String(Settings.Default.Password));
+        }
+    }
+}
