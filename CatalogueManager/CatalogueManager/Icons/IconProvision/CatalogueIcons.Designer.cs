40604cd7c332dbe839e8cd0fcb7b14b51245b187
tznind <t.z.nind@dundee.ac.uk>
2017-11-08 15:18:35 +0000

    Added Automation Slots to the TableInfoCollectionUI.cs which puts them into tree system

--- CatalogueManager/CatalogueManager/Icons/IconProvision/CatalogueIcons.Designer.cs
+++ CatalogueManager/CatalogueManager/Icons/IconProvision/CatalogueIcons.Designer.cs
@@ -93,6 +93,16 @@ namespace CatalogueManager.Icons.IconProvision {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap AllAutomationServerSlotsNode {
+            get {
+                object obj = ResourceManager.GetObject("AllAutomationServerSlotsNode", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap AllCataloguesUsedByLoadMetadataNode {
             get {
                 object obj = ResourceManager.GetObject("AllCataloguesUsedByLoadMetadataNode", resourceCulture);
