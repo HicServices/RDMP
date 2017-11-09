da2ed41ebda38945707c387ba7ac609f944b962f
tznind <t.z.nind@dundee.ac.uk>
2017-11-09 09:24:08 +0000

    RDMPDEV-826 Support for PipelineUser DatabaseEntities that come from other repositories (i.e. not CatalogueRepository or DataExportRepository)

--- CatalogueManager/CatalogueLibrary/Data/Pipelines/PipelineUser.cs
+++ CatalogueManager/CatalogueLibrary/Data/Pipelines/PipelineUser.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Reflection;
+using System.Web.Security.AntiXss;
 using CatalogueLibrary.Data.Cache;
 using CatalogueLibrary.Repositories;
+using ReusableLibraryCode.Reflection;
 
 namespace CatalogueLibrary.Data.Pipelines
 {
@@ -14,22 +16,37 @@ namespace CatalogueLibrary.Data.Pipelines
         public PipelineGetter Getter { get; private set; }
         public PipelineSetter Setter { get; private set; }
 
-        public PipelineUser(PropertyInfo property, DatabaseEntity user)
+        public PipelineUser(PropertyInfo property, DatabaseEntity user,ICatalogueRepository repository = null)
         {
             _property = property;
             User = user;
 
-            _catalogueRepository = User.Repository as ICatalogueRepository;
-            var dataExportRepo = User.Repository as IDataExportRepository;
+            //if user passed in an explicit one
+            _catalogueRepository = repository;
 
-            if (dataExportRepo != null)
-                _catalogueRepository = dataExportRepo.CatalogueRepository;
-
+            //otherwise get it from the user
             if (_catalogueRepository == null)
-                throw new Exception("Repository of Host '" + User + "' was not an ICatalogueRepository or a IDataExportRepository");
-            
+            {
+                if (User.Repository == null)
+                    throw new Exception("User does not have a Repository! how can it be a DatabaseEntity!");
+
+                _catalogueRepository = User.Repository as ICatalogueRepository;
+                var dataExportRepo = User.Repository as IDataExportRepository;
+
+                if (dataExportRepo != null)
+                    _catalogueRepository = dataExportRepo.CatalogueRepository;
+
+                if (_catalogueRepository == null)
+                    throw new Exception("Repository of Host '" + User + "' was not an ICatalogueRepository or a IDataExportRepository.  user came from a Repository called '" + user.Repository.GetType().Name + "' in this case you will need to specify the ICatalogueRepository property to this method so we know where to fetch Pipelines from");
+                
+            }
             Getter = Get;
             Setter = Set;
+            
+        }
+        public PipelineUser(string nameOfProperty, DatabaseEntity user, ICatalogueRepository repository = null): this(user.GetType().GetProperty(nameOfProperty), user, repository)
+        {
+
         }
 
         public PipelineUser(CacheProgress cacheProgress):this(typeof(CacheProgress).GetProperty("Pipeline_ID"),cacheProgress)
