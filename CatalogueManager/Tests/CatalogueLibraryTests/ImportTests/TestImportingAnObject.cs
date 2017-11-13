bdc18ac2b2976a36930c5ea8194859c89acd499d
tznind <t.z.nind@dundee.ac.uk>
2017-11-13 13:08:07 +0000

    RDMPDEV-804 New class ObjectImporter and test Removed Obsolete method InsertAndReturnID(string sql, Dictionary<string, object> parameters = null) Centralised SetParameterToValue(DbParameter implementation in TableRepository)

---
+++ CatalogueManager/Tests/CatalogueLibraryTests/ImportTests/TestImportingAnObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatalogueLibrary.Data;
+using MapsDirectlyToDatabaseTable;
+using MapsDirectlyToDatabaseTable.Importing;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Tests.Common;
+
+namespace CatalogueLibraryTests.ImportTests
+{
+    public class TestImportingAnObject:DatabaseTests
+    {
+        [Test]
+        public void ImportACatalogue()
+        {
+            ObjectImporter importer = new ObjectImporter(CatalogueRepository);
+
+            var c = new Catalogue(CatalogueRepository, "omg cata");
+            Assert.AreEqual(CatalogueRepository.GetAllCatalogues().Count(), 1);
+
+            var dict = TableRepository.GetPropertyInfos(typeof (Catalogue)).ToDictionary(p => p.Name, p2 => p2.GetValue(c));
+            var c2 = (Catalogue)importer.ImportObject(typeof(Catalogue),dict);
+
+            Assert.AreEqual(c.Name, c2.Name);
+            Assert.AreNotEqual(c.ID,c2.ID);
+
+            Assert.AreEqual(CatalogueRepository.GetAllCatalogues().Count(),2);
+
+        }
+    }
+}
---
+++ Reusable/MapsDirectlyToDatabaseTable/Importing/ObjectImporter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Remotion.Linq.Utilities;
+using ReusableLibraryCode;
+using ReusableLibraryCode.DatabaseHelpers.Discovery;
+
+namespace MapsDirectlyToDatabaseTable.Importing
+{
+    public class ObjectImporter
+    {
+        private readonly TableRepository _targetRepository;
+
+        public ObjectImporter(TableRepository targetRepository)
+        {
+            _targetRepository = targetRepository;
+        }
+
+        public IMapsDirectlyToDatabaseTable ImportObject(Type t, Dictionary<string, object> properties)
+        {
+            if(!typeof(IMapsDirectlyToDatabaseTable).IsAssignableFrom(t))
+                throw new ArgumentEmptyException("Type must be IMapsDirectlyToDatabaseTable");
+
+            using (var con = _targetRepository.GetConnection())
+            {
+                var cmd = DatabaseCommandHelper.GetCommand("SELECT * FROM " + t.Name, con.Connection);
+                var cmdbuilder = new DiscoveredServer(_targetRepository.ConnectionStringBuilder).Helper.GetCommandBuilder(cmd);
+
+                DbCommand cmdInsert = cmdbuilder.GetInsertCommand(true);
+                cmdInsert.CommandText += ";SELECT @@IDENTITY;";
+
+                _targetRepository.PrepareCommand(cmdInsert, properties);
+
+                var id = Convert.ToInt32(cmdInsert.ExecuteScalar());
+
+                return _targetRepository.GetObjectByID(t, id);
+            }
+        }
+    }
+}
