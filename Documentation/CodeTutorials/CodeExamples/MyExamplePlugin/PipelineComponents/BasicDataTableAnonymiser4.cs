4cadac57384a7340b7d4e61f54edbcb65075e54d
tznind <t.z.nind@dundee.ac.uk>
2017-12-05 15:27:04 +0000

    Some more comments New tutorial content covering the Checks system Updated Type names to specify namespace where they are mentioned the first time in the documentation

---
+++ Documentation/CodeTutorials/CodeExamples/MyExamplePlugin/PipelineComponents/BasicDataTableAnonymiser4.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CatalogueLibrary.Data;
+using CatalogueLibrary.DataFlowPipeline;
+using ReusableLibraryCode.Checks;
+using ReusableLibraryCode.DataAccess;
+using ReusableLibraryCode.Progress;
+
+namespace MyExamplePlugin.PipelineComponents
+{
+    public class BasicDataTableAnonymiser4 : IPluginDataFlowComponent<DataTable>
+    {
+        [DemandsInitialization("Table containing a single column which must have a list of names to redact from columns", mandatory: true)]
+        public TableInfo NamesTable { get; set; }
+
+        [DemandsInitialization("Columns matching this regex pattern will be skipped")]
+        public Regex ColumnsNotToEvaluate { get; set; }
+
+        private string[] _commonNames;
+
+        public DataTable ProcessPipelineData(DataTable toProcess, IDataLoadEventListener listener, GracefulCancellationToken cancellationToken)
+        {
+            GetCommonNamesTable(new ThrowImmediatelyCheckNotifier());
+
+            //Go through each row in the table
+            foreach (DataRow row in toProcess.Rows)
+            {
+                //for each cell in current row
+                foreach (DataColumn col in toProcess.Columns)
+                {
+                    //if it's not a column we are skipping
+                    if (ColumnsNotToEvaluate != null && ColumnsNotToEvaluate.IsMatch(col.ColumnName))
+                        continue;
+
+                    //if it is a string
+                    var stringValue = row[col] as string;
+
+                    if (stringValue != null)
+                    {
+                        //replace any common names with REDACTED
+                        foreach (var name in _commonNames)
+                            stringValue = Regex.Replace(stringValue, name, "REDACTED", RegexOptions.IgnoreCase);
+
+                        row[col] = stringValue;
+                    }
+                }
+            }
+
+            return toProcess;
+        }
+
+        private void GetCommonNamesTable(ICheckNotifier notifier)
+        {
+            if (_commonNames == null)
+            {
+                if (NamesTable == null)
+                {
+                    notifier.OnCheckPerformed(
+                        new CheckEventArgs(
+                            "No NamesTable has been set, this must be a Table containing a list of names to REDACT from the pipeline data being processed",
+                            CheckResult.Fail));
+
+                    return;
+                }
+
+                //get access to the database under DataLoad context
+                var databaseDiscovered = DataAccessPortal.GetInstance().ExpectDatabase(NamesTable, DataAccessContext.DataLoad);
+
+                if (databaseDiscovered.Exists())
+                    notifier.OnCheckPerformed(new CheckEventArgs("Found Database '" + databaseDiscovered + "' ",CheckResult.Success));
+                else
+                    notifier.OnCheckPerformed(new CheckEventArgs("Database '" + databaseDiscovered + "' does not exist ", CheckResult.Fail));
+
+                //expect a table matching the TableInfo
+                var tableDiscovered = databaseDiscovered.ExpectTable(NamesTable.GetRuntimeName());
+
+                if (tableDiscovered.Exists())
+                    notifier.OnCheckPerformed(new CheckEventArgs("Found table '" + tableDiscovered + "' ", CheckResult.Success));
+                else
+                    notifier.OnCheckPerformed(new CheckEventArgs("Table '" + tableDiscovered + "' does not exist ", CheckResult.Fail));
+
+                //make sure it exists
+                if (!tableDiscovered.Exists())
+                    throw new NotSupportedException("TableInfo '" + tableDiscovered + "' does not exist!");
+
+                //Download all the data
+                var dataTable = tableDiscovered.GetDataTable();
+
+                //Make sure it has the correct expected schema (i.e. 1 column)
+                if (dataTable.Columns.Count != 1)
+                    throw new NotSupportedException("Expected a single column in DataTable '" + tableDiscovered + "'");
+
+                //turn it into an array
+                _commonNames = dataTable.AsEnumerable().Select(r => r.Field<string>(0)).ToArray();
+
+                if (_commonNames.Length == 0)
+                {
+                    notifier.OnCheckPerformed(new CheckEventArgs("Table '" + tableDiscovered + "' did not have any rows in it!", CheckResult.Fail));
+                    
+                    //reset it just in case
+                    _commonNames = null;
+                }
+                else
+                    notifier.OnCheckPerformed(new CheckEventArgs("Read " + _commonNames.Length + " names from name table", CheckResult.Success));
+            }
+        }
+
+        public void Dispose(IDataLoadEventListener listener, Exception pipelineFailureExceptionIfAny)
+        {
+
+        }
+
+        public void Abort(IDataLoadEventListener listener)
+        {
+
+        }
+
+        public void Check(ICheckNotifier notifier)
+        {
+            notifier.OnCheckPerformed(new CheckEventArgs("Ready to start checking", CheckResult.Success, null, null));
+            GetCommonNamesTable(notifier);
+        }
+    }
+}
--- Reusable/ReusableLibraryCode/CommandExecution/ICommandExecution.cs
+++ Reusable/ReusableLibraryCode/CommandExecution/ICommandExecution.cs
@@ -1,5 +1,9 @@
 ﻿namespace ReusableLibraryCode.CommandExecution
 {
+    /// <summary>
+    /// Describes an executable command that is available to the user (or assembled and executed in code).  ICommandExecution instances are allowed to be in
+    /// illegal states (IsImpossible = true) and this should be respected by Execute i.e. it will throw an exception if Executed.
+    /// </summary>
     public interface ICommandExecution
     {
         bool IsImpossible { get; }
