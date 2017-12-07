487110f1e9513779b71b8eeef213ee7fa952ff89
tznind <t.z.nind@dundee.ac.uk>
2017-12-07 10:53:07 +0000

    RDMPDEV-884 Comments

--- CatalogueManager/CatalogueLibrary/Data/DataLoad/IResolveDuplication.cs
+++ CatalogueManager/CatalogueLibrary/Data/DataLoad/IResolveDuplication.cs
@@ -3,6 +3,10 @@ using ReusableLibraryCode;
 
 namespace CatalogueLibrary.Data.DataLoad
 {
+    /// <summary>
+    /// Common interface for columns which can be used in RAW to resolve primary key duplication (See PrimaryKeyCollisionResolver).  This includes both PreLoadDiscardedColumns
+    /// and ColumnInfos.
+    /// </summary>
     public interface IResolveDuplication : IHasRuntimeName, ISaveable, IHasStageSpecificRuntimeName
     {
         int? DuplicateRecordResolutionOrder { get; set; }
--- CatalogueManager/CatalogueLibrary/Data/DataLoad/ITableInfoCollectionHost.cs
+++ CatalogueManager/CatalogueLibrary/Data/DataLoad/ITableInfoCollectionHost.cs
@@ -2,6 +2,11 @@ using System.Collections.Generic;
 
 namespace CatalogueLibrary.Data.DataLoad
 {
+    /// <summary>
+    /// Interface for defining that a given class is dependent or operates on one or more TableInfos.  This is used when you declare a [DemandsInitialization] property on
+    /// a plugin component of type ITableInfo to determine which instances to offer at design time (i.e. only show TableInfos that are associated with the load you are
+    /// editing).
+    /// </summary>
     public interface ITableInfoCollectionHost
     {
         IEnumerable<TableInfo> GetTableInfos();
