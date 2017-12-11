1ac7de8b43c5ba9615dbef7c792ff781d9a3c6d1
tznind <t.z.nind@dundee.ac.uk>
2017-12-11 09:23:43 +0000

    Comments

--- CatalogueManager/CatalogueLibrary/ExternalDatabaseServerPatching/IPatcher.cs
+++ CatalogueManager/CatalogueLibrary/ExternalDatabaseServerPatching/IPatcher.cs
@@ -3,6 +3,15 @@ using CatalogueLibrary.Data;
 
 namespace CatalogueLibrary.ExternalDatabaseServerPatching
 {
+    /// <summary>
+    /// Identifies databases belong to a specific .Database assembly that might need patching at Startup.  Document the host and database assembly classes (e.g. CatalogueLibrary
+    ///  and CatalogueLibrary.Database).
+    /// Identifies databases which belong to a specific .Database assembly that might need patching at Startup.  Document the host and database assembly classes 
+    /// (e.g. CatalogueLibrary and CatalogueLibrary.Database).  These databases will be checked at startup for whether they need patching and if so new scripts
+    /// in the up folder will be applied and the databse version will be incremented to match the Assembly versions.
+    /// 
+    /// If you are writing a plugin you should use IPluginPatcher instead which is MEF discoverable
+    /// </summary>
     public interface IPatcher
     {
         IExternalDatabaseServer[] FindDatabases(out Assembly hostAssembly, out Assembly dbAssembly);
