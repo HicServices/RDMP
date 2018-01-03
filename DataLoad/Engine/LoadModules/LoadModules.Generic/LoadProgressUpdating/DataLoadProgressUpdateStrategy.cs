d81dd82a40b4dc5dc339774e2347a9e018f2b3a0
tznind <t.z.nind@dundee.ac.uk>
2018-01-03 14:55:35 +0000

    Comments

--- DataLoad/Engine/LoadModules/LoadModules.Generic/LoadProgressUpdating/DataLoadProgressUpdateStrategy.cs
+++ DataLoad/Engine/LoadModules/LoadModules.Generic/LoadProgressUpdating/DataLoadProgressUpdateStrategy.cs
@@ -1,5 +1,9 @@
 ﻿namespace LoadModules.Generic.LoadProgressUpdating
 {
+    /// <summary>
+    /// Approaches user/system can use to determine what Date to update a LoadProgress to after a succesful data load (See UpdateProgressIfLoadsuccessful for 
+    /// description of why this is non trivial).
+    /// </summary>
     public enum DataLoadProgressUpdateStrategy
     {
         UseMaxRequestedDay,
