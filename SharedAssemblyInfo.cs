3085686afa8017e90427f51efebcac2a4382a4e3
Leandro <leandro.tramma@gmail.com>
2018-01-15 17:27:45 +0000

    Tweaking the NuGet package generation

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // These should be replaced with correct values by the release process
-[assembly: AssemblyVersion("2.5.4.5")]
-[assembly: AssemblyFileVersion("2.5.4.5")]
-[assembly: AssemblyInformationalVersion("2.5.4.5")]
+[assembly: AssemblyVersion("2.5.4.6")]
+[assembly: AssemblyFileVersion("2.5.4.6")]
+[assembly: AssemblyInformationalVersion("2.5.4.6")]
