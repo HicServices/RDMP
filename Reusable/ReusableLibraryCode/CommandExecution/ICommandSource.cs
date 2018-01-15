031e2eceeb14de6ecba60332ba467fa8367e694a
tznind <t.z.nind@dundee.ac.uk>
2018-01-15 09:10:45 +0000

    RDMPDEV-791 Moved ICommandSource into Library (out of UI) framework so that drag/copy events can be supported for plugins (currently plugins can only respond to drop events - via ICommandExecutionProposal)

---
+++ Reusable/ReusableLibraryCode/CommandExecution/ICommandSource.cs
@@ -0,0 +1,10 @@
+﻿namespace ReusableLibraryCode.CommandExecution
+{
+    /// <summary>
+    /// Object which can be converted into an ICommand e.g. by starting a dragg operation on it.
+    /// </summary>
+    public interface ICommandSource
+    {
+        ICommand GetCommand();
+    }
+}
