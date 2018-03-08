dd943d93f2e041bbb73b338f4d4a64157568b45c
Leandro <leandro.tramma@gmail.com>
2018-03-08 09:39:10 +0000

    bump version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // These should be replaced with correct values by the release process
-[assembly: AssemblyVersion("2.6.5.2")]
-[assembly: AssemblyFileVersion("2.6.5.2")]
-[assembly: AssemblyInformationalVersion("2.6.5.2")]
+[assembly: AssemblyVersion("2.6.5.3")]
+[assembly: AssemblyFileVersion("2.6.5.3")]
+[assembly: AssemblyInformationalVersion("2.6.5.3")]
