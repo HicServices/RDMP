ddf835e6f415fd7046e440f1a0a17e99b82d6ee7
tznind <t.z.nind@dundee.ac.uk>
2018-03-12 11:26:08 +0000

    RDMPDEV-937 Basic classes for discovering objects related to other objects (groundwork for renaming ColumnInfos)

---
+++ CatalogueManager/CatalogueLibrary/Refactoring/Gathering/GatheredObject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatalogueLibrary.Data;
+using MapsDirectlyToDatabaseTable;
+
+namespace CatalogueLibrary.Refactoring.Gathering
+{
+    public class GatheredObject
+    {
+        public GatheredObject(IMapsDirectlyToDatabaseTable o)
+        {
+            Object = o;
+        }
+
+        public IMapsDirectlyToDatabaseTable Object { get; set; }
+
+        /// <summary>
+        /// The described Object is only tenously related to the original object and you shouldn't worry too much if during refactoring you don't find any references. 
+        /// An example of this would be all Filters in a Catalogue where a single ColumnInfo is being renamed.  Any filter in the catalogue could contain a reference to
+        /// the ColumnInfo but most won't.
+        /// </summary>
+        public bool TenousRelationship { get; set; }
+
+
+        #region Equality
+        protected bool Equals(GatheredObject other)
+        {
+            return Equals(Object, other.Object);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((GatheredObject) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Object != null ? Object.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(GatheredObject left, GatheredObject right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(GatheredObject left, GatheredObject right)
+        {
+            return !Equals(left, right);
+        }
+        #endregion
+    }
+}
---
+++ CatalogueManager/CatalogueLibrary/Refactoring/Gathering/Gatherer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CatalogueLibrary.Data;
+using CatalogueLibrary.Repositories;
+using ReusableLibraryCode;
+
+namespace CatalogueLibrary.Refactoring.Gathering
+{
+    /// <summary>
+    /// Gathers dependencies of a given object in a more advanced/selective way than simply using methods of IHasDependencies
+    /// </summary>
+    public class Gatherer
+    {
+        private readonly IRDMPPlatformRepositoryServiceLocator _repositoryLocator;
+
+        public Gatherer(IRDMPPlatformRepositoryServiceLocator repositoryLocator)
+        {
+            _repositoryLocator = repositoryLocator;
+        }
+
+        public GatheredObject[] GatherDependencies(ColumnInfo c)
+        {
+            HashSet<GatheredObject> foundSoFar = new HashSet<GatheredObject>();
+
+            HashSet<Catalogue> catalogues = new HashSet<Catalogue>();
+
+            //the table it belongs to
+            foundSoFar.Add(new GatheredObject(c.TableInfo));
+
+            //the CatalogueItems (descriptive elements) that relate to it (including transforms)
+            foreach (CatalogueItem ci in _repositoryLocator.CatalogueRepository.GetAllObjects<CatalogueItem>("WHERE ColumnInfo_ID = " + c.ID))
+            {
+                foundSoFar.Add(new GatheredObject(ci));
+
+                //record the unique Catalogues for later evaluation
+                if (catalogues.All(cata => cata.ID != ci.Catalogue_ID))
+                    catalogues.Add(ci.Catalogue);
+
+                //The extraction SQL / Category / Transformation 
+                var ei = ci.ExtractionInformation;
+                if (ei != null)
+                {
+                    foundSoFar.Add(new GatheredObject(ei));
+
+                    //The filters belonging to extraction informations
+                    foreach (ExtractionFilter extractionFilter in ei.ExtractionFilters)
+                        foundSoFar.Add(new GatheredObject(extractionFilter));
+                }
+            }
+
+            //ExtractableDataset columns
+            
+            //deployed extraction filters
+
+            foreach (Catalogue catalogue in catalogues)
+            {
+                //Aggregates
+
+                //Aggregate dimensions
+
+                //Aggregate filters
+
+                //
+            }
+
+            return foundSoFar.ToArray();
+        }
+
+    }
+}
