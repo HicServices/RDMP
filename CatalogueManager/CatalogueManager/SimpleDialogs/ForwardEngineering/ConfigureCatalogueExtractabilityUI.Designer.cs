5dffb099c81b294583c41667ae9b3ce22f333d84
rdteviotdale <rdteviotdale@dundee.ac.uk>
2018-04-06 12:11:27 +0100

    RDMPDEV-985 Added functionality to select which ExtractionInformation(s) will be ExtractionIdentifier

--- CatalogueManager/CatalogueManager/SimpleDialogs/ForwardEngineering/ConfigureCatalogueExtractabilityUI.Designer.cs
+++ CatalogueManager/CatalogueManager/SimpleDialogs/ForwardEngineering/ConfigureCatalogueExtractabilityUI.Designer.cs
@@ -35,6 +35,9 @@
             this.olvExtractable = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
             this.olvIsExtractionIdentifier = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAddToExisting = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.ddCategoriseMany = new System.Windows.Forms.ComboBox();
             this.btnClearFilterMany = new System.Windows.Forms.Button();
@@ -42,9 +45,6 @@
             this.lbPastedColumns = new System.Windows.Forms.ListBox();
             this.label6 = new System.Windows.Forms.Label();
             this.tbFilter = new System.Windows.Forms.TextBox();
-            this.btnOk = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.btnAddToExisting = new System.Windows.Forms.Button();
             this.gbMarkAllExtractable.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.olvColumnExtractability)).BeginInit();
             this.panel2.SuspendLayout();
@@ -57,7 +57,7 @@
             this.gbMarkAllExtractable.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gbMarkAllExtractable.Location = new System.Drawing.Point(0, 0);
             this.gbMarkAllExtractable.Name = "gbMarkAllExtractable";
-            this.gbMarkAllExtractable.Size = new System.Drawing.Size(692, 717);
+            this.gbMarkAllExtractable.Size = new System.Drawing.Size(722, 717);
             this.gbMarkAllExtractable.TabIndex = 7;
             this.gbMarkAllExtractable.TabStop = false;
             this.gbMarkAllExtractable.Text = "Extractability";
@@ -82,7 +82,7 @@
             this.olvColumnExtractability.Name = "olvColumnExtractability";
             this.olvColumnExtractability.RowHeight = 19;
             this.olvColumnExtractability.ShowGroups = false;
-            this.olvColumnExtractability.Size = new System.Drawing.Size(686, 485);
+            this.olvColumnExtractability.Size = new System.Drawing.Size(716, 485);
             this.olvColumnExtractability.TabIndex = 7;
             this.olvColumnExtractability.UseCompatibleStateImageBehavior = false;
             this.olvColumnExtractability.UseFiltering = true;
@@ -125,8 +125,38 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel2.Location = new System.Drawing.Point(3, 507);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(686, 207);
+            this.panel2.Size = new System.Drawing.Size(716, 207);
             this.panel2.TabIndex = 5;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(214, 175);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(207, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel (Do not create a Catalogue)";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnAddToExisting
+            // 
+            this.btnAddToExisting.Location = new System.Drawing.Point(427, 175);
+            this.btnAddToExisting.Name = "btnAddToExisting";
+            this.btnAddToExisting.Size = new System.Drawing.Size(161, 23);
+            this.btnAddToExisting.TabIndex = 7;
+            this.btnAddToExisting.Text = "Add to existing Catalogue";
+            this.btnAddToExisting.UseVisualStyleBackColor = true;
+            this.btnAddToExisting.Click += new System.EventHandler(this.btnAddToExisting_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(89, 175);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(119, 23);
+            this.btnOk.TabIndex = 7;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
             // label3
             // 
@@ -175,7 +205,7 @@
             this.lbPastedColumns.FormattingEnabled = true;
             this.lbPastedColumns.Location = new System.Drawing.Point(118, 29);
             this.lbPastedColumns.Name = "lbPastedColumns";
-            this.lbPastedColumns.Size = new System.Drawing.Size(227, 134);
+            this.lbPastedColumns.Size = new System.Drawing.Size(257, 134);
             this.lbPastedColumns.TabIndex = 2;
             this.lbPastedColumns.KeyUp += new System.Windows.Forms.KeyEventHandler(this.lbPastedColumns_KeyUp);
             // 
@@ -196,41 +226,11 @@
             this.tbFilter.TabIndex = 0;
             this.tbFilter.TextChanged += new System.EventHandler(this.tbFilter_TextChanged);
             // 
-            // btnOk
-            // 
-            this.btnOk.Location = new System.Drawing.Point(89, 175);
-            this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(119, 23);
-            this.btnOk.TabIndex = 7;
-            this.btnOk.Text = "Ok";
-            this.btnOk.UseVisualStyleBackColor = true;
-            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.Location = new System.Drawing.Point(214, 175);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(207, 23);
-            this.btnCancel.TabIndex = 7;
-            this.btnCancel.Text = "Cancel (Do not create a Catalogue)";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
-            // 
-            // btnAddToExisting
-            // 
-            this.btnAddToExisting.Location = new System.Drawing.Point(427, 175);
-            this.btnAddToExisting.Name = "btnAddToExisting";
-            this.btnAddToExisting.Size = new System.Drawing.Size(161, 23);
-            this.btnAddToExisting.TabIndex = 7;
-            this.btnAddToExisting.Text = "Add to existing Catalogue";
-            this.btnAddToExisting.UseVisualStyleBackColor = true;
-            this.btnAddToExisting.Click += new System.EventHandler(this.btnAddToExisting_Click);
-            // 
             // ConfigureCatalogueExtractabilityUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(692, 717);
+            this.ClientSize = new System.Drawing.Size(722, 717);
             this.Controls.Add(this.gbMarkAllExtractable);
             this.Name = "ConfigureCatalogueExtractabilityUI";
             this.Text = "ConfigureCatalogueExtractabilityUI";
