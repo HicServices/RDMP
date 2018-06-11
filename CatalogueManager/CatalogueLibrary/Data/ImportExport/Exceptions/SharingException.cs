23aa4943cded2ce51c0396898bef460489702587
tznind <t.z.nind@dundee.ac.uk>
2018-06-11 09:21:31 +0100

    Unit test fixes

--- CatalogueManager/CatalogueLibrary/Data/ImportExport/Exceptions/SharingException.cs
+++ CatalogueManager/CatalogueLibrary/Data/ImportExport/Exceptions/SharingException.cs
@@ -2,6 +2,9 @@
 
 namespace CatalogueLibrary.Data.ImportExport.Exceptions
 {
+    /// <summary>
+    /// Thrown when there are problems importing or exporting objects from RDMP (either in gathering dependencies or loading <see cref="CatalogueLibrary.Data.Serialization.ShareDefinition"/>
+    /// </summary>
     public class SharingException:Exception
     {
         public SharingException(string msg) : base(msg)
