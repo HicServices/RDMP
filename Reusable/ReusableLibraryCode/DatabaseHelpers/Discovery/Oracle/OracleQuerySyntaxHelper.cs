15710fd345473a448c11f1eb52f6262ae6681596
tznind <t.z.nind@dundee.ac.uk>
2018-06-12 15:38:21 +0100

    Fixed oracle column escape character to double quotes, fixed name of import table dialog

--- Reusable/ReusableLibraryCode/DatabaseHelpers/Discovery/Oracle/OracleQuerySyntaxHelper.cs
+++ Reusable/ReusableLibraryCode/DatabaseHelpers/Discovery/Oracle/OracleQuerySyntaxHelper.cs
@@ -15,7 +15,7 @@ namespace ReusableLibraryCode.DatabaseHelpers.Discovery.Oracle
         public override string GetRuntimeName(string s)
         {
             //upper it because oracle is stupid
-            string toReturn =  s.Substring(s.LastIndexOf(".") + 1).Trim('`').ToUpper();
+            string toReturn =  s.Substring(s.LastIndexOf(".") + 1).Trim('"').ToUpper();
 
             //truncate it to 30 maximum because oracle cant count higher than 30
             return toReturn.Length > 30 ? toReturn.Substring(0, 30) : toReturn;
@@ -24,7 +24,7 @@ namespace ReusableLibraryCode.DatabaseHelpers.Discovery.Oracle
 
         public override string EnsureWrappedImpl(string databaseOrTableName)
         {
-            return "`" + GetRuntimeName(databaseOrTableName) + "`";
+            return '"' + GetRuntimeName(databaseOrTableName) + '"';
         }
 
         public override TopXResponse HowDoWeAchieveTopX(int x)
