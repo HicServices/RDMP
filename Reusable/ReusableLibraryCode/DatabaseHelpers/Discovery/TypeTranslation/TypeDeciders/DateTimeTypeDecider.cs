28da84d10b9e9f3d24740bad6de7e0495987e277
tznind <t.z.nind@dundee.ac.uk>
2018-06-14 15:08:25 +0100

    Made DateTimeTypeDecider throw out anything that can be parsed as a TimeSpan or Decimal (e.g. 1.1)

--- Reusable/ReusableLibraryCode/DatabaseHelpers/Discovery/TypeTranslation/TypeDeciders/DateTimeTypeDecider.cs
+++ Reusable/ReusableLibraryCode/DatabaseHelpers/Discovery/TypeTranslation/TypeDeciders/DateTimeTypeDecider.cs
@@ -5,12 +5,24 @@ namespace ReusableLibraryCode.DatabaseHelpers.Discovery.TypeTranslation.TypeDeci
 {
     internal class DateTimeTypeDecider : DecideTypesForStrings
     {
+        private TimeSpanTypeDecider _timeSpanTypeDecider = new TimeSpanTypeDecider();
+        private DecimalTypeDecider _decimalChecker = new DecimalTypeDecider();
+
         public DateTimeTypeDecider(): base(TypeCompatibilityGroup.Exclusive, typeof(DateTime))
         {
         }
 
         protected override TypeDeciderResult IsAcceptableAsTypeImpl(string candidateString)
         {
+            //if it's a float then it isn't a date is it! thanks C# for thinking 1.1 is the first of January
+            if (_decimalChecker.IsAcceptableAsType(candidateString).IsCompatible)
+                return new TypeDeciderResult(false);
+
+            //likewise if it is just the Time portion of the date then we have a column with mixed dates and times which SQL will not deal with well in the end database (e.g. it will set the
+            //date portion of times to todays date which will be very confusing
+            if (_timeSpanTypeDecider.IsAcceptableAsType(candidateString).IsCompatible)
+                return new TypeDeciderResult(false);
+
             try
             {
                 DateTime t;
