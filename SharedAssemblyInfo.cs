d89075fd2254ba0433e1be62c1d2fc2e6c224ade
tznind <t.z.nind@dundee.ac.uk>
2018-08-09 13:16:07 +0100

    Version bump

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // These should be replaced with correct values by the release process
-[assembly: AssemblyVersion("2.9.2.1")]
-[assembly: AssemblyFileVersion("2.9.2.1")]
-[assembly: AssemblyInformationalVersion("2.9.2.1")]
+[assembly: AssemblyVersion("2.9.2.2")]
+[assembly: AssemblyFileVersion("2.9.2.2")]
+[assembly: AssemblyInformationalVersion("2.9.2.2")]
 
 [assembly:InternalsVisibleTo("CatalogueManager")]
