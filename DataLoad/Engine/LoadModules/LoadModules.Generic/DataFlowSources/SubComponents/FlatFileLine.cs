7189fc4be8e4dab276483d89ea2dc2baeae2b069
tznind <t.z.nind@dundee.ac.uk>
2018-10-31 15:06:25 +0000

    Commented FlatFileLine

--- DataLoad/Engine/LoadModules/LoadModules.Generic/DataFlowSources/SubComponents/FlatFileLine.cs
+++ DataLoad/Engine/LoadModules/LoadModules.Generic/DataFlowSources/SubComponents/FlatFileLine.cs
@@ -2,10 +2,27 @@
 
 namespace LoadModules.Generic.DataFlowSources.SubComponents
 {
+
+    /// <summary>
+    /// Point in time record of a line read from CsvHelper including ReadingContext information such as <see cref="LineNumber"/>.  Can include multiple lines
+    /// of the underlying file if there is proper qualifying quotes and newlines in the csv e.g. when including free text columns.
+    /// </summary>
     public class FlatFileLine
     {
+        /// <summary>
+        /// The RAW file line number that this line reflects.  Where a record spans multiple lines (e.g. when it has newlines in quote qualified fields) it 
+        /// seems to be the last line number in the record
+        /// </summary>
         public int LineNumber { get; private set; }
+
+        /// <summary>
+        /// The values as interpreted by CsvHelper for the current line
+        /// </summary>
         public string[] Cells { get; set; }
+
+        /// <summary>
+        /// The absolute text as it appears in the flat file being read for this 'line'
+        /// </summary>
         public string RawRecord { get; set; }
 
         public FlatFileLine(ReadingContext context)
