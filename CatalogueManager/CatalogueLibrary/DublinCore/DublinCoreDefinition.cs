d7d0c7af867d792a987a0fc3dc684fa5b346db19
tznind <t.z.nind@dundee.ac.uk>
2018-11-15 11:06:19 +0000

    RDMPDEV-1030 Added class for exporting in DublinCore format

---
+++ CatalogueManager/CatalogueLibrary/DublinCore/DublinCoreDefinition.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace CatalogueLibrary.DublinCore
+{
+    class DublinCoreDefinition
+    {
+        public string Title { get; set; }
+        public string Alternative { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public string Publisher { get; set; }
+        public Uri IsPartOf { get; set; }
+        public Uri Identifier { get; set; }
+        public DateTime? Modified { get; set; }
+        public string Format { get; set; }
+
+        /// <summary>
+        /// Writes the defintion in the format listed in http://dublincore.org/documents/dc-xml-guidelines/
+        /// </summary>
+        /// <param name="to"></param>
+        public void WriteXml(Stream to)
+        {
+
+            XNamespace xsi = "http://www.w3.org/2001/XMLSchema-instance";
+            XNamespace dc = "http://purl.org/dc/elements/1.1/";
+            XNamespace dcterms = "http://purl.org/dc/terms/";
+
+
+            var xsiAttr = new XAttribute(XNamespace.Xmlns + "xsi", xsi);
+            var dcAttr = new XAttribute(XNamespace.Xmlns + "dc", dc);
+            var dctermsAttr = new XAttribute(XNamespace.Xmlns + "dcterms",dcterms);
+            
+            XDocument doc = new XDocument(new XElement("metadata",xsiAttr,dcAttr,dctermsAttr));
+            doc.Root.Add(new XElement(dc + "title", Title));
+            doc.Root.Add(new XElement(dcterms + "alternative", Alternative));
+            doc.Root.Add(new XElement(dc + "subject", Subject));
+            doc.Root.Add(new XElement(dc + "description", Description));
+            doc.Root.Add(new XElement(dc + "publisher", Publisher));
+
+            doc.Root.Add(new XElement(dcterms + "isPartOf", new XAttribute(xsi + "type", "dcters:URI"), IsPartOf));
+            doc.Root.Add(new XElement(dcterms + "identifier", new XAttribute(xsi + "type", "dcters:URI"), Identifier));
+
+            //<dcterms:modified xsi:type="dcterms:W3CDTF">
+            if (Modified.HasValue)
+                doc.Root.Add(new XElement(dcterms + "modified", new XAttribute(xsi + "type", "dcterms:W3CDTF"), Modified.Value.ToString("yyyy-MM-dd")));
+
+            doc.Root.Add(new XElement(dc + "format", new XAttribute(xsi + "type", "dcterms:IMT"), Format));
+            
+            using(StreamWriter sw = new StreamWriter(to))
+                sw.Write(doc.ToString(SaveOptions.None));
+        }
+    }
+}
---
+++ CatalogueManager/Tests/CatalogueLibraryTests/DublinCoreTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using CatalogueLibrary.DublinCore;
+using NUnit.Framework;
+
+namespace CatalogueLibraryTests
+{
+    class DublinCoreTests
+    {
+        [Test]
+        public void TestWrittingDocument()
+        {
+            var def = new DublinCoreDefinition()
+            {
+                Title =  "ssssshh",
+                Alternative =  "O'Rly",
+                Description = "Description of stuff",
+                Format = "text/html",
+                Identifier = new Uri("http://foo.com"),
+                Publisher = "University of Dundee",
+                IsPartOf = new Uri("http://foo2.com"),
+                Modified = new DateTime(2001,1,1),
+                Subject = "Interesting, PayAttention, HighPriority, Omg"
+            };
+
+            var f = new FileInfo(Path.Combine(TestContext.CurrentContext.WorkDirectory, "dublinTest.xml"));
+
+            using(var fw = f.OpenWrite())
+                def.WriteXml(fw);
+
+            var contents = File.ReadAllText(f.FullName);
+            StringAssert.Contains(def.Title, contents);
+            StringAssert.Contains(def.Alternative, contents);
+            StringAssert.Contains(def.Description, contents);
+            StringAssert.Contains(def.Format, contents);
+            StringAssert.Contains(def.Publisher, contents);
+            StringAssert.Contains(def.Subject, contents);
+
+            StringAssert.Contains("2001-01-01", contents);
+            StringAssert.Contains("http://foo.com", contents);
+            StringAssert.Contains("http://foo2.com", contents);
+        }
+    }
+}
