497fcbfbf7c916933d8272718b5cd730c5907c7d
Leandro <leandro.tramma@gmail.com>
2019-01-23 14:37:43 +0000

    Initial work. It looks ok but needs refining.

---
+++ Application/RDMPInstaller/MainInstallerForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace RDMPInstaller
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ddlVersions = new System.Windows.Forms.ComboBox();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Version:";
+            // 
+            // ddlVersions
+            // 
+            this.ddlVersions.FormattingEnabled = true;
+            this.ddlVersions.Location = new System.Drawing.Point(65, 10);
+            this.ddlVersions.Name = "ddlVersions";
+            this.ddlVersions.Size = new System.Drawing.Size(285, 21);
+            this.ddlVersions.TabIndex = 1;
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Location = new System.Drawing.Point(274, 113);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(75, 23);
+            this.btnInstall.TabIndex = 2;
+            this.btnInstall.Text = "GO";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 148);
+            this.Controls.Add(this.btnInstall);
+            this.Controls.Add(this.ddlVersions);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "RDMP Installer";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ddlVersions;
+        private System.Windows.Forms.Button btnInstall;
+    }
+}
+
---
+++ Application/RDMPInstaller/MainInstallerForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RDMPInstaller
+{
+    public partial class frmMain : Form
+    {
+        public string Url { get; set; }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Url = "https://hic.dundee.ac.uk/installers/rdmp";
+            var client = new WebClient();
+            var installers = client.DownloadString(Url);
+            var allLinks = Regex.Matches(installers, @"HREF=\""(/installers/rdmp/(\d+\.\d+\.\d+\.\d+(\-\w+)?)/)\""");
+
+            var allVersions = new List<RDMPVersion>();
+
+            foreach (Match link in allLinks)
+            {
+                allVersions.Add(new RDMPVersion
+                {
+                    Version = link.Groups[2].Value,
+                    Link = "/" + link.Groups[2].Value + "/"
+                });
+            }
+
+            ddlVersions.Items.Add(new RDMPVersion()
+            {
+                Link = "/Stable/",
+                Version = "Latest"
+            });
+
+            try
+            {
+                ddlVersions.Items.AddRange(allVersions.ToArray());
+                ddlVersions.ValueMember = "Link";
+                ddlVersions.DisplayMember = "Version";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            var selected = ddlVersions.SelectedItem as RDMPVersion;
+            if (selected != null)
+                Installer.InstallApplication(Url + selected.Link + "ResearchDataManagementPlatform.application");
+        }
+
+    }
+
+    internal class RDMPVersion : IComparable<RDMPVersion>
+    {
+        public string Version { get; set; }
+        public string Link { get; set; }
+
+        public bool IsReleaseCandidate { get { return Version.Contains("-"); } }
+
+        public int CompareTo(RDMPVersion other)
+        {
+            var thisversion = IsReleaseCandidate ? new Version(Version.Split('-')[0]) : new Version(Version);
+            var otherversion = other.IsReleaseCandidate ? new Version(other.Version.Split('-')[0]) : new Version(other.Version);
+
+            if (IsReleaseCandidate)
+                thisversion = new Version(thisversion.Major, thisversion.Minor, thisversion.Build, thisversion.Revision - 1);
+            if (other.IsReleaseCandidate)
+                otherversion = new Version(otherversion.Major, otherversion.Minor, otherversion.Build, otherversion.Revision - 1);
+
+            var result = thisversion.CompareWith(otherversion, 4);
+            if (result != 0)
+                return result;
+
+            if (this.IsReleaseCandidate)
+                return -1;
+
+            return 1;
+        }
+    }
+
+    public static class VersionExtensions
+    {
+        public static int CompareWith(this Version version, Version otherVersion, int significantParts)
+        {
+            if (version == null)
+            {
+                throw new ArgumentNullException("version");
+            }
+            if (otherVersion == null)
+            {
+                return 1;
+            }
+
+            if (version.Major != otherVersion.Major && significantParts >= 1)
+                if (version.Major > otherVersion.Major)
+                    return 1;
+                else
+                    return -1;
+
+            if (version.Minor != otherVersion.Minor && significantParts >= 2)
+                if (version.Minor > otherVersion.Minor)
+                    return 1;
+                else
+                    return -1;
+
+            if (version.Build != otherVersion.Build && significantParts >= 3)
+                if (version.Build > otherVersion.Build)
+                    return 1;
+                else
+                    return -1;
+
+            if (version.Revision != otherVersion.Revision && significantParts >= 4)
+                if (version.Revision > otherVersion.Revision)
+                    return 1;
+                else
+                    return -1;
+
+            return 0;
+        }
+    }
+}
