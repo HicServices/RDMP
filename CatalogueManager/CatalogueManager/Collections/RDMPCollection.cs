217bfe4fa4498578cfb2d95aac0549cd73b0e9f3
Leandro <leandro.tramma@gmail.com>
2019-02-06 12:23:24 +0000

    Copyright and license information added to all code files.

--- CatalogueManager/CatalogueManager/Collections/RDMPCollection.cs
+++ CatalogueManager/CatalogueManager/Collections/RDMPCollection.cs
@@ -1,4 +1,10 @@
-﻿namespace CatalogueManager.Collections
+// Copyright (c) The University of Dundee 2018-2019
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
+namespace CatalogueManager.Collections
 {
     /// <summary>
     /// Enum for specifying which RDMPCollectionUI a given operation relates to.  This allows you to request a collection be shown etc without having to pass typeof(X) 
--- Reusable/MapsDirectlyToDatabaseTable/ISaveable.cs
+++ Reusable/MapsDirectlyToDatabaseTable/ISaveable.cs
@@ -1,4 +1,10 @@
-﻿namespace MapsDirectlyToDatabaseTable
+// Copyright (c) The University of Dundee 2018-2019
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
+namespace MapsDirectlyToDatabaseTable
 {
     /// <summary>
     /// Object (usually a IMapsDirectlyToDatabaseTable) which can have it's state saved into a database.  The object must already exist in the database (See 
