f92ff8c56dd9f64b879059c5193641868ce0129b
tznind <t.z.nind@dundee.ac.uk>
2019-02-11 08:48:16 +0000

    It is no longer possible to create INamed objects with no Name (in database)

---
+++ DataExportManager/DataExportLibrary.Database/db/up/019_NamesMustNotBeNull.sql
@@ -0,0 +1,7 @@
+﻿--Version:2.12.0.1
+--Description: Database constraints Name properties are never null
+update ExternalCohortTable set Name = 'NoName' where Name is null
+alter table ExternalCohortTable alter column Name varchar(1000) not null
+
+update Project set Name = 'NoName' where Name is null
+alter table Project alter column Name varchar(1000) not null
