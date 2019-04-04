0dbb8365b0bcb080235f23e5e55eaa6908a088fb
tznind <t.z.nind@dundee.ac.uk>
2019-04-04 15:00:23 +0100

    RDMPDEV-1375 Removed call to GetSoftwareVersion in startup in favour of going directly to the database table.

--- Reusable/MapsDirectlyToDatabaseTable/Versioning/DatabaseVersionProvider.cs
+++ Reusable/MapsDirectlyToDatabaseTable/Versioning/DatabaseVersionProvider.cs
@@ -26,49 +26,15 @@ namespace MapsDirectlyToDatabaseTable.Versioning
             using (var con = DatabaseCommandHelper.GetConnection(builder))
             {
                 con.Open();
+                
+                var cmd = DatabaseCommandHelper.GetCommand(@"SELECT top 1 version from RoundhousE.Version order by version desc", con);
 
-                var cmd = DatabaseCommandHelper.GetCommand("Select [dbo].[GetSoftwareVersion]()", con);
-                Version version;
+                var o = cmd.ExecuteScalar();
 
-                try
-                {
-                    var o = cmd.ExecuteScalar();
+                if(o == DBNull.Value)
+                    return new Version(0,0,0,0);
 
-                    if(o == DBNull.Value)
-                        return new Version(0,0,0,0);
-
-                    version = new Version(o.ToString());
-                }
-                catch (DbException e)
-                {
-                    if (e.Message.Contains("GetSoftwareVersion") && e.Message.ToLower().Contains("cannot find"))
-                    {
-                        CreateSoftwareVersionFunction(builder);
-                        version = new Version(cmd.ExecuteScalar().ToString());
-                    }
-                    else
-                        throw;
-                }
-
-                return version;
-            }
-        }
-
-        private static void CreateSoftwareVersionFunction(DbConnectionStringBuilder builder)
-        {
-            const string sql = @"CREATE FUNCTION [dbo].[GetSoftwareVersion]()
-RETURNS nvarchar(50)
-AS
-BEGIN
-	-- Return the result of the function
-	RETURN (SELECT top 1 version from RoundhousE.Version order by version desc)
-END";
-
-            using (var con = DatabaseCommandHelper.GetConnection(builder))
-            {
-                con.Open();
-                var cmd = DatabaseCommandHelper.GetCommand(sql, con);
-                cmd.ExecuteNonQuery();
+                return new Version(o.ToString());
             }
         }
     }
