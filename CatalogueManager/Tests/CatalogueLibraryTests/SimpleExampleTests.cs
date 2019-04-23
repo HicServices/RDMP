09605397e02bd75f316d1a0ce4d5ea47509a436b
Thomas Nind <TZNind@dundee.ac.uk>
2019-04-23 08:57:54 +0100

    RDMPIMG-382 Changed Tests.Common to target netstandard2.0 and moved Tests into CatalogueLibraryTests

--- CatalogueManager/Tests/CatalogueLibraryTests/SimpleExampleTests.cs
+++ CatalogueManager/Tests/CatalogueLibraryTests/SimpleExampleTests.cs
@@ -4,16 +4,16 @@
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
-using System;
 using System.Data;
 using System.Linq;
 using CatalogueLibrary.Data;
+using CatalogueLibrary.Repositories;
 using FAnsi;
 using NUnit.Framework;
-using ReusableLibraryCode;
 using ReusableLibraryCode.DataAccess;
+using Tests.Common;
 
-namespace Tests.Common
+namespace CatalogueLibraryTests
 {
     public class SimpleExampleTests : DatabaseTests
     {
