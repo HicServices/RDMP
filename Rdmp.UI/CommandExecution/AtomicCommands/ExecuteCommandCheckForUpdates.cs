910c63b907f302145a26bc3ac0ab57b3ee5abe21
ltramma <l.tramma@dundee.ac.uk>
2019-05-17 17:18:54 +0100

    RDMPDEV-1412 Finally nailed the auto update mechanism. This should now get a decent interface!

--- Rdmp.UI/CommandExecution/AtomicCommands/ExecuteCommandCheckForUpdates.cs
+++ Rdmp.UI/CommandExecution/AtomicCommands/ExecuteCommandCheckForUpdates.cs
@@ -1,12 +1,21 @@
-﻿using Rdmp.UI.ItemActivation;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using NuGet;
+using Rdmp.UI.ItemActivation;
 using ReusableLibraryCode.CommandExecution.AtomicCommands;
+using ReusableUIComponents.Dialogs;
 using Squirrel;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Squirrel.UpdateManager;
 
 namespace Rdmp.UI.CommandExecution.AtomicCommands
 {
@@ -19,14 +28,68 @@ namespace Rdmp.UI.CommandExecution.AtomicCommands
         public override void Execute()
         {
             base.Execute();
+            var updateTask = new Task(() => RunUpdate());
             
-            using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/HicServices/RDMP"))
+            updateTask.Start();
+        }
+
+        private void RunUpdate()
+        {
+            try
             {
-                var entry = mgr.Result.CheckForUpdate(false, (int f) => Debug.Print("progress: " + f)).Result;
-                if (entry.IsBootstrapping)
+                using (var mgr = GetGitHubUpdateManager("https://github.com/HicServices/RDMP"))
                 {
-                    Console.WriteLine("Ehy I am bootstrapping... whoooo");
+                    var entry = mgr.CheckForUpdate().Result;
+                    if (entry.ReleasesToApply.Any() && MessageBox.Show("Got things for you: " + entry.ReleasesToApply.Count(), "UPDATE & RESTART!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        var download = Task.Run(async () => { await mgr.DownloadReleases(entry.ReleasesToApply); });
+                        download.Wait();
+                        var updatePath = mgr.ApplyReleases(entry).Result;
+                        if (MessageBox.Show("Got things for you: " + updatePath, "RESTART?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            var task = Task.Run(() => Process.Start(new ProcessStartInfo(Path.Combine(updatePath, "ResearchDataManagementPlatform"))));
+                            Application.Exit();
+                        }
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                ExceptionViewer.Show(ex);
+            }
+        }
+
+        private UpdateManager GetGitHubUpdateManager(string repoUrl, bool prerelease = true)
+        {
+            var repoUri = new Uri(repoUrl);
+            var userAgent = new ProductInfoHeaderValue("Squirrel", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+            if (repoUri.Segments.Length != 3)
+            {
+                throw new Exception("Repo URL must be to the root URL of the repo e.g. https://github.com/myuser/myrepo");
+            }
+
+            var releasesApiBuilder = new StringBuilder("repos")
+                .Append(repoUri.AbsolutePath)
+                .Append("/releases");
+
+            Uri baseAddress = new Uri("https://api.github.com/");
+
+            using (var client = new System.Net.Http.HttpClient() { BaseAddress = baseAddress })
+            {
+                client.DefaultRequestHeaders.UserAgent.Add(userAgent);
+                var response = client.GetAsync(releasesApiBuilder.ToString()).Result;
+                response.EnsureSuccessStatusCode();
+
+                var releases = JsonConvert.DeserializeObject<List<Release>>(response.Content.ReadAsStringAsync().Result);
+                var latestRelease = releases
+                    .Where(x => prerelease || !x.Prerelease)
+                    .OrderByDescending(x => x.PublishedAt)
+                    .First();
+
+                var latestReleaseUrl = latestRelease.HtmlUrl.Replace("/tag/", "/download/");
+
+                return new UpdateManager(latestReleaseUrl);
             }
         }
     }
