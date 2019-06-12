4c08eee053e1c2ee14d7d1578d95bbec8204251c
Thomas Nind <TZNind@dundee.ac.uk>
2019-06-12 10:02:57 +0100

    RDMPDEV-1440 Added pivot views for conditions and operations and integration test.

---
+++ Rdmp.Core.Tests/CommandLine/ExampleDatasetsCreationTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Rdmp.Core.CommandLine.DatabaseCreation;
+using Rdmp.Core.Curation.Data;
+using Rdmp.Core.Curation.Data.Aggregation;
+using ReusableLibraryCode.Checks;
+using Tests.Common;
+
+namespace Rdmp.Core.Tests.CommandLine
+{
+    class ExampleDatasetsCreationTests:DatabaseTests
+    {
+        /// <summary>
+        /// Tests the creation of example datasets during first installation of RDMP or when running "rdmp.exe install [...] -e" from the CLI
+        /// </summary>
+        [Test]
+        public void Test_ExampleDatasetsCreation()
+        {
+            //Should be empty RDMP metadata database
+            Assert.AreEqual(0,CatalogueRepository.GetAllObjects<Catalogue>().Length);
+            Assert.AreEqual(0,CatalogueRepository.GetAllObjects<AggregateConfiguration>().Length);
+
+            //create all the stuff
+            var db = GetCleanedServer(FAnsi.DatabaseType.MicrosoftSQLServer);
+            var creator = new ExampleDatasetsCreation(RepositoryLocator);
+            creator.Create(db,true,new ThrowImmediatelyCheckNotifier());
+
+            //should have at least created some catalogues, graphs etc
+            Assert.GreaterOrEqual(CatalogueRepository.GetAllObjects<Catalogue>().Length,4);
+            Assert.GreaterOrEqual(CatalogueRepository.GetAllObjects<AggregateConfiguration>().Length,4);
+        }
+    }
+}
--- Rdmp.Core/CommandLine/DatabaseCreation/ExampleDatasetsCreation.cs
+++ Rdmp.Core/CommandLine/DatabaseCreation/ExampleDatasetsCreation.cs
@@ -7,28 +7,29 @@
 using BadMedicine;
 using BadMedicine.Datasets;
 using FAnsi.Discovery;
+using FAnsi.Discovery.TypeTranslation;
 using Rdmp.Core.Curation;
 using Rdmp.Core.Curation.Data;
 using Rdmp.Core.Curation.Data.Aggregation;
-using Rdmp.Core.Curation.Data.Cohort;
 using Rdmp.Core.Curation.FilterImporting;
 using Rdmp.Core.DataExport.Data;
+using Rdmp.Core.Repositories;
 using ReusableLibraryCode.Checks;
 using System;
 using System.Linq;
 
 namespace Rdmp.Core.CommandLine.DatabaseCreation
 {
-    internal class ExampleDatasetsCreation
+    public class ExampleDatasetsCreation
     {
-        private PlatformDatabaseCreationRepositoryFinder _repos;
+        private IRDMPPlatformRepositoryServiceLocator _repos;
 
 
         private const int NumberOfPeople = 5000;
         private const int NumberOfRowsPerDataset = 10000;
 
 
-        public ExampleDatasetsCreation(PlatformDatabaseCreationRepositoryFinder repos)
+        public ExampleDatasetsCreation(IRDMPPlatformRepositoryServiceLocator repos)
         {
             this._repos = repos;
         }
@@ -60,16 +61,107 @@ namespace Rdmp.Core.CommandLine.DatabaseCreation
             var demography = ImportCatalogue(Create<Demography>(db,people,r,notifier,NumberOfRowsPerDataset,"chi","dtCreated","hb_extract"));
             var prescribing = ImportCatalogue(Create<Prescribing>(db,people,r,notifier,NumberOfRowsPerDataset/10,"chi","PrescribedDate","Name")); //<- this is slooo!
             var admissions = ImportCatalogue(Create<HospitalAdmissions>(db,people,r,notifier,NumberOfRowsPerDataset,"chi","AdmissionDate"));
-
-            //create only on disk this one
             var carotid = Create<CarotidArteryScan>(db,people,r,notifier,NumberOfRowsPerDataset,"RECORD_NUMBER");
 
-            CreateGraph(biochem,"Test Codes","TestCode",null,false);
-            CreateGraph(biochem,"Test Codes By Date","TestCode","SampleDate",true);
-            
-            CreateFilter(biochem,"Creatinine","TestCode","TestCode like '%CRE%'",@"Serum creatinine is a blood measurement.  It is an indicator of renal health.");
+            CreateAdmissionsViews(db);
+            var vConditions = ImportCatalogue(db.ExpectTable("vConditions"));
+            var vOperations = ImportCatalogue(db.ExpectTable("vOperations"));    
 
+            CreateGraph(biochem,"Test Codes","TestCode",false,null);
+            CreateGraph(biochem,"Test Codes By Date","SampleDate",true,"TestCode");
+
+            CreateFilter(biochem,"Creatinine","TestCode","TestCode like '%CRE%'",@"Serum creatinine is a blood measurement.  It is an indicator of renal health.");
             CreateFilter(biochem,"Test Code","TestCode","TestCode like @code","Filters any test code set");
+            
+            CreateFilter(
+                CreateGraph(vConditions,"Conditions frequency","Condition",false,"Field"),
+                "Common Conditions Only",
+                @"(Condition in 
+(select top 20 Condition from vConditions c
+ WHERE Condition <> 'NULL' AND Condition <> 'Nul' 
+ group by Condition order by count(*) desc))");
+                        
+            CreateFilter(
+                CreateGraph(vOperations,"Operation frequency","Operation",false,"Field"),
+                "Common Operation Only",
+                @"(Operation in 
+(select top 20 Operation from vOperations c
+ WHERE Operation <> 'NULL' AND Operation <> 'Nul' 
+ group by Operation order by count(*) desc))");            
+            
+            //group these all into the same folder
+            admissions.Folder = new CatalogueFolder(admissions,@"\admissions");
+            admissions.SaveToDatabase();
+            vConditions.Folder = new CatalogueFolder(vConditions,@"\admissions");
+            vConditions.SaveToDatabase();
+            vOperations.Folder = new CatalogueFolder(vOperations,@"\admissions");
+            vOperations.SaveToDatabase();
+
+
+        }
+
+        private IFilter CreateFilter(AggregateConfiguration graph, string name, string whereSql)
+        {
+            AggregateFilterContainer container;
+            if(graph.RootFilterContainer_ID == null)
+            {
+                container = new AggregateFilterContainer(_repos.CatalogueRepository,FilterContainerOperation.AND);
+                graph.RootFilterContainer_ID =container.ID;
+                graph.SaveToDatabase();
+            }
+            else
+                container = graph.RootFilterContainer;
+            
+            var filter = new AggregateFilter(_repos.CatalogueRepository,name,container);
+            filter.WhereSQL = whereSql;
+            filter.SaveToDatabase();
+
+            return filter;
+        }
+
+        private void CreateAdmissionsViews(DiscoveredDatabase db)
+        {
+            using(var con = db.Server.GetConnection())
+            {
+                con.Open();
+                var cmd = db.Server.GetCommand(
+
+                @"create view vConditions as
+
+SELECT chi,DateOfBirth,AdmissionDate,DischargeDate,Condition,Field
+FROM
+(
+  SELECT chi,DateOfBirth,AdmissionDate,DischargeDate,MainCondition,OtherCondition1,OtherCondition2,OtherCondition3
+  FROM HospitalAdmissions
+) AS cp
+UNPIVOT 
+(
+  Condition FOR Field IN (MainCondition,OtherCondition1,OtherCondition2,OtherCondition3)
+) AS up;",con);
+                cmd.ExecuteNonQuery();
+
+                
+
+                cmd = db.Server.GetCommand(
+@"create view vOperations as
+
+SELECT chi,DateOfBirth,AdmissionDate,DischargeDate,Operation,Field
+FROM
+(
+  SELECT chi,DateOfBirth,AdmissionDate,DischargeDate,MainOperation,OtherOperation1,OtherOperation2,OtherOperation3
+  FROM HospitalAdmissions
+) AS cp
+UNPIVOT 
+(
+  Operation FOR Field IN (MainOperation,OtherOperation1,OtherOperation2,OtherOperation3)
+) AS up;",con);
+                cmd.ExecuteNonQuery();
+                
+
+            }
+            
+
+
         }
 
         private IFilter CreateFilter(Catalogue cata, string name,string parentExtractionInformation, string whereSql,string desc)
@@ -85,27 +177,36 @@ namespace Rdmp.Core.CommandLine.DatabaseCreation
             return filter;
         }
 
-        private void CreateGraph(Catalogue cata, string name, string dimension, string axisIfAny, bool pivot)
+        /// <summary>
+        /// Creates a new AggregateGraph for the given dataset (<paramref name="cata"/>)
+        /// </summary>
+        /// <param name="cata"></param>
+        /// <param name="name">The name to give the graph</param>
+        /// <param name="dimension1">The first dimension e.g. pass only one dimension to create a bar chart</param>
+        /// <param name="isAxis">True if <paramref name="dimension1"/> should be created as a axis (creates a line chart)</param>
+        /// <param name="dimension2">Optional second dimension to create (this will be the pivot column)</param>
+        private AggregateConfiguration CreateGraph(Catalogue cata, string name, string dimension1,bool isAxis, string dimension2)
         {
             var ac = new AggregateConfiguration(_repos.CatalogueRepository,cata,name);
             
-            var mainDimension = ac.AddDimension(GetExtractionInformation(cata,dimension));
+            var mainDimension = ac.AddDimension(GetExtractionInformation(cata,dimension1));
+            var otherDimension = string.IsNullOrWhiteSpace(dimension2) ? null : ac.AddDimension(GetExtractionInformation(cata,dimension2));
             
-            if(!string.IsNullOrWhiteSpace(axisIfAny))
+            if(isAxis)
             {
-                var axisDimension = ac.AddDimension(GetExtractionInformation(cata,axisIfAny));
-                var axis = new AggregateContinuousDateAxis(_repos.CatalogueRepository,axisDimension);
+                var axis = new AggregateContinuousDateAxis(_repos.CatalogueRepository,mainDimension);
                 axis.StartDate = "'1970-01-01'";
                 axis.AxisIncrement = FAnsi.Discovery.QuerySyntax.Aggregation.AxisIncrement.Year;
                 axis.SaveToDatabase();
             }
 
-            if(pivot)
+            if(otherDimension != null)
             {
-                ac.PivotOnDimensionID = mainDimension.ID;
+                ac.PivotOnDimensionID = otherDimension.ID;
                 ac.SaveToDatabase();
-            }
-                
+            }          
+            
+            return ac;
         }
 
         private ExtractionInformation GetExtractionInformation(Catalogue cata, string name)
@@ -132,7 +233,7 @@ namespace Rdmp.Core.CommandLine.DatabaseCreation
             var dt = biochem.GetDataTable(people,numberOfRecords);
 
             notifier.OnCheckPerformed(new CheckEventArgs("Uploading " + dataset,CheckResult.Success));
-            var tbl = db.CreateTable(dataset,dt);
+            var tbl = db.CreateTable(dataset,dt,GetExplicitColumnDefinitions<T>());
 
             if(primaryKey.Length != 0)
             {
@@ -143,6 +244,28 @@ namespace Rdmp.Core.CommandLine.DatabaseCreation
 
             return tbl;            
         }
+
+        private DatabaseColumnRequest[] GetExplicitColumnDefinitions<T>() where T : IDataGenerator
+        {
+            
+            if(typeof(T) == typeof(HospitalAdmissions))
+            {
+                return new []{ 
+                    new DatabaseColumnRequest("MainOperation",new DatabaseTypeRequest(typeof(string),4)),
+                    new DatabaseColumnRequest("MainOperationB",new DatabaseTypeRequest(typeof(string),4)),
+                    new DatabaseColumnRequest("OtherOperation1",new DatabaseTypeRequest(typeof(string),4)),
+                    new DatabaseColumnRequest("OtherOperation1B",new DatabaseTypeRequest(typeof(string),4)),
+                    new DatabaseColumnRequest("OtherOperation2",new DatabaseTypeRequest(typeof(string),4)),
+                    new DatabaseColumnRequest("OtherOperation2B",new DatabaseTypeRequest(typeof(string),4)),
+                    new DatabaseColumnRequest("OtherOperation3",new DatabaseTypeRequest(typeof(string),4)),
+                    new DatabaseColumnRequest("OtherOperation3B",new DatabaseTypeRequest(typeof(string),4))
+                    };
+            }
+
+
+            return null;
+        }
+
         private TableInfo ImportTableInfo(DiscoveredTable tbl)
         {
             var importer = new TableInfoImporter(_repos.CatalogueRepository,tbl);
