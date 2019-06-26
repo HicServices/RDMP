f5b299feaa82332b86f6fb1ffc310e3b1e718d8d
Thomas Nind <TZNind@dundee.ac.uk>
2019-06-26 10:08:48 +0100

    RDMPDEV-1343 Fixed various incorrect references in nuspec.  Added test for keeping csproj, nuspec and markdown in sync

---
+++ Reusable/Tests/ReusableCodeTests/NuspecIsCorrectTests.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) The University of Dundee 2018-2019
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ReusableCodeTests
+{
+    /// <summary>
+    /// Tests to confirm that the dependencies in csproj files (NuGet packages) match those in the .nuspec files and that packages.md 
+    /// lists the correct versions (in documentation)
+    /// </summary>
+    class NuspecIsCorrectTests
+    {
+        [TestCase("../../../../../../Tests.Common/Tests.Common.csproj","../../../../../../Plugins/Plugin.Test/Plugin.Test.nuspec","../../../../../../Documentation/CodeTutorials/Packages.md")]
+        public void TestDependencyCorrect( string csproj, string nuspec, string packagesMarkdown)
+        {
+            if(!File.Exists(csproj))
+                Assert.Fail("Could not find file {0}",csproj);
+            if(!File.Exists(nuspec))
+                Assert.Fail("Could not find file {0}",nuspec);
+
+            if(packagesMarkdown != null && !File.Exists(packagesMarkdown))
+                Assert.Fail("Could not find file {0}",packagesMarkdown);
+            
+            //<PackageReference Include="NUnit3TestAdapter" Version="3.13.0" />
+            Regex rPackageRef = new Regex(@"<PackageReference\s+Include=""(.*)""\s+Version=""(.*)""",RegexOptions.IgnoreCase);
+
+            //<dependency id="CsvHelper" version="12.1.2" />
+            Regex rDependencyRef = new Regex(@"<dependency\s+id=""(.*)""\s+version=""(.*)""",RegexOptions.IgnoreCase);
+            
+            //For each dependency listed in the csproj
+            foreach(Match p in rPackageRef.Matches(File.ReadAllText(csproj)))
+            {
+                string package = p.Groups[1].Value;
+                string version = p.Groups[2].Value;
+
+                bool found = false;
+                 
+                //make sure it appears in the nuspec
+                foreach(Match d in rDependencyRef.Matches(File.ReadAllText(nuspec)))
+                {
+                    string packageDependency = d.Groups[1].Value;
+                    string versionDependency = d.Groups[2].Value;
+
+                    if(packageDependency.Equals(package))
+                    {
+                        Assert.AreEqual(version,versionDependency, "Package {0} is version {1} in {2} but version {3} in {4}",package,version,csproj,packageDependency,versionDependency,nuspec);   
+                        found = true;
+                    }
+                }
+
+                if(!found)
+                    Assert.Fail("Package {0} in {1} is not listed as a dependency of {2}",package,csproj,nuspec);
+
+                //And make sure it appears in the packages.md file
+                if(packagesMarkdown != null)
+                {
+                    found = false;
+                    foreach(string line in File.ReadAllLines(packagesMarkdown))
+                    {
+                        if(Regex.IsMatch(line,@"[\s[]"+Regex.Escape(package) + @"[\s\]]",RegexOptions.IgnoreCase))
+                        {
+                            int count = new Regex(Regex.Escape(version)).Matches(line).Count;
+                                                       
+                            Assert.AreEqual(2,count, "Markdown file {0} did not contain 2 instances of the version {1} for package {2} in {3}",packagesMarkdown,version,package,csproj);
+                            found = true;
+                        }
+                    }
+
+                    if(!found)
+                        Assert.Fail("Package {0} in {1} is not documented in {2}. Recommended line is:\r\n{3}",package,csproj,packagesMarkdown,BuildRecommendedLine(package,version));
+                }
+            }
+        }
+
+        private object BuildRecommendedLine(string package, string version)
+        {
+            return string.Format("| {0} | [GitHub]() | [{1}](https://www.nuget.org/packages/{0}/{1}) | | | |",package,version);
+        }
+    }
+}
