b6edd1cee7162e5074b5ea1072a7d00af8a9305a
Thomas Nind <TZNind@dundee.ac.uk>
2019-07-05 09:16:51 +0100

    RDMPDEV-1470 Changed ORM layer to allow extra fields in database that do not appear on model.

--- Reusable/MapsDirectlyToDatabaseTable/UpdateCommandStore.cs
+++ Reusable/MapsDirectlyToDatabaseTable/UpdateCommandStore.cs
@@ -7,7 +7,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 using FAnsi.Discovery;
 using ReusableLibraryCode;
 
@@ -45,50 +47,16 @@ namespace MapsDirectlyToDatabaseTable
         }
         public void Add(Type o, DbConnectionStringBuilder builder, DbConnection connection, DbTransaction transaction)
         {
-            var cmd = DatabaseCommandHelper.GetCommand("SELECT * FROM " + o.Name + " WHERE ID=0", connection, transaction);
-            var cmdbuilder = new DiscoveredServer(builder).Helper.GetCommandBuilder(cmd);
+            var command = DatabaseCommandHelper.GetCommand("UPDATE " + o.Name + " SET {0} WHERE ID=@ID;" , connection, transaction);
+                        
+            var props = TableRepository.GetPropertyInfos(o);
+
+            foreach(PropertyInfo p in props)
+                command.Parameters.Add(DatabaseCommandHelper.GetParameter("@"+p.Name,command));
+
+            command.CommandText = string.Format(command.CommandText,string.Join(",",props.Where(p=>p.Name != "ID").Select(p=> "["+p.Name+"]=@" + p.Name)));
             
-            DbCommand cmdUpdate = cmdbuilder.GetUpdateCommand(true);
-
-            //throw away addititional UPDATE crud and add WHERE ID=@ID 
-            string adjustedcmd = cmdUpdate.CommandText;
-            adjustedcmd = adjustedcmd.Substring(0, adjustedcmd.IndexOf("WHERE"));
-            adjustedcmd += "WHERE ID=@ID;";
-
-            cmdUpdate.CommandText = adjustedcmd;
-            AdjustUpdateCommand(o, cmdUpdate);
-
-            UpdateCommands.Add(o, cmdUpdate);
-        }
-
-        /// <summary>
-        /// Removes unwanted _Orig values for the Where statement.  All properties in the destination database must be named the same as in this data class so that reflection can be used to 
-        /// fill in the command.  The only WHERE condition after running this method is 'WHERE ID = @ID'
-        /// </summary>
-        private void AdjustUpdateCommand(Type objectType, DbCommand command)
-        {
-            List<DbParameter> toDiscard = new List<DbParameter>();
-
-            foreach (DbParameter p in command.Parameters)
-            {
-                PropertyInfo prop = objectType.GetProperty(p.ParameterName.Trim('@'));
-
-                //set parameters that exist
-                if (prop == null)
-                {
-                    //if it is a set param
-                    if (p.ParameterName.StartsWith("@Original_") || p.ParameterName.StartsWith("@IsNull") || p.ParameterName.Equals("@hic_validFrom"))
-                        toDiscard.Add(p);
-                    else
-                        throw new Exception("Column " + p.ParameterName + " was present in UPDATE command but was not found in the data class " + objectType.Name);
-                }
-            }
-
-            foreach (DbParameter p in toDiscard)
-                command.Parameters.Remove(p);
-
-            if (!command.Parameters.Contains("@ID"))
-                DatabaseCommandHelper.AddParameterWithValueToCommand("@ID", command, null);       
+            UpdateCommands.Add(o, command);
         }
 
         public bool ContainsKey(IMapsDirectlyToDatabaseTable toCreate)
