cc7249a2397209ed015ca0eac3d3e52b195dbf97
Thomas Nind <TZNind@dundee.ac.uk>
2019-08-13 09:56:07 +0100

    RDMPDEV-1538 Refactored ExtractionConfigurationMenu to fully use the ExecuteCommand paradigm

---
+++ Rdmp.UI/CommandExecution/AtomicCommands/ExecuteCommandAddPackageToConfiguration.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) The University of Dundee 2018-2019
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
+using System.Drawing;
+using System.Linq;
+using Rdmp.Core.DataExport.Data;
+using Rdmp.Core.Providers;
+using Rdmp.UI.CommandExecution.AtomicCommands;
+using Rdmp.UI.Copying.Commands;
+using Rdmp.UI.Icons.IconProvision;
+using Rdmp.UI.ItemActivation;
+using ReusableLibraryCode.CommandExecution.AtomicCommands;
+using ReusableLibraryCode.Icons.IconProvision;
+
+namespace Rdmp.UI.CommandExecution.AtomicCommands
+{
+    internal class ExecuteCommandAddPackageToConfiguration : BasicUICommandExecution, IAtomicCommand
+    {
+        private ExtractionConfiguration _extractionConfiguration;
+        private ExtractableDataSetPackage[] _packages;
+
+        public ExecuteCommandAddPackageToConfiguration(IActivateItems activator, ExtractionConfiguration extractionConfiguration):base(activator)
+        {
+            this._extractionConfiguration = extractionConfiguration;
+
+            if(extractionConfiguration.IsReleased)
+                SetImpossible("ExtractionConfiguration has already been released");
+
+            if(activator.CoreChildProvider is DataExportChildProvider childProvider)
+            {
+                if (childProvider.AllPackages.Any())
+                    _packages = childProvider.AllPackages;
+                else
+                    SetImpossible("There are no ExtractableDatasetPackages configured");
+            }
+            else
+                SetImpossible("CoreChildProvider is not DataExportIconProvider");
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            if(SelectOne(_packages,out ExtractableDataSetPackage package))
+                new ExecuteCommandAddDatasetsToConfiguration(Activator, new ExtractableDataSetCommand(package), _extractionConfiguration).Execute();
+        }
+
+        public override Image GetImage(IIconProvider iconProvider)
+        {
+            return iconProvider.GetImage(RDMPConcept.ExtractableDataSetPackage,OverlayKind.Import);
+        }
+    }
+}
