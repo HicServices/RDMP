ecb0459289a8dba97fa36d12c330cecc38085956
Thomas Nind <TZNind@dundee.ac.uk>
2019-10-16 07:50:49 +0100

    MSS-104 Renamed generic test command to highlight it is for testing and added two parameter version

--- Rdmp.Core.Tests/CommandExecution/CommandInvokerTests.cs
+++ Rdmp.Core.Tests/CommandExecution/CommandInvokerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FAnsi.Discovery;
 using NUnit.Framework;
 using Rdmp.Core.CommandExecution;
 using Rdmp.Core.CommandExecution.AtomicCommands;
@@ -52,8 +53,12 @@ namespace Rdmp.Core.Tests.CommandExecution
 
             WhenIHaveA<Catalogue>();
 
-            invoker.ExecuteCommand(typeof(GenericCommand<DatabaseEntity>),GetPicker("Catalogue:*"));
-            invoker.ExecuteCommand(typeof(GenericCommand<Type>),GetPicker("Pipeline"));
+            invoker.ExecuteCommand(typeof(GenericTestCommand<DatabaseEntity>),GetPicker("Catalogue:*"));
+            invoker.ExecuteCommand(typeof(GenericTestCommand<Type>),GetPicker("Pipeline"));
+            invoker.ExecuteCommand(typeof(GenericTestCommand<DiscoveredDatabase,bool>),
+                GetPicker(
+                    "DatabaseType:MicrosoftSqlServer:Name:imaging:Server=localhost\\sqlexpress;Database=master;Trusted_Connection=True;",
+                    "true"));
             
         }
 
@@ -62,11 +67,11 @@ namespace Rdmp.Core.Tests.CommandExecution
             return new CommandLineObjectPicker(args, RepositoryLocator);
         }
 
-        private class GenericCommand<T> : BasicCommandExecution
+        private class GenericTestCommand<T> : BasicCommandExecution
         {
             private readonly T _arg;
             
-            public GenericCommand(T a)
+            public GenericTestCommand(T a)
             {
                 _arg = a;
             }
@@ -76,6 +81,27 @@ namespace Rdmp.Core.Tests.CommandExecution
                 base.Execute();
                 Console.Write("Arg was " + _arg);
                 Assert.IsNotNull(_arg);
+            }
+        }
+
+        private class GenericTestCommand<T1,T2> : BasicCommandExecution
+        {
+            private readonly T1 _a;
+            private readonly T2 _b;
+            
+            public GenericTestCommand(T1 a, T2 b)
+            {
+                _a = a;
+                _b = b;
+            }
+
+            public override void Execute()
+            {
+                base.Execute();
+                Console.Write("_a was " + _a);
+                Console.Write("_b was " + _b);
+                Assert.IsNotNull(_a);
+                Assert.IsNotNull(_b);
             }
         }
     }
