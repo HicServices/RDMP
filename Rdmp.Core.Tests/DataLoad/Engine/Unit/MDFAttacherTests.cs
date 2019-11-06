6d7bc3c75bddaf3e9d5f943043d4260bb9cbcee6
tznind <tznind@dundee.ac.uk>
2019-11-06 21:37:21 +0000

    fixed line endings in tests

--- Rdmp.Core.Tests/DataLoad/Engine/Unit/MDFAttacherTests.cs
+++ Rdmp.Core.Tests/DataLoad/Engine/Unit/MDFAttacherTests.cs
@@ -196,7 +196,7 @@ namespace Rdmp.Core.Tests.DataLoad.Engine.Unit
             }
 
             var memory2 = new ToMemoryCheckNotifier(new ThrowImmediatelyCheckNotifier());
-            mdf.OverrideMDFFileCopyDestination = @"C:/temp";
+            mdf.OverrideMDFFileCopyDestination = TestContext.CurrentContext.WorkDirectory;
             mdf.Check(memory2);
             Assert.IsTrue(memory2.Messages.Any(m => Regex.IsMatch(m.Message,@"Found server DATA folder .*C:/temp") && m.Result == CheckResult.Success));
 
