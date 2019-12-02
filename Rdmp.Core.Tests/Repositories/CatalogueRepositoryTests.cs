edad696981ec24b10078c9c1fb18ce9b345cf2f3
tznind <t.z.nind@dundee.ac.uk>
2019-12-02 08:29:44 +0000

    MSS-114 TestConnection now shows obfuscated connection string in Exception message (on failure)

---
+++ Rdmp.Core.Tests/Repositories/CatalogueRepositoryTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using FAnsi.Implementation;
+using NUnit.Framework;
+using Rdmp.Core.Repositories;
+
+namespace Rdmp.Core.Tests.Repositories
+{
+    class CatalogueRepositoryTests
+    {
+        /// <summary>
+        /// Tests that when a <see cref="CatalogueRepository"/> fails connection testing that the password is not exposed but that
+        /// the rest of the connection string is exposed (including any optional settings like connection timeout)
+        /// </summary>
+        [Test]
+        public void TestConnection_NoServer_DoNotShowPassword()
+        {
+            ImplementationManager.Load<FAnsi.Implementations.MicrosoftSQL.MicrosoftSQLImplementation>();
+
+            var repo = new CatalogueRepository(new SqlConnectionStringBuilder()
+            {
+                DataSource = "NonExistant11",
+                UserID = "fish",
+                Password = "omg",
+                ConnectTimeout = 2
+            });
+
+            var msg = Assert.Throws<Exception>(()=>repo.TestConnection());
+
+            StringAssert.StartsWith("Testing connection failed",msg.Message);
+            StringAssert.DoesNotContain("omg",msg.Message);
+            StringAssert.Contains("****",msg.Message);
+            StringAssert.Contains("Timeout",msg.Message);
+            StringAssert.Contains("2",msg.Message);
+        }
+
+        [Test]
+        public void TestConnection_NoServer_IntegratedSecurity()
+        {
+            ImplementationManager.Load<FAnsi.Implementations.MicrosoftSQL.MicrosoftSQLImplementation>();
+
+            var repo = new CatalogueRepository(new SqlConnectionStringBuilder()
+            {
+                DataSource = "NonExistant11",
+                IntegratedSecurity = true,
+                ConnectTimeout = 2
+            });
+
+            var msg = Assert.Throws<Exception>(()=>repo.TestConnection());
+
+            StringAssert.StartsWith("Testing connection failed",msg.Message);
+            StringAssert.Contains("Integrated Security=",msg.Message);
+            StringAssert.Contains("Timeout",msg.Message);
+            StringAssert.Contains("2",msg.Message);
+        }
+
+    }
+}
