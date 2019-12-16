bd9dd4034f4acc1a3b39d9153f7d9f6b8e63b1e0
Thomas Nind <TZNind@dundee.ac.uk>
2019-12-16 09:06:03 +0000

    MSS-109  Added tests for describe commands

---
+++ Rdmp.Core.Tests/CommandExecution/TestExecuteCommandDescribe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using Rdmp.Core.CommandExecution;
+using Rdmp.Core.CommandExecution.AtomicCommands;
+using Rdmp.Core.Curation.Data;
+using Tests.Common;
+
+namespace Rdmp.Core.Tests.CommandExecution
+{
+    class TestExecuteCommandDescribe : UnitTests
+    {
+        [Test]
+        public void TestDescribeCatalogue()
+        {
+            var mock = new Mock<IBasicActivateItems>();
+            mock.Setup(m => m.Show(It.IsAny<string>()));
+            
+            var c = WhenIHaveA<Catalogue>();
+            c.Description = "fish";
+            
+            var describe = new ExecuteCommandDescribe(mock.Object,new []{c});
+            Assert.IsFalse(describe.IsImpossible,describe.ReasonCommandImpossible);
+
+            describe.Execute();
+
+            // Called once
+            mock.Verify(m => m.Show(It.IsRegex(".*Description:fish.*")), Times.Once());
+        }
+    }
+}
---
+++ Rdmp.Core.Tests/CommandExecution/TestExecuteCommandDescribeCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Moq;
+using NUnit.Framework;
+using Rdmp.Core.CommandExecution;
+using Rdmp.Core.CommandExecution.AtomicCommands;
+using Tests.Common;
+
+namespace Rdmp.Core.Tests.CommandExecution
+{
+    class TestExecuteCommandDescribeCommand : UnitTests
+    {
+        [Test]
+        public void Test_DescribeDeleteCommand()
+        {
+            var mock = new Mock<IBasicActivateItems>();
+            mock.Setup(m => m.RepositoryLocator).Returns(RepositoryLocator);
+            mock.Setup(m => m.GetDelegates()).Returns(new List<CommandInvokerDelegate>());
+            mock.Setup(m => m.Show(It.IsAny<string>()));
+
+            var cmd = new ExecuteCommandDescribeCommand(mock.Object, typeof(ExecuteCommandDelete));
+            Assert.IsFalse(cmd.IsImpossible,cmd.ReasonCommandImpossible);
+
+            cmd.Execute();
+
+            string contents = Regex.Escape(@"cmd Delete <deletable> 
+PARAMETERS:
+deletable	IDeleteable	The object you want to delete");
+
+            // Called once
+            mock.Verify(m => m.Show(It.IsRegex(contents)), Times.Once());
+
+            
+        }
+    }
+}
