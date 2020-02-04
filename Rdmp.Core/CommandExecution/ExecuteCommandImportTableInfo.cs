8994c4d7c2c16135fa03b442d066760752916339
tznind <t.z.nind@dundee.ac.uk>
2020-02-04 13:20:09 +0000

    Added import tableinfo CLI command

---
+++ Rdmp.Core/CommandExecution/ExecuteCommandImportTableInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FAnsi.Discovery;
+using Rdmp.Core.Curation;
+using Rdmp.Core.Curation.Data;
+
+namespace Rdmp.Core.CommandExecution
+{
+    class ExecuteCommandImportTableInfo : BasicCommandExecution
+    {
+        private readonly DiscoveredTable _table;
+        private readonly bool _createCatalogue;
+
+        public ExecuteCommandImportTableInfo(IBasicActivateItems activator, DiscoveredTable table, bool createCatalogue): base(activator)
+        {
+            _table = table;
+            _createCatalogue = createCatalogue;
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            Catalogue c = null;
+
+            var importer = new TableInfoImporter(BasicActivator.RepositoryLocator.CatalogueRepository, _table);
+            importer.DoImport(out TableInfo ti, out ColumnInfo[] cis);
+            
+            BasicActivator.Show($"Successfully imported new TableInfo { ti.Name} with ID {ti.ID}");
+
+            if (_createCatalogue)
+            {
+                var forwardEngineer = new ForwardEngineerCatalogue(ti, cis, true);
+                forwardEngineer.ExecuteForwardEngineering(out c,out _,out _);
+
+                BasicActivator.Show($"Successfully imported new Catalogue { c.Name} with ID {c.ID}");
+            }
+                
+            Publish((DatabaseEntity) c ?? ti);
+        }
+    }
+}
