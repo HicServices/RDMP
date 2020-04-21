c7fddacab29ab32776c9608ed422c381cc2e5238
tznind <t.z.nind@dundee.ac.uk>
2020-04-21 14:31:30 +0100

    Changed HomeUI to use new box controls

--- Application/ResearchDataManagementPlatform/WindowManagement/HomePane/HomeUI.Designer.cs
+++ Application/ResearchDataManagementPlatform/WindowManagement/HomePane/HomeUI.Designer.cs
@@ -32,31 +32,14 @@ namespace ResearchDataManagementPlatform.WindowManagement.HomePane
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeUI));
             this.label2 = new System.Windows.Forms.Label();
-            this.lblManagement = new System.Windows.Forms.Label();
-            this.lblExport = new System.Windows.Forms.Label();
             this.pictureBox10 = new System.Windows.Forms.PictureBox();
-            this.tlpDataManagement = new System.Windows.Forms.TableLayoutPanel();
-            this.tlpDataExport = new System.Windows.Forms.TableLayoutPanel();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.tlpCohortCreation = new System.Windows.Forms.TableLayoutPanel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.tlpDataLoad = new System.Windows.Forms.TableLayoutPanel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.tlpRecent = new System.Windows.Forms.TableLayoutPanel();
-            this.homeBoxUI1 = new ResearchDataManagementPlatform.WindowManagement.HomePane.HomeBoxUI();
+            this.boxCatalogue = new ResearchDataManagementPlatform.WindowManagement.HomePane.HomeBoxUI();
+            this.boxCohort = new ResearchDataManagementPlatform.WindowManagement.HomePane.HomeBoxUI();
+            this.boxProject = new ResearchDataManagementPlatform.WindowManagement.HomePane.HomeBoxUI();
+            this.boxDataLoad = new ResearchDataManagementPlatform.WindowManagement.HomePane.HomeBoxUI();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
-            this.panel1.SuspendLayout();
-            this.panel4.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel5.SuspendLayout();
-            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label2
@@ -71,28 +54,6 @@ namespace ResearchDataManagementPlatform.WindowManagement.HomePane
             this.label2.Text = "Research Data Management Platform";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // lblManagement
-            // 
-            this.lblManagement.AutoSize = true;
-            this.lblManagement.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblManagement.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.lblManagement.Location = new System.Drawing.Point(0, 0);
-            this.lblManagement.Name = "lblManagement";
-            this.lblManagement.Size = new System.Drawing.Size(253, 33);
-            this.lblManagement.TabIndex = 0;
-            this.lblManagement.Text = "Data Management";
-            // 
-            // lblExport
-            // 
-            this.lblExport.AutoSize = true;
-            this.lblExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblExport.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.lblExport.Location = new System.Drawing.Point(0, 0);
-            this.lblExport.Name = "lblExport";
-            this.lblExport.Size = new System.Drawing.Size(168, 33);
-            this.lblExport.TabIndex = 0;
-            this.lblExport.Text = "Data Export";
-            // 
             // pictureBox10
             // 
             this.pictureBox10.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox10.Image")));
@@ -103,177 +64,48 @@ namespace ResearchDataManagementPlatform.WindowManagement.HomePane
             this.pictureBox10.TabIndex = 8;
             this.pictureBox10.TabStop = false;
             // 
-            // tlpDataManagement
-            // 
-            this.tlpDataManagement.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tlpDataManagement.ColumnCount = 1;
-            this.tlpDataManagement.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tlpDataManagement.Location = new System.Drawing.Point(0, 36);
-            this.tlpDataManagement.Name = "tlpDataManagement";
-            this.tlpDataManagement.RowCount = 1;
-            this.tlpDataManagement.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tlpDataManagement.Size = new System.Drawing.Size(400, 208);
-            this.tlpDataManagement.TabIndex = 13;
-            // 
-            // tlpDataExport
-            // 
-            this.tlpDataExport.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tlpDataExport.ColumnCount = 1;
-            this.tlpDataExport.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tlpDataExport.Location = new System.Drawing.Point(0, 36);
-            this.tlpDataExport.Name = "tlpDataExport";
-            this.tlpDataExport.RowCount = 1;
-            this.tlpDataExport.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tlpDataExport.Size = new System.Drawing.Size(400, 205);
-            this.tlpDataExport.TabIndex = 16;
-            // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.Controls.Add(this.panel1);
-            this.flowLayoutPanel1.Controls.Add(this.panel4);
-            this.flowLayoutPanel1.Controls.Add(this.panel2);
-            this.flowLayoutPanel1.Controls.Add(this.panel5);
-            this.flowLayoutPanel1.Controls.Add(this.panel3);
-            this.flowLayoutPanel1.Controls.Add(this.homeBoxUI1);
+            this.flowLayoutPanel1.Controls.Add(this.boxCatalogue);
+            this.flowLayoutPanel1.Controls.Add(this.boxCohort);
+            this.flowLayoutPanel1.Controls.Add(this.boxProject);
+            this.flowLayoutPanel1.Controls.Add(this.boxDataLoad);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(18, 94);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(1269, 603);
             this.flowLayoutPanel1.TabIndex = 17;
             // 
-            // panel1
+            // boxCatalogue
             // 
-            this.panel1.Controls.Add(this.lblManagement);
-            this.panel1.Controls.Add(this.tlpDataManagement);
-            this.panel1.Location = new System.Drawing.Point(3, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(400, 244);
-            this.panel1.TabIndex = 18;
+            this.boxCatalogue.Location = new System.Drawing.Point(3, 3);
+            this.boxCatalogue.Name = "boxCatalogue";
+            this.boxCatalogue.Size = new System.Drawing.Size(400, 315);
+            this.boxCatalogue.TabIndex = 21;
             // 
-            // panel4
+            // boxCohort
             // 
-            this.panel4.Controls.Add(this.label1);
-            this.panel4.Controls.Add(this.tlpCohortCreation);
-            this.panel4.Location = new System.Drawing.Point(409, 3);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(400, 244);
-            this.panel4.TabIndex = 19;
+            this.boxCohort.Location = new System.Drawing.Point(409, 3);
+            this.boxCohort.Name = "boxCohort";
+            this.boxCohort.Size = new System.Drawing.Size(400, 315);
+            this.boxCohort.TabIndex = 22;
             // 
-            // label1
+            // boxProject
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(220, 33);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Cohort Creation";
+            this.boxProject.Location = new System.Drawing.Point(815, 3);
+            this.boxProject.Name = "boxProject";
+            this.boxProject.Size = new System.Drawing.Size(400, 315);
+            this.boxProject.TabIndex = 23;
             // 
-            // tlpCohortCreation
+            // boxDataLoad
             // 
-            this.tlpCohortCreation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tlpCohortCreation.ColumnCount = 1;
-            this.tlpCohortCreation.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tlpCohortCreation.Location = new System.Drawing.Point(0, 36);
-            this.tlpCohortCreation.Name = "tlpCohortCreation";
-            this.tlpCohortCreation.RowCount = 1;
-            this.tlpCohortCreation.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tlpCohortCreation.Size = new System.Drawing.Size(400, 205);
-            this.tlpCohortCreation.TabIndex = 16;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.lblExport);
-            this.panel2.Controls.Add(this.tlpDataExport);
-            this.panel2.Location = new System.Drawing.Point(815, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(400, 241);
-            this.panel2.TabIndex = 18;
-            // 
-            // panel5
-            // 
-            this.panel5.Controls.Add(this.label3);
-            this.panel5.Controls.Add(this.tlpDataLoad);
-            this.panel5.Location = new System.Drawing.Point(3, 253);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(400, 194);
-            this.panel5.TabIndex = 19;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.label3.Location = new System.Drawing.Point(0, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(148, 33);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Data Load";
-            // 
-            // tlpDataLoad
-            // 
-            this.tlpDataLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tlpDataLoad.ColumnCount = 1;
-            this.tlpDataLoad.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tlpDataLoad.Location = new System.Drawing.Point(0, 36);
-            this.tlpDataLoad.Name = "tlpDataLoad";
-            this.tlpDataLoad.RowCount = 1;
-            this.tlpDataLoad.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tlpDataLoad.Size = new System.Drawing.Size(400, 158);
-            this.tlpDataLoad.TabIndex = 16;
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.label4);
-            this.panel3.Controls.Add(this.tlpRecent);
-            this.panel3.Location = new System.Drawing.Point(409, 253);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(400, 194);
-            this.panel3.TabIndex = 20;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.label4.Location = new System.Drawing.Point(0, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(107, 33);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Recent";
-            // 
-            // tlpRecent
-            // 
-            this.tlpRecent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tlpRecent.ColumnCount = 1;
-            this.tlpRecent.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tlpRecent.Location = new System.Drawing.Point(0, 36);
-            this.tlpRecent.Name = "tlpRecent";
-            this.tlpRecent.RowCount = 1;
-            this.tlpRecent.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tlpRecent.Size = new System.Drawing.Size(400, 158);
-            this.tlpRecent.TabIndex = 16;
-            // 
-            // homeBoxUI1
-            // 
-            this.homeBoxUI1.Location = new System.Drawing.Point(3, 453);
-            this.homeBoxUI1.Name = "homeBoxUI1";
-            this.homeBoxUI1.Size = new System.Drawing.Size(480, 499);
-            this.homeBoxUI1.TabIndex = 21;
+            this.boxDataLoad.Location = new System.Drawing.Point(3, 324);
+            this.boxDataLoad.Name = "boxDataLoad";
+            this.boxDataLoad.Size = new System.Drawing.Size(400, 315);
+            this.boxDataLoad.TabIndex = 24;
             // 
             // HomeUI
             // 
@@ -288,16 +120,6 @@ namespace ResearchDataManagementPlatform.WindowManagement.HomePane
             this.Size = new System.Drawing.Size(1287, 697);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
             this.flowLayoutPanel1.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            this.panel5.ResumeLayout(false);
-            this.panel5.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -306,23 +128,11 @@ namespace ResearchDataManagementPlatform.WindowManagement.HomePane
         #endregion
 
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label lblManagement;
-        private System.Windows.Forms.Label lblExport;
         private System.Windows.Forms.PictureBox pictureBox10;
-        private System.Windows.Forms.TableLayoutPanel tlpDataManagement;
-        private System.Windows.Forms.TableLayoutPanel tlpDataExport;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TableLayoutPanel tlpCohortCreation;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TableLayoutPanel tlpDataLoad;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TableLayoutPanel tlpRecent;
-        private HomeBoxUI homeBoxUI1;
+        private HomeBoxUI boxCatalogue;
+        private HomeBoxUI boxCohort;
+        private HomeBoxUI boxProject;
+        private HomeBoxUI boxDataLoad;
     }
 }
--- Application/ResearchDataManagementPlatform/WindowManagement/HomePane/HomeUI.cs
+++ Application/ResearchDataManagementPlatform/WindowManagement/HomePane/HomeUI.cs
@@ -5,19 +5,13 @@
 // You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
 using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Windows.Forms;
-using Rdmp.Core;
-using Rdmp.Core.CommandExecution.AtomicCommands;
 using Rdmp.Core.Curation.Data;
-using Rdmp.Core.Providers;
-using Rdmp.UI.Collections.Providers;
+using Rdmp.Core.Curation.Data.Cohort;
+using Rdmp.Core.Curation.Data.DataLoad;
+using Rdmp.Core.DataExport.Data;
 using Rdmp.UI.CommandExecution.AtomicCommands;
 using Rdmp.UI.CommandExecution.AtomicCommands.CohortCreationCommands;
 using Rdmp.UI.CommandExecution.AtomicCommands.UIFactory;
-using Rdmp.UI.CommandExecution.AtomicCommands.WindowArranging;
 using Rdmp.UI.ItemActivation;
 using Rdmp.UI.Refreshing;
 using Rdmp.UI.TestsAndSetup.ServicePropogation;
@@ -44,7 +38,7 @@ namespace ResearchDataManagementPlatform.WindowManagement.HomePane
 
         private void BuildCommandLists()
         {
-            homeBoxUI1.SetUp(Activator,"Catalogue",typeof(Catalogue),_uiFactory,
+            boxCatalogue.SetUp(Activator,"Catalogue",typeof(Catalogue),_uiFactory,
                 new ExecuteCommandCreateNewCatalogueByImportingFile(_activator)
                 {
                     OverrideCommandName = "From File"
@@ -53,104 +47,21 @@ namespace ResearchDataManagementPlatform.WindowManagement.HomePane
                 {
                     OverrideCommandName = "From Database"
                 });
-
-            tlpDataManagement.Controls.Clear();
-            tlpCohortCreation.Controls.Clear();
-            tlpDataExport.Controls.Clear();
-            tlpDataLoad.Controls.Clear();
-            tlpRecent.Controls.Clear();
-
-
-            /////////////////////////////////////Data Management/////////////////////////////////
-            //AddLabel("New Catalogue", tlpDataManagement);
-
-            AddCommand(new ExecuteCommandCreateNewCatalogueByImportingFile(_activator), tlpDataManagement);
-
-            AddCommand(new ExecuteCommandCreateNewCatalogueByImportingExistingDataTable(_activator), tlpDataManagement);
-
-            AddCommand(new ExecuteCommandEditExistingCatalogue(_activator),tlpDataManagement);
-
-            AddCommand(new ExecuteCommandRunDQEOnCatalogue(_activator),tlpDataManagement);
-
-            /////////////////////////////////////Cohort Creation/////////////////////////////////
-
-            AddCommand(new ExecuteCommandCreateNewCohortFromFile(_activator),tlpCohortCreation);
-
-            AddCommand(new ExecuteCommandCreateNewCohortIdentificationConfiguration(_activator)
-            {
-                OverrideCommandName = GlobalStrings.CreateNewCohortIdentificationQuery
-            },tlpCohortCreation);
-
-            AddCommand(new ExecuteCommandCreateNewCohortByExecutingACohortIdentificationConfiguration(_activator)
-            {
-                OverrideCommandName = "Create New Cohort From Cohort Identification Query"
-            }, tlpCohortCreation);
-
-            AddCommand(new ExecuteCommandEditExistingCohortIdentificationConfiguration(_activator)
-            {
-                OverrideCommandName = "Edit Cohort Identification Query"
-            }, tlpCohortCreation);
-
+            boxProject.SetUp(Activator,"Project",typeof(Project),_uiFactory, new ExecuteCommandCreateNewDataExtractionProject(_activator));
             
-            /////////////////////////////////////Data Export/////////////////////////////////
-            
-            var dataExportChildProvider = _activator.CoreChildProvider as DataExportChildProvider;
-            if (dataExportChildProvider != null)
-            {
-                AddCommand(new ExecuteCommandCreateNewDataExtractionProject(_activator), tlpDataExport);
-                AddCommand(new ExecuteCommandEditDataExtractionProject(_activator), tlpDataExport);
-            }
-
-            //////////////////////////////////Data Loading////////////////////////////////////
-            AddCommand(new ExecuteCommandCreateNewLoadMetadata(_activator),tlpDataLoad);
-            AddCommand(new ExecuteCommandExecuteLoadMetadata(_activator), tlpDataLoad);
-
-            foreach (var entry in _activator.HistoryProvider.History.OrderByDescending(e=>e.Date).Take(10))
-                if(((DatabaseEntity)entry.Object).Exists())
-                    AddCommand(new ExecuteCommandActivate(_activator, entry.Object)
-                    {
-                        OverrideCommandName = entry.Object.ToString(),
-                        AlsoShow = true
-                    },tlpRecent);
-            
-            FixSizingOfTableLayoutPanel(tlpDataManagement);
-            FixSizingOfTableLayoutPanel(tlpCohortCreation);
-            FixSizingOfTableLayoutPanel(tlpDataExport);
-            FixSizingOfTableLayoutPanel(tlpDataLoad);
-            FixSizingOfTableLayoutPanel(tlpRecent);
-
-
+            boxCohort.SetUp(Activator,"Cohort Builder", typeof(CohortIdentificationConfiguration),_uiFactory,
+                new ExecuteCommandCreateNewCohortIdentificationConfiguration(_activator)
+                {
+                    OverrideCommandName = "Query"
+                },
+                new ExecuteCommandCreateNewCohortFromFile(_activator)
+                {
+                    OverrideCommandName = "From File"
+                }
+            );
+            boxDataLoad.SetUp(Activator,"Data Load",typeof(LoadMetadata),_uiFactory,new ExecuteCommandCreateNewLoadMetadata(_activator));
         }
-
-        private void AddCommand(IAtomicCommand command, TableLayoutPanel tableLayoutPanel)
-        {
-            var control = _uiFactory.CreateLinkLabel(command);
-
-            SetBackgroundColor(tableLayoutPanel, control);
-
-            tableLayoutPanel.Controls.Add(control, 0, tableLayoutPanel.Controls.Count);
-            
-            //extend the size to match
-            var panel = (Panel)tableLayoutPanel.Parent;
-            panel.Width = Math.Max(panel.Width, control.Width + 10);
-        }
-
-        readonly Dictionary<TableLayoutPanel, int> _alternateBackgroundColours = new Dictionary<TableLayoutPanel, int>();
-
-        private void SetBackgroundColor(TableLayoutPanel tableLayoutPanel, Control control)
-        {
-            if (!_alternateBackgroundColours.ContainsKey(tableLayoutPanel))
-                _alternateBackgroundColours.Add(tableLayoutPanel, 0);
-
-            control.BackColor = _alternateBackgroundColours[tableLayoutPanel]++ % 2 == 0 ? Color.AliceBlue : Color.White;
-        }
-
-        private void FixSizingOfTableLayoutPanel(TableLayoutPanel tableLayoutPanel)
-        {
-            foreach (RowStyle style in tableLayoutPanel.RowStyles)
-                style.SizeType = SizeType.AutoSize;
-        }
-
+        
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
