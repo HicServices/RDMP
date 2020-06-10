65a27da2abb4118ad256abd31e0ebe77c3d3528b
tznind <t.z.nind@dundee.ac.uk>
2020-06-10 08:31:42 +0100

    Added tests

--- Rdmp.Core.Tests/CommandLine/RdmpScriptTests.cs
+++ Rdmp.Core.Tests/CommandLine/RdmpScriptTests.cs
@@ -41,8 +41,37 @@ namespace Rdmp.Core.Tests.CommandLine
             Assert.AreEqual(1,RepositoryLocator.CatalogueRepository.GetAllObjects<Catalogue>().Length);
 
             Assert.AreEqual(expectedName,RepositoryLocator.CatalogueRepository.GetAllObjects<Catalogue>().Single().Name);
+        }
 
+        [TestCase("NewObject Catalogue 'fffff'","NewObject CatalogueItem Catalogue:*fff* 'bbbb'","bbbb")]
+        [TestCase("NewObject Catalogue '\"fff\"'","NewObject CatalogueItem 'Catalogue:\"fff\"' 'bbbb'","bbbb")]
+        public void RdmpScriptNewCatalogueItem_BasicName(string cataCommand,string cataItemCommand, string expectedCataItemName)
+        {
+            foreach(var c in RepositoryLocator.CatalogueRepository.GetAllObjects<Catalogue>())
+                c.DeleteInDatabase();
 
+            var runner = new ExecuteCommandRunner(new ExecuteCommandOptions()
+            {
+                Script = new RdmpScript()
+                {
+                    Commands = new[]
+                    {
+                        cataCommand,
+                        cataItemCommand
+                    }
+                }
+            });
+            
+            SetupMEF();
+
+            var exitCode = runner.Run(RepositoryLocator, new ThrowImmediatelyDataLoadEventListener(), new ThrowImmediatelyCheckNotifier(), new GracefulCancellationToken());
+
+            Assert.AreEqual(0,exitCode);
+            Assert.AreEqual(1,RepositoryLocator.CatalogueRepository.GetAllObjects<Catalogue>().Length);
+            var ci = RepositoryLocator.CatalogueRepository.GetAllObjects<Catalogue>().Single().CatalogueItems.Single();
+
+            Assert.AreEqual(expectedCataItemName,ci.Name);
+            
         }
     }
 }
