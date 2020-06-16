fe05dfa1b33946e73b9e87eda55a72b06123e36c
tznind <t.z.nind@dundee.ac.uk>
2020-06-16 11:58:20 +0100

    Added 2 catalogue test
    with foreach CatalogueItems

--- Rdmp.Core.Tests/Reports/CustomMetadataReportTests.cs
+++ Rdmp.Core.Tests/Reports/CustomMetadataReportTests.cs
@@ -215,6 +215,80 @@ A cool dataset with interesting stuff
         }
 
         [Test]
+        public void TestCustomMetadataReport_TwoCataloguesWithItems()
+        {
+            var c1 = WhenIHaveA<Catalogue>();
+            c1.Name = "ffff";
+            c1.Description = "A cool dataset with interesting stuff";
+            c1.SaveToDatabase();
+
+            var c1ci1 = new CatalogueItem(RepositoryLocator.CatalogueRepository, c1, "Col1");
+            c1ci1.Description = "some info about column 1";
+            c1ci1.SaveToDatabase();
+
+            var c1ci2 = new CatalogueItem(RepositoryLocator.CatalogueRepository, c1, "Col2");
+            c1ci2.Description = "some info about column 2";
+            c1ci2.SaveToDatabase();
+
+            
+            var c2 = WhenIHaveA<Catalogue>();
+            c2.Name = "Demog";
+            c2.Description = "This is expensive dataset: $30 to use";
+            c2.SaveToDatabase();
+
+            var c2ci1 = new CatalogueItem(RepositoryLocator.CatalogueRepository, c2, "Name");
+            c2ci1.Description = "Name of the patient";
+            c2ci1.SaveToDatabase();
+            var c2ci2 = new CatalogueItem(RepositoryLocator.CatalogueRepository, c2, "Address");
+            c2ci2.Description = "Where they live";
+            c2ci2.SaveToDatabase();
+            var c2ci3 = new CatalogueItem(RepositoryLocator.CatalogueRepository, c2, "Postcode");
+            c2ci3.Description = "Patients postcode";
+            c2ci3.SaveToDatabase();
+
+
+            var template = new FileInfo(Path.Combine(TestContext.CurrentContext.WorkDirectory, "template.md"));
+            var outDir = new DirectoryInfo(Path.Combine(TestContext.CurrentContext.WorkDirectory, "outDir"));
+
+            if(outDir.Exists)
+                outDir.Delete(true);
+            
+            outDir.Create();
+
+            File.WriteAllText(template.FullName,
+                @"## $Name
+$Description
+Price: $30
+| Column | Description |
+$foreach CatalogueItem
+
+| $Name | $Description |
+$end");
+
+            var cmd = new ExecuteCommandExtractMetadata(null, new[] {c1,c2}, outDir, template, "Datasets.md", true);
+            cmd.Execute();
+
+            var outFile = Path.Combine(outDir.FullName, "Datasets.md");
+
+            FileAssert.Exists(outFile);
+            var resultText = File.ReadAllText(outFile);
+
+            StringAssert.AreEqualIgnoringCase(@"## ffff
+A cool dataset with interesting stuff
+Price: $30
+| Column | Description |
+| Col1 | some info about column 1 |
+| Col2 | some info about column 2 |
+## Demog
+This is expensive dataset: $30 to use
+Price: $30
+| Column | Description |
+| Name | Name of the patient |
+| Address | Where they live |
+| Postcode | Patients postcode |",resultText.TrimEnd());
+        }
+
+        [Test]
         public void TestCustomMetadataReport_CatalogueItems_NoEndBlock()
         {
             var cata = WhenIHaveA<Catalogue>();
