fff2cc2da8b33355b8fe4254ddfae5eb2e8fc9eb
tznind <t.z.nind@dundee.ac.uk>
2020-06-24 12:27:40 +0100

    Standardised GoTo and made tab context menu rebuild after refresh

--- Application/ResearchDataManagementPlatform/WindowManagement/WindowFactory.cs
+++ Application/ResearchDataManagementPlatform/WindowManagement/WindowFactory.cs
@@ -4,6 +4,7 @@
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 using MapsDirectlyToDatabaseTable;
@@ -129,7 +130,27 @@ namespace ResearchDataManagementPlatform.WindowManagement
             var tab = content as RDMPSingleControlTab;
 
             if (tab != null)
+            {
                 content.TabPageContextMenuStrip = new RDMPSingleControlTabMenu(activator, tab, _windowManager);
+
+                //Create handler for AfterPublish
+                RefreshObjectEventHandler handler = null;
+                handler = (s,e)=>
+                { 
+                    // After global changes, rebuild the context menu
+
+                    if(!content.IsDisposed)
+                        content.TabPageContextMenuStrip = new RDMPSingleControlTabMenu(activator, tab, _windowManager);
+                    else
+                        if(handler != null)
+                            activator.RefreshBus.AfterPublish -= handler; //don't leak handlers
+                };
+
+                //register the event handler
+                activator.RefreshBus.AfterPublish += handler;
+                
+            }
+                
         }
     }
 }
