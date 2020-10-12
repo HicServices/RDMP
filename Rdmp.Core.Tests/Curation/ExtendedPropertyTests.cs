90a2ab7da438da4875a23b3d96dc60b8a532ada9
tznind <t.z.nind@dundee.ac.uk>
2020-10-12 07:40:00 +0100

    Added ExtendedProperty class and database schema

---
+++ Rdmp.Core.Tests/Curation/ExtendedPropertyTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Rdmp.Core.Curation.Data;
+using Tests.Common;
+
+namespace Rdmp.Core.Tests.Curation
+{
+    class ExtendedPropertyTests : DatabaseTests
+    {
+        [Test]
+        public void ExtendedProperty_Catalogue()
+        {
+            var cata = new Catalogue(CatalogueRepository,"My cata");
+            var prop = new ExtendedProperty(CatalogueRepository,cata,"Fish",5);
+
+            Assert.AreEqual(5,prop.GetValueAsSystemType());
+            Assert.IsTrue(prop.IsReferenceTo(cata));
+
+            prop.SetValue(10);
+            prop.SaveToDatabase();
+            
+            Assert.AreEqual(10,prop.GetValueAsSystemType());
+            Assert.IsTrue(prop.IsReferenceTo(cata));
+        }
+    }
+}
