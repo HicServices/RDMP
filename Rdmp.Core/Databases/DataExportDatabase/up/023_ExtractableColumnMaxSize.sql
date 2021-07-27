0b917364952713ad490aa7120a0e55e026ad9719
Thomas Nind <TZNind@dundee.ac.uk>
2021-07-27 10:58:02 +0100

    Removed limit of 500 characters on extraction SQL of columns

---
+++ Rdmp.Core/Databases/DataExportDatabase/up/023_ExtractableColumnMaxSize.sql
@@ -0,0 +1,5 @@
+﻿--Version:5.0.1
+--Description: Changes ExtractableColumn SelectSQL to nvarchar(max) to match the size in Catalogue database
+
+
+alter table [ExtractableColumn] alter column [SelectSQL] nvarchar(max) NULL
