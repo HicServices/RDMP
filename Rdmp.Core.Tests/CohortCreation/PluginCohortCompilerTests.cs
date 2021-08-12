753fa6850e5a160326a60dc65c38340869b38d6b
Thomas Nind <TZNind@dundee.ac.uk>
2021-08-12 18:05:09 +0100

    Added (failing) test for IPluginCohortCompiler

---
+++ Rdmp.Core.Tests/CohortCreation/PluginCohortCompilerTests.cs
@@ -0,0 +1,78 @@
+﻿using FAnsi.Discovery;
+using NUnit.Framework;
+using Rdmp.Core.CohortCommitting.Pipeline.Sources;
+using Rdmp.Core.CohortCreation;
+using Rdmp.Core.Curation.Data;
+using Rdmp.Core.Curation.Data.Aggregation;
+using Rdmp.Core.Curation.Data.Cohort;
+using Rdmp.Core.DataFlowPipeline;
+using Rdmp.Core.QueryCaching.Aggregation;
+using Rdmp.Core.QueryCaching.Aggregation.Arguments;
+using Rdmp.Core.Tests.CohortCreation.QueryTests;
+using ReusableLibraryCode.Progress;
+using System.Data;
+using Tests.Common;
+using TypeGuesser;
+
+namespace Rdmp.Core.Tests.CohortCreation
+{
+    class PluginCohortCompilerTests : CohortQueryBuilderWithCacheTests
+    {
+        [Test]
+        public void TestIPluginCohortCompiler_PopulatesCacheCorrectly()
+        {
+            // create a cohort config
+            var cic = new CohortIdentificationConfiguration(CatalogueRepository, "mycic");
+            cic.QueryCachingServer_ID = externalDatabaseServer.ID;
+            cic.SaveToDatabase();
+
+            // this special Catalogue will be detected by GenRandom and interpreted as an API call
+            var myApi = new Catalogue(CatalogueRepository,"myapi");
+
+            // create a use of the API as an AggregateConfiguration
+            var ac = new AggregateConfiguration(CatalogueRepository, myApi,"myac");
+
+            // add it to the cohort config
+            cic.CreateRootContainerIfNotExists();
+            cic.RootCohortAggregateContainer.AddChild(ac,0);
+
+            // run the cic
+            var source = new CohortIdentificationConfigurationSource();
+            source.PreInitialize(cic, new ThrowImmediatelyDataLoadEventListener());
+
+            var dt = source.GetChunk(new ThrowImmediatelyDataLoadEventListener(),new GracefulCancellationToken());
+
+            Assert.AreEqual(2, dt);
+
+            var results = new []{ (string)dt.Rows[0][0],(string)dt.Rows[1][0] };
+
+            Assert.Contains("0101010101", results);
+            Assert.Contains("0202020202", results);
+        }
+
+        class GenRandom : IPluginCohortCompiler
+        {
+            public void Run(AggregateConfiguration ac, CachedAggregateConfigurationResultsManager cache)
+            {
+                // simulate going to an API and getting 2 results
+                using var dt = new DataTable();
+                dt.Columns.Add("identifiers");
+
+                dt.Rows.Add("0101010101");
+                dt.Rows.Add("0202020202");
+
+
+                // this is how you commit the results to the cache
+                var args = new CacheCommitIdentifierList(ac,ac.Description??"none",dt,
+                    new DatabaseColumnRequest("identifiers",new DatabaseTypeRequest(typeof(string),10),false),5000);
+
+                cache.CommitResults(args);
+            }
+
+            public bool ShouldRun(AggregateConfiguration ac)
+            {
+                return ac.Catalogue.Name.Equals("myapi");
+            }
+        }
+    }
+}
---
+++ Rdmp.Core/CohortCreation/IPluginCohortCompiler.cs
@@ -0,0 +1,34 @@
+﻿using Rdmp.Core.Curation.Data.Aggregation;
+using Rdmp.Core.QueryCaching.Aggregation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.Core.CohortCreation
+{
+    /// <summary>
+    /// Interface for plugins that want to perform custom tasks when part of a cohort builder query is run
+    /// e.g. call out to an external API and store the resulting identifier list in the query cache
+    /// </summary>
+    public interface IPluginCohortCompiler
+    {
+        /// <summary>
+        /// Return true if the <paramref name="ac"/> is of a type that should be handled by your class.
+        /// All aggregates will regularly be passed to this when run so ensure that your response is fast
+        /// </summary>
+        /// <param name="ac"></param>
+        /// <returns></returns>
+        bool ShouldRun(AggregateConfiguration ac);
+
+
+        /// <summary>
+        /// Must be implemented such that by the time the method completes the <paramref name="cache"/> 
+        /// is populated with an identifier list that matches the expectations of <paramref name="ac"/>
+        /// </summary>
+        /// <param name="ac"></param>
+        /// <param name="cache"></param>
+        void Run(AggregateConfiguration ac, CachedAggregateConfigurationResultsManager cache);
+    }
+}
