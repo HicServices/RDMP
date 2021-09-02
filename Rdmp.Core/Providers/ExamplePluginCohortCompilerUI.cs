3fcb357f40eb6d98ec7607d58a050a2fda3382fd
James A Sutherland <j@sutherland.pw>
2021-09-02 11:40:50 -0500

    Streamline prelude logic slightly

--- Rdmp.Core/Providers/ExamplePluginCohortCompilerUI.cs
+++ Rdmp.Core/Providers/ExamplePluginCohortCompilerUI.cs
@@ -24,39 +24,34 @@ namespace Rdmp.Core.Providers
         public override bool CustomActivate(IMapsDirectlyToDatabaseTable o)
         {
             // we only care about responding to opening AggregateConfiguration objects
-            if (o is not AggregateConfiguration ac)
+            // and then only if the aggregate being edited is one of the ones that our API handles
+            if (o is not AggregateConfiguration ac || !compiler.ShouldRun(ac))
             {
                 return false;
             }
 
-            // If the aggregate being edited is one of the ones that our API handles
-            if (compiler.ShouldRun(ac))
+            // Look at the Description property for a number (your API could use a complex XML or YAML syntax if you want)
+            // You could also store some info at the Catalogue level for reuse in other Aggregates
+
+            if (!int.TryParse(ac.Description, out var number))
             {
-                int number;
-
-                // Look at the Description property for a number (your API could use a complex XML or YAML syntax if you want)
-                // You could also store some info at the Catalogue level for reuse in other Aggregates
-
-                if (!int.TryParse(ac.Description, out number))
-                {
-                    number = 5;
-                }
-
-                // Launch a UI that prompts a new value to be entered
-                if (BasicActivator.TypeText("Generate random CHIs", "Number of Chis:", 100, number.ToString(), out string result, false))
-                {
-                    if (int.TryParse(result, out int newCount))
-                    {
-                        ac.Description = newCount.ToString();
-                        ac.SaveToDatabase();
-                    }
-                }
-
-                // we handled this, don't launch the default user interface
-                return true;
+                number = 5;
             }
 
-            return false;
+            // Launch a UI that prompts a new value to be entered
+            if (BasicActivator.TypeText("Generate random CHIs", "Number of Chis:", 100, number.ToString(),
+                out string result, false))
+            {
+                if (int.TryParse(result, out int newCount))
+                {
+                    ac.Description = newCount.ToString();
+                    ac.SaveToDatabase();
+                }
+            }
+
+            // we handled this, don't launch the default user interface
+            return true;
+
         }
     }
 }
