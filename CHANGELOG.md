8e9570fb6b815e4eafd8717f964a5d7ece960403
Thomas Nind <TZNind@dundee.ac.uk>
2021-09-27 14:34:59 +0100

    Fixed CohortAggregateContainer not appearing properly in find dialog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Renamed `SelectIMapsDirectlyToDatabaseTableDialog` to `SelectDialog<T>` (now supports any object Type)
 - Selected datasets icon now includes all symbols of the Catalogue they represent (e.g. ProjectSpecific, Internal)
 - Changed how RDMP treats cohorts where the data has been deleted from the cohort table.  'Broken Cohort' renamed 'Orphan Cohort' and made more stable
+- [CohortAggregateContainer] now show up in the find dialog (you can disable this in UserSettings)
 
 ### Removed
 
@@ -50,6 +51,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Fixed various issues when viewing the DQE results of a run on an empty table
 - DatasetRaceway in dashboards now shows 'Table(s) were empty for...' instead of `No DQE Evaluation for...` when the DQE was run but there was no result set
 - Added better error message when trying to create a new RDMP platform database into an existing database that already has one set up
+- Fixed [CohortAggregateContainer] and filter containers not showing up in Find when explicitly requested
 
 ## [6.0.2] - 2021-08-26
 
@@ -948,3 +950,4 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 [CohortIdentificationConfiguration]: ./Documentation/CodeTutorials/Glossary.md#CohortIdentificationConfiguration
 [LoadMetadata]: ./Documentation/CodeTutorials/Glossary.md#LoadMetadata
 [ExtractableCohort]: ./Documentation/CodeTutorials/Glossary.md#ExtractableCohort
+[CohortAggregateContainer]: ./Documentation/CodeTutorials/Glossary.md#CohortAggregateContainer
---
+++ Rdmp.Core.Tests/Providers/SearchablesMatchScorerTests.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) The University of Dundee 2018-2019
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
+using NUnit.Framework;
+using Rdmp.Core.Curation.Data;
+using Rdmp.Core.Curation.Data.Cohort;
+using Rdmp.Core.DataExport.Data;
+using Rdmp.Core.Providers;
+using ReusableLibraryCode.Checks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Tests.Common;
+
+namespace Rdmp.Core.Tests.Providers
+{
+    class SearchablesMatchScorerTests : UnitTests
+    {
+        [Test]
+        public void Find_ExactMatch_ScoresHigher()
+        {
+            var cata = WhenIHaveA<Catalogue>();
+            cata.Name = "FF";
+            var proj = WhenIHaveA<Project>();
+            proj.Name = "FFFF";
+
+            var scorer = new SearchablesMatchScorer();
+
+            var childProvider = new DataExportChildProvider(RepositoryLocator, null, new ThrowImmediatelyCheckNotifier(), null);
+            var scores = scorer.ScoreMatches(childProvider.GetAllSearchables(), "FF", CancellationToken.None, new List<Type>());
+
+            var cataScore = scores.Single(d => Equals(d.Key.Key, cata));
+            var projScore = scores.Single(d => Equals(d.Key.Key, proj));
+
+            // Both score because they have the text FF
+            Assert.Greater(cataScore.Value, 0);
+            Assert.Greater(projScore.Value, 0);
+
+            // Catalogue scores higher because it is an exact match to the name
+            Assert.Greater(cataScore.Value, projScore.Value);
+        }
+
+
+        [Test]
+        public void Find_CohortAggregateContainer()
+        {
+            var container = WhenIHaveA<CohortAggregateContainer>();
+
+            var scorer = new SearchablesMatchScorer();
+            scorer.TypeNames.Add("CohortAggregateContainer");
+
+            var childProvider = new DataExportChildProvider(RepositoryLocator, null, new ThrowImmediatelyCheckNotifier(), null);
+
+            var scores = scorer.ScoreMatches(childProvider.GetAllSearchables(),"", CancellationToken.None, new List<Type>() { typeof(CohortAggregateContainer)});
+
+            var score = scores.Single(d => Equals(d.Key.Key, container));
+            Assert.Greater(score.Value, 0);
+        }
+    }
+}
