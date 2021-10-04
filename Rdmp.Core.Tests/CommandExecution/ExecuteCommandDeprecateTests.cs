fc289fac2195f21ec5192a3a9a4d5df1de796dee
Thomas Nind <TZNind@dundee.ac.uk>
2021-10-04 11:03:54 +0100

    Test for Deprecate command

---
+++ Rdmp.Core.Tests/CommandExecution/ExecuteCommandDeprecateTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Rdmp.Core.Curation.Data;
+using Rdmp.Core.DataExport.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.Core.Tests.CommandExecution
+{
+    class ExecuteCommandDeprecateTests : CommandCliTests
+    {
+        [Test]
+        public void TestDeprecateCommand()
+        {
+            var c = WhenIHaveA<Catalogue>();
+
+            Assert.IsFalse(c.IsDeprecated);
+
+            Run("Deprecate", $"Catalogue:{c.ID}");
+
+            Assert.IsTrue(c.IsDeprecated);
+        }
+    }
+}
