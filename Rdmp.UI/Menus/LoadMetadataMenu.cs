53a39b49874b70d7927b051128404bd96b450a6e
Thomas Nind <TZNind@dundee.ac.uk>
2021-10-04 10:32:31 +0100

    Fixed new command appearing in gui client

--- Rdmp.UI/Menus/LoadMetadataMenu.cs
+++ Rdmp.UI/Menus/LoadMetadataMenu.cs
@@ -4,7 +4,6 @@
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
-using Rdmp.Core.CommandExecution.AtomicCommands;
 using Rdmp.Core.Curation.Data.DataLoad;
 using Rdmp.Core.Icons.IconProvision;
 using Rdmp.UI.CommandExecution.AtomicCommands;
@@ -17,8 +16,6 @@ namespace Rdmp.UI.Menus
     {
         public LoadMetadataMenu(RDMPContextMenuStripArgs args, LoadMetadata loadMetadata) : base(args, loadMetadata)
         {
-            args.SkipCommand<ExecuteCommandCreateNewDataLoadDirectory>();
-
             Add(new ExecuteCommandEditLoadMetadataDescription(_activator, loadMetadata));
             Add(new ExecuteCommandViewLoadDiagram(_activator, loadMetadata));
             ReBrandActivateAs("Check and Execute",RDMPConcept.LoadMetadata,OverlayKind.Execute);
