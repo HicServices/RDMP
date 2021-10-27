f8e1d8973f28719109330567673485be720a4af1
Thomas Nind <TZNind@dundee.ac.uk>
2021-10-27 14:18:53 +0100

    Fixed ArchivalDataLoadInfo ToString when tasks ran for more than 24 hours

---
+++ Rdmp.Core.Tests/Logging/ArchivalDataLoadInfoTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Rdmp.Core.Logging.PastEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.Core.Tests.Logging
+{
+    class ArchivalDataLoadInfoTests
+    {
+        [Test]
+        public void ArchivalDataLoadInfo_ToString()
+        {
+            var adi = new ArchivalDataLoadInfo();
+            adi.StartTime = new DateTime(2010, 1, 1,12,0,0);
+            adi.EndTime = new DateTime(2010, 1, 3,13,20,23);
+
+            // This dle took 2 days, 1 hour, 20 mins and 23 seconds
+            StringAssert.Contains("(49:20:23)", adi.ToString());
+        }
+    }
+}
