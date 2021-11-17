81ba14f3e5dd96e2b54a7f8b459a3abc77d3f808
Thomas Nind <TZNind@dundee.ac.uk>
2021-11-17 13:11:06 +0000

    Changed PipelineWorkArea to use Dock instead of Anchor

--- Rdmp.UI/PipelineUIs/Pipelines/PipelineWorkAreaUI.Designer.cs
+++ Rdmp.UI/PipelineUIs/Pipelines/PipelineWorkAreaUI.Designer.cs
@@ -34,26 +34,26 @@ namespace Rdmp.UI.PipelineUIs.Pipelines
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.label3 = new System.Windows.Forms.Label();
             this.olvComponents = new BrightIdeasSoftware.ObjectListView();
-            this.olvName = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvNamespace = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvRole = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvCompatible = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvName = new BrightIdeasSoftware.OLVColumn();
+            this.olvNamespace = new BrightIdeasSoftware.OLVColumn();
+            this.olvRole = new BrightIdeasSoftware.OLVColumn();
+            this.olvCompatible = new BrightIdeasSoftware.OLVColumn();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.gbArguments = new System.Windows.Forms.GroupBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.diagramPanel = new System.Windows.Forms.Panel();
             this.btnReRunChecks = new System.Windows.Forms.Button();
-            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.gbArguments = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.olvComponents)).BeginInit();
-            this.diagramPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
             this.splitContainer2.Panel1.SuspendLayout();
             this.splitContainer2.Panel2.SuspendLayout();
             this.splitContainer2.SuspendLayout();
+            this.diagramPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -61,31 +61,33 @@ namespace Rdmp.UI.PipelineUIs.Pipelines
             this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.label3);
             this.splitContainer1.Panel1.Controls.Add(this.olvComponents);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
             this.splitContainer1.Panel1.Controls.Add(this.label1);
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
             this.splitContainer1.Panel2.Controls.Add(this.label2);
-            this.splitContainer1.Size = new System.Drawing.Size(1125, 539);
-            this.splitContainer1.SplitterDistance = 374;
+            this.splitContainer1.Size = new System.Drawing.Size(1312, 622);
+            this.splitContainer1.SplitterDistance = 436;
+            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 0;
             // 
             // label3
             // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
             this.label3.ForeColor = System.Drawing.Color.Brown;
-            this.label3.Location = new System.Drawing.Point(66, 5);
+            this.label3.Location = new System.Drawing.Point(0, 18);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(301, 16);
+            this.label3.Size = new System.Drawing.Size(432, 18);
             this.label3.TabIndex = 2;
             this.label3.Text = "(Add Components from below with Drag and Drop)";
             // 
@@ -95,9 +97,6 @@ namespace Rdmp.UI.PipelineUIs.Pipelines
             this.olvComponents.AllColumns.Add(this.olvNamespace);
             this.olvComponents.AllColumns.Add(this.olvRole);
             this.olvComponents.AllColumns.Add(this.olvCompatible);
-            this.olvComponents.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.olvComponents.CellEditUseWholeCell = false;
             this.olvComponents.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.olvName,
@@ -105,10 +104,13 @@ namespace Rdmp.UI.PipelineUIs.Pipelines
             this.olvRole,
             this.olvCompatible});
             this.olvComponents.Cursor = System.Windows.Forms.Cursors.Default;
+            this.olvComponents.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.olvComponents.HideSelection = false;
             this.olvComponents.IsSimpleDragSource = true;
-            this.olvComponents.Location = new System.Drawing.Point(3, 24);
+            this.olvComponents.Location = new System.Drawing.Point(0, 36);
+            this.olvComponents.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.olvComponents.Name = "olvComponents";
-            this.olvComponents.Size = new System.Drawing.Size(364, 508);
+            this.olvComponents.Size = new System.Drawing.Size(432, 582);
             this.olvComponents.TabIndex = 1;
             this.olvComponents.UseCompatibleStateImageBehavior = false;
             this.olvComponents.View = System.Windows.Forms.View.Details;
@@ -118,8 +120,9 @@ namespace Rdmp.UI.PipelineUIs.Pipelines
             // 
             this.olvName.AspectName = "ToString";
             this.olvName.Groupable = false;
-            this.olvName.Text = "Name";
             this.olvName.MinimumWidth = 100;
+            this.olvName.Text = "Name";
+            this.olvName.Width = 100;
             // 
             // olvNamespace
             // 
@@ -143,64 +146,20 @@ namespace Rdmp.UI.PipelineUIs.Pipelines
             // label1
             // 
             this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.Size = new System.Drawing.Size(432, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Components";
             // 
-            // label2
-            // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.label2.Location = new System.Drawing.Point(3, 5);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(737, 16);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Current Pipeline Configuration";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // gbArguments
-            // 
-            this.gbArguments.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gbArguments.Location = new System.Drawing.Point(0, 0);
-            this.gbArguments.Name = "gbArguments";
-            this.gbArguments.Size = new System.Drawing.Size(733, 246);
-            this.gbArguments.TabIndex = 5;
-            this.gbArguments.TabStop = false;
-            this.gbArguments.Text = "Arguments:";
-            // 
-            // diagramPanel
-            // 
-            this.diagramPanel.AutoSize = true;
-            this.diagramPanel.Controls.Add(this.btnReRunChecks);
-            this.diagramPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.diagramPanel.Location = new System.Drawing.Point(0, 0);
-            this.diagramPanel.MinimumSize = new System.Drawing.Size(0, 110);
-            this.diagramPanel.Name = "diagramPanel";
-            this.diagramPanel.Size = new System.Drawing.Size(733, 250);
-            this.diagramPanel.TabIndex = 0;
-            this.diagramPanel.Text = "pipelineDiagram";
-            // 
-            // btnReRunChecks
-            // 
-            this.btnReRunChecks.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnReRunChecks.Location = new System.Drawing.Point(629, 224);
-            this.btnReRunChecks.Name = "btnReRunChecks";
-            this.btnReRunChecks.Size = new System.Drawing.Size(101, 23);
-            this.btnReRunChecks.TabIndex = 0;
-            this.btnReRunChecks.Text = "Re-Run Checks";
-            this.btnReRunChecks.UseVisualStyleBackColor = true;
-            this.btnReRunChecks.Click += new System.EventHandler(this.btnReRunChecks_Click);
-            // 
             // splitContainer2
             // 
-            this.splitContainer2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.splitContainer2.Location = new System.Drawing.Point(3, 24);
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 18);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.splitContainer2.Name = "splitContainer2";
             this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -211,28 +170,79 @@ namespace Rdmp.UI.PipelineUIs.Pipelines
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.gbArguments);
-            this.splitContainer2.Size = new System.Drawing.Size(737, 508);
-            this.splitContainer2.SplitterDistance = 254;
+            this.splitContainer2.Size = new System.Drawing.Size(871, 604);
+            this.splitContainer2.SplitterDistance = 302;
+            this.splitContainer2.SplitterWidth = 5;
             this.splitContainer2.TabIndex = 7;
             // 
-            // PipelineWorkArea
+            // diagramPanel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.diagramPanel.AutoSize = true;
+            this.diagramPanel.Controls.Add(this.btnReRunChecks);
+            this.diagramPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.diagramPanel.Location = new System.Drawing.Point(0, 0);
+            this.diagramPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.diagramPanel.MinimumSize = new System.Drawing.Size(0, 127);
+            this.diagramPanel.Name = "diagramPanel";
+            this.diagramPanel.Size = new System.Drawing.Size(867, 298);
+            this.diagramPanel.TabIndex = 0;
+            this.diagramPanel.Text = "pipelineDiagram";
+            // 
+            // btnReRunChecks
+            // 
+            this.btnReRunChecks.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReRunChecks.Location = new System.Drawing.Point(746, 268);
+            this.btnReRunChecks.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnReRunChecks.Name = "btnReRunChecks";
+            this.btnReRunChecks.Size = new System.Drawing.Size(118, 27);
+            this.btnReRunChecks.TabIndex = 0;
+            this.btnReRunChecks.Text = "Re-Run Checks";
+            this.btnReRunChecks.UseVisualStyleBackColor = true;
+            this.btnReRunChecks.Click += new System.EventHandler(this.btnReRunChecks_Click);
+            // 
+            // gbArguments
+            // 
+            this.gbArguments.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbArguments.Location = new System.Drawing.Point(0, 0);
+            this.gbArguments.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.gbArguments.Name = "gbArguments";
+            this.gbArguments.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.gbArguments.Size = new System.Drawing.Size(867, 293);
+            this.gbArguments.TabIndex = 5;
+            this.gbArguments.TabStop = false;
+            this.gbArguments.Text = "Arguments:";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(871, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Current Pipeline Configuration";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // PipelineWorkAreaUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.splitContainer1);
-            this.Name = "PipelineWorkArea";
-            this.Size = new System.Drawing.Size(1125, 539);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "PipelineWorkAreaUI";
+            this.Size = new System.Drawing.Size(1312, 622);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.olvComponents)).EndInit();
-            this.diagramPanel.ResumeLayout(false);
             this.splitContainer2.Panel1.ResumeLayout(false);
             this.splitContainer2.Panel1.PerformLayout();
             this.splitContainer2.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
             this.splitContainer2.ResumeLayout(false);
+            this.diagramPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
