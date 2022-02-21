618449139d10dfa25c29574c3f36170f3009a952
James A Sutherland <j@sutherland.pw>
2022-02-21 16:47:11 +0000

    Revert erroneous commit

--- Rdmp.UI/MainFormUITabs/ExtractionProgressUI.cs
+++ Rdmp.UI/MainFormUITabs/ExtractionProgressUI.cs
@@ -19,138 +19,143 @@ using System.ComponentModel;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace Rdmp.UI.MainFormUITabs;
-
-public partial class ExtractionProgressUI : ExtractionProgressUI_Design, ISaveableUI
+namespace Rdmp.UI.MainFormUITabs
 {
-    public ExtractionProgress ExtractionProgress => (ExtractionProgress)DatabaseObject;
-    public IDetermineDatasetTimespan TimespanCalculator { get; set; } = new DatasetTimespanCalculator();
+    public partial class ExtractionProgressUI : ExtractionProgressUI_Design, ISaveableUI
+    {
+        public ExtractionProgress ExtractionProgress { get => (ExtractionProgress)DatabaseObject; }
+        public IDetermineDatasetTimespan TimespanCalculator { get; set; } = new DatasetTimespanCalculator();
         
-    private Tuple<DateTime?, DateTime?> dqeResult;
+        private Tuple<DateTime?, DateTime?> dqeResult;
 
-    public ExtractionProgressUI()
-    {
-        InitializeComponent();
-        var tt = new ToolTip();
-        tt.SetToolTip(btnFromDQE, "Populate Start and End dates according to the latest DQE results");
-        ddRetry.DataSource = Enum.GetValues(typeof(RetryStrategy));
-    }
-
-    protected override void SetBindings(BinderWithErrorProviderFactory rules, ExtractionProgress databaseObject)
-    {
-        base.SetBindings(rules, databaseObject);
-
-        Bind(tbID, "Text", "ID", d => d.ID);
-        Bind(tbDaysPerBatch, "Text", "NumberOfDaysPerBatch", d => d.NumberOfDaysPerBatch);
-    }
-
-    public override void SetDatabaseObject(IActivateItems activator, ExtractionProgress databaseObject)
-    {
-        base.SetDatabaseObject(activator, databaseObject);
-
-        var result = TimespanCalculator.GetMachineReadableTimespanIfKnownOf(databaseObject.ExtractionInformation.CatalogueItem.Catalogue, false, out DateTime? date);
-
-        btnFromDQE.Image = activator.CoreIconProvider.GetImage(RDMPConcept.DQE, OverlayKind.Import);
-
-        if (date != null)
+        public ExtractionProgressUI()
         {
-            lblEvaluationDate.Text = $"(DQE Run on {date})";
-            btnFromDQE.Enabled = true;
-            dqeResult = result;
+            InitializeComponent();
+            var tt = new ToolTip();
+            tt.SetToolTip(btnFromDQE, "Populate Start and End dates according to the latest DQE results");
+            ddRetry.DataSource = Enum.GetValues(typeof(RetryStrategy));
         }
-        else
+
+        protected override void SetBindings(BinderWithErrorProviderFactory rules, ExtractionProgress databaseObject)
         {
-            lblEvaluationDate.Text = "(DQE has not been run)";
-            btnFromDQE.Enabled = false;
+            base.SetBindings(rules, databaseObject);
+
+            Bind(tbID, "Text", "ID", d => d.ID);
+            Bind(tbDaysPerBatch, "Text", "NumberOfDaysPerBatch", d => d.NumberOfDaysPerBatch);
         }
+
+        public override void SetDatabaseObject(IActivateItems activator, ExtractionProgress databaseObject)
+        {
+            base.SetDatabaseObject(activator, databaseObject);
+
+            var result = TimespanCalculator.GetMachineReadableTimespanIfKnownOf(databaseObject.ExtractionInformation.CatalogueItem.Catalogue, false, out DateTime? date);
+
+            btnFromDQE.Image = activator.CoreIconProvider.GetImage(RDMPConcept.DQE, OverlayKind.Import);
+
+            if (date != null)
+            {
+                lblEvaluationDate.Text = $"(DQE Run on {date})";
+                btnFromDQE.Enabled = true;
+                dqeResult = result;
+            }
+            else
+            {
+                lblEvaluationDate.Text = $"(DQE has not been run)";
+                btnFromDQE.Enabled = false;
+            }
             
 
-        tbStartDate.Text = databaseObject.StartDate == null ? "" :databaseObject.StartDate.Value.ToString("yyyy-MM-dd") ?? "";
-        tbEndDate.Text = databaseObject.EndDate == null ? "" : databaseObject.EndDate.Value.ToString("yyyy-MM-dd");
-        tbProgress.Text = databaseObject.ProgressDate == null ? "" : databaseObject.ProgressDate.Value.ToString("yyyy-MM-dd");
+            tbStartDate.Text = databaseObject.StartDate == null ? "" :databaseObject.StartDate.Value.ToString("yyyy-MM-dd") ?? "";
+            tbEndDate.Text = databaseObject.EndDate == null ? "" : databaseObject.EndDate.Value.ToString("yyyy-MM-dd");
+            tbProgress.Text = databaseObject.ProgressDate == null ? "" : databaseObject.ProgressDate.Value.ToString("yyyy-MM-dd");
 
-        ddRetry.SelectedItem = databaseObject.Retry;
+            ddRetry.SelectedItem = databaseObject.Retry;
 
-        var cata = databaseObject.SelectedDataSets.GetCatalogue();
-        ddColumn.DataSource = cata.GetAllExtractionInformation();
+            var cata = databaseObject.SelectedDataSets.GetCatalogue();
+            ddColumn.DataSource = cata.GetAllExtractionInformation();
 
 
-        try
+            try
+            {
+                ddColumn.SelectedItem = databaseObject.ExtractionInformation;
+                ragSmiley1.Reset();
+                ExtractionProgress.ValidateSelectedColumn(ragSmiley1, databaseObject.ExtractionInformation);
+            }
+            catch (Exception)
+            {
+                // could be that the user has deleted this ExtractionInformation
+                ddColumn.SelectedItem = null;
+            }
+        }
+
+
+        private void tbDate_TextChanged(object sender, System.EventArgs e)
         {
-            ddColumn.SelectedItem = databaseObject.ExtractionInformation;
+            if(sender == tbStartDate)
+            {
+                SetDate(tbStartDate, (v) => ExtractionProgress.StartDate = v);
+            }
+
+            if (sender == tbEndDate)
+            {
+                SetDate(tbEndDate, (v) => ExtractionProgress.EndDate = v);
+            }
+
+            if (sender == tbProgress)
+            {
+                SetDate(tbProgress, (v) => ExtractionProgress.ProgressDate = v);
+            }
+
+        }
+
+        private void ddColumn_SelectionChangeCommitted(object sender, EventArgs e)
+        {
             ragSmiley1.Reset();
-            ExtractionProgress.ValidateSelectedColumn(ragSmiley1, databaseObject.ExtractionInformation);
+            if(ddColumn.SelectedItem is ExtractionInformation ei)
+            {
+                ExtractionProgress.ValidateSelectedColumn(ragSmiley1, ei);
+                ExtractionProgress.ExtractionInformation_ID = ei.ID;
+            }
         }
-        catch (Exception)
+
+        private void btnPickColumn_Click(object sender, EventArgs e)
         {
-            // could be that the user has deleted this ExtractionInformation
-            ddColumn.SelectedItem = null;
+            var col  = (ExtractionInformation)Activator.SelectOne("Column", ddColumn.Items.Cast<object>().OfType<ExtractionInformation>().ToArray());
+
+            if(col != null)
+            {
+                ddColumn.SelectedItem = col;
+                ragSmiley1.Reset();
+                ExtractionProgress.ValidateSelectedColumn(ragSmiley1, col);
+                ExtractionProgress.ExtractionInformation_ID = col.ID;
+            }
         }
-    }
 
-
-    private void tbDate_TextChanged(object sender, EventArgs e)
-    {
-        if(sender == tbStartDate)
+        private void btnFromDQE_Click(object sender, EventArgs e)
         {
-            SetDate(tbStartDate, v => ExtractionProgress.StartDate = v);
+            tbStartDate.Text = dqeResult.Item1?.ToString("yyyy-MM-dd");
+            tbEndDate.Text = dqeResult.Item2?.AddDays(1).ToString("yyyy-MM-dd");
         }
 
-        if (sender == tbEndDate)
+        private void btnResetProgress_Click(object sender, EventArgs e)
         {
-            SetDate(tbEndDate, v => ExtractionProgress.EndDate = v);
+            tbProgress.Text = "";
         }
 
-        if (sender == tbProgress)
+        private void ddRetry_SelectedIndexChanged(object sender, EventArgs e)
         {
-            SetDate(tbProgress, v => ExtractionProgress.ProgressDate = v);
+            if(ExtractionProgress == null)
+            {
+                return;
+            }
+            ExtractionProgress.Retry = (RetryStrategy)ddRetry.SelectedItem;
         }
-
     }
 
-    private void ddColumn_SelectionChangeCommitted(object sender, EventArgs e)
+
+    [TypeDescriptionProvider(typeof(AbstractControlDescriptionProvider<ExtractionProgressUI_Design, UserControl>))]
+    public abstract class ExtractionProgressUI_Design : RDMPSingleDatabaseObjectControl<ExtractionProgress>
     {
-        ragSmiley1.Reset();
-        if (ddColumn.SelectedItem is not ExtractionInformation ei) return;
-        ExtractionProgress.ValidateSelectedColumn(ragSmiley1, ei);
-        ExtractionProgress.ExtractionInformation_ID = ei.ID;
+
     }
-
-    private void btnPickColumn_Click(object sender, EventArgs e)
-    {
-        var col  = (ExtractionInformation)Activator.SelectOne("Column", ddColumn.Items.Cast<object>().OfType<ExtractionInformation>().ToArray());
-
-        if (col == null) return;
-        ddColumn.SelectedItem = col;
-        ragSmiley1.Reset();
-        ExtractionProgress.ValidateSelectedColumn(ragSmiley1, col);
-        ExtractionProgress.ExtractionInformation_ID = col.ID;
-    }
-
-    private void btnFromDQE_Click(object sender, EventArgs e)
-    {
-        tbStartDate.Text = dqeResult.Item1?.ToString("yyyy-MM-dd");
-        tbEndDate.Text = dqeResult.Item2?.AddDays(1).ToString("yyyy-MM-dd");
-    }
-
-    private void btnResetProgress_Click(object sender, EventArgs e)
-    {
-        tbProgress.Text = "";
-    }
-
-    private void ddRetry_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        if(ExtractionProgress == null)
-        {
-            return;
-        }
-        ExtractionProgress.Retry = (RetryStrategy)ddRetry.SelectedItem;
-    }
-}
-
-
-[TypeDescriptionProvider(typeof(AbstractControlDescriptionProvider<ExtractionProgressUI_Design, UserControl>))]
-public abstract class ExtractionProgressUI_Design : RDMPSingleDatabaseObjectControl<ExtractionProgress>
-{
-
 }
