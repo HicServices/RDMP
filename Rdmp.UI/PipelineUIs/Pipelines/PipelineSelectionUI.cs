a7c6cffdc0b1e99e1f5ff95a24c03b91ac6c565c
Thomas Nind <TZNind@dundee.ac.uk>
2022-03-23 13:09:50 +0000

    Added 'select pipeline' prompt when it is null

--- Rdmp.UI/PipelineUIs/Pipelines/PipelineSelectionUI.cs
+++ Rdmp.UI/PipelineUIs/Pipelines/PipelineSelectionUI.cs
@@ -116,15 +116,19 @@ namespace Rdmp.UI.PipelineUIs.Pipelines
         void cmb_Type_DrawItem(object sender, DrawItemEventArgs e)
         {
             e.DrawBackground();
-            if(e.Index == -1)
+            
+            var italic = new Font(ddPipelines.Font, FontStyle.Italic);
+
+            if (e.Index == -1)
             {
+                e.Graphics.FillRectangle(new SolidBrush(Color.Pink), e.Bounds);
+                TextRenderer.DrawText(e.Graphics, "Select Pipeline", italic, new Rectangle(new Point(e.Bounds.Left, e.Bounds.Top + 1), e.Bounds.Size), Color.Black, TextFormatFlags.Left);
                 return;
             }
 
             var render = ddPipelines.Items[e.Index].ToString();
             bool isIncompatible = e.Index > ddPipelines.Items.IndexOf(ShowAll);
 
-            var italic = new Font(ddPipelines.Font, FontStyle.Italic);
 
             if (Equals(ddPipelines.Items[e.Index],ShowAll))
             {
