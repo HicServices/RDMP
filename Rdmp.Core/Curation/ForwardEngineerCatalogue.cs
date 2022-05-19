HicServices/RDMP

commit 6a0f7fafde9ef6287f5575c14ec319c5a39bf105

New CatalogueItems are now automatically marked Core extractable Fixes #1165 Fixes #1164

Rdmp.Core/Curation/ForwardEngineerCatalogue.cs
@@ -17,19 +17,16 @@ namespace Rdmp.Core.Curation
     {
         private readonly ITableInfo _tableInfo;
         private readonly ColumnInfo[] _columnInfos;
-        private readonly bool _markAllExtractable;
 
         /// <summary>
         /// Sets up the class to create a new <see cref="Catalogue"/> from the supplied table reference
         /// </summary>
         /// <param name="tableInfo"></param>
         /// <param name="columnInfos"></param>
-        /// <param name="markAllExtractable"></param>
-        public ForwardEngineerCatalogue(ITableInfo tableInfo, ColumnInfo[] columnInfos, bool markAllExtractable = false)
+        public ForwardEngineerCatalogue(ITableInfo tableInfo, ColumnInfo[] columnInfos)
         {
             _tableInfo = tableInfo;
             _columnInfos = columnInfos;
-            _markAllExtractable = markAllExtractable;
         }
 
 
@@ -84,19 +81,11 @@ namespace Rdmp.Core.Curation
                 //create it with the same name
                 CatalogueItem cataItem = new CatalogueItem(repo, intoExistingCatalogue, col.Name.Substring(col.Name.LastIndexOf(".") + 1).Trim('[', ']', '`','"'));
                 catalogueItemsCreated.Add(cataItem);
-
-                if (_markAllExtractable)
-                {
-                    var newExtractionInfo = new ExtractionInformation(repo, cataItem, col, col.Name);
-                    newExtractionInfo.Order = order;
-                    newExtractionInfo.SaveToDatabase();
-                    extractionInformationsCreated.Add(newExtractionInfo);
-                }
-                else
-                {
-                    cataItem.ColumnInfo_ID =  col.ID;
-                    cataItem.SaveToDatabase();
-                }
+                                
+                var newExtractionInfo = new ExtractionInformation(repo, cataItem, col, col.Name);
+                newExtractionInfo.Order = order;
+                newExtractionInfo.SaveToDatabase();
+                extractionInformationsCreated.Add(newExtractionInfo);
             }
 
             extractionInformations = extractionInformationsCreated.ToArray();

Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandAddNewCatalogueItem.cs
@@ -60,7 +60,9 @@ namespace Rdmp.Core.CommandExecution.AtomicCommands
             var c = _catalogue;
             var existingColumnInfos = _existingColumnInfos;
 
-            if(c == null)
+            var repo = BasicActivator.RepositoryLocator.CatalogueRepository;
+
+            if (c == null)
             {
                 if (BasicActivator.SelectObject(new DialogArgs() {
                     WindowTitle = "Add CatalogueItem",
@@ -102,7 +104,11 @@ namespace Rdmp.Core.CommandExecution.AtomicCommands
 
                     //set the associated column if they did pick it
                     if(columnInfo != null)
+                    {
                         ci.SetColumnInfo(columnInfo);
+                        // also make extractable
+                        new ExtractionInformation(repo, ci, columnInfo, columnInfo.GetFullyQualifiedName());
+                    }
 
                     ci.SaveToDatabase();
 
@@ -117,9 +123,12 @@ namespace Rdmp.Core.CommandExecution.AtomicCommands
                     if(AlreadyInCatalogue(columnInfo, existingColumnInfos))
                         continue;
 
-                    var ci = new CatalogueItem(BasicActivator.RepositoryLocator.CatalogueRepository, c, columnInfo.GetRuntimeName());
+                    var ci = new CatalogueItem(repo, c, columnInfo.GetRuntimeName());
                     ci.SetColumnInfo(columnInfo);
                     ci.SaveToDatabase();
+
+                    // also make extractable
+                    new ExtractionInformation(repo, ci, columnInfo, columnInfo.GetFullyQualifiedName());
                 }
 
                 Publish(c);

Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandImportTableInfo.cs
@@ -59,7 +59,7 @@ namespace Rdmp.Core.CommandExecution.AtomicCommands
 
             if (_createCatalogue)
             {
-                var forwardEngineer = new ForwardEngineerCatalogue(ti, cis, true);
+                var forwardEngineer = new ForwardEngineerCatalogue(ti, cis);
                 forwardEngineer.ExecuteForwardEngineering(out c, out _, out _);
 
                 BasicActivator.Show($"Successfully imported new Catalogue { c.Name} with ID {c.ID}");

Rdmp.Core/CommandExecution/BasicActivateItems.cs
@@ -727,7 +727,7 @@ namespace Rdmp.Core.CommandExecution
         public virtual ICatalogue CreateAndConfigureCatalogue(ITableInfo tableInfo, ColumnInfo[] extractionIdentifierColumns, string initialDescription, IProject projectSpecific, string catalogueFolder)
         {
             // Create a new Catalogue based on the table info
-            var engineer = new ForwardEngineerCatalogue(tableInfo,tableInfo.ColumnInfos,true);
+            var engineer = new ForwardEngineerCatalogue(tableInfo,tableInfo.ColumnInfos);
             engineer.ExecuteForwardEngineering(out ICatalogue cata, out _, out ExtractionInformation[] eis);
 
             // if we know the linkable private identifier column(s)

Rdmp.Core/CommandLine/DatabaseCreation/ExampleDatasetsCreation.cs
@@ -581,7 +581,7 @@ UNPIVOT
         }
         private ICatalogue ImportCatalogue(ITableInfo ti)
         {
-            var forwardEngineer = new ForwardEngineerCatalogue(ti,ti.ColumnInfos,true);
+            var forwardEngineer = new ForwardEngineerCatalogue(ti,ti.ColumnInfos);
             forwardEngineer.ExecuteForwardEngineering(out var cata, out _,out ExtractionInformation[] eis);
             
             //get descriptions of the columns from BadMedicine

Rdmp.Core/Curation/TableInfoSynchronizer.cs
@@ -200,7 +200,7 @@ namespace Rdmp.Core.Curation
                         "Also make columns Extractable?")))
                     {
                         //Create CatalogueItems for the new columns
-                        ForwardEngineerCatalogue c = new ForwardEngineerCatalogue(_tableToSync, added.ToArray(), true);
+                        ForwardEngineerCatalogue c = new ForwardEngineerCatalogue(_tableToSync, added.ToArray());
 
                         //In the Catalogue
                         c.ExecuteForwardEngineering(relatedCatalogues[0],out var cata,out var cis, out var eis);

commit 678d8f50a9396fc9f1d68ed6c1bebf6fe632da9a

Added obsolete constructor signature to revert breaking change introduced in 6a0f7fafde9ef6287f5575c14ec319c5a39bf105

Rdmp.Core/Curation/ForwardEngineerCatalogue.cs
@@ -4,6 +4,7 @@
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
+using System;
 using System.Collections.Generic;
 using Rdmp.Core.Curation.Data;
 
@@ -17,6 +18,14 @@ namespace Rdmp.Core.Curation
     {
         private readonly ITableInfo _tableInfo;
         private readonly ColumnInfo[] _columnInfos;
+
+
+        [Obsolete("markAllExtractable is ignored, this constructor is included for API backwards compatibility only.")]
+        public ForwardEngineerCatalogue(ITableInfo tableInfo, ColumnInfo[] columnInfos, bool markAllExtractable)
+            : this(tableInfo, columnInfos)
+        {
+
+        }
 
         /// <summary>
         /// Sets up the class to create a new <see cref="Catalogue"/> from the supplied table reference