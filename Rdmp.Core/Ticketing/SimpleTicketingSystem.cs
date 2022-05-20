fe98a2822165fa06369ac54aacd1e4818857de2b
Thomas Nind <TZNind@dundee.ac.uk>
2022-05-20 13:40:32 +0100

    Added SimpleTicketSystem Fixes #775

---
+++ Rdmp.Core/Ticketing/SimpleTicketingSystem.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) The University of Dundee 2018-2019
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using ReusableLibraryCode;
+using ReusableLibraryCode.Checks;
+using ReusableLibraryCode.DataAccess;
+
+namespace Rdmp.Core.Ticketing
+{
+    public class SimpleTicketingSystem : ICheckable, ITicketingSystem
+    {
+        protected IDataAccessCredentials Credentials { get; set; }
+        protected string Url { get; set; }
+
+        protected SimpleTicketingSystem(TicketingSystemConstructorParameters parameters)
+        {
+            Credentials = parameters.Credentials;
+            Url = parameters.Url;
+        }
+
+        public void Check(ICheckNotifier notifier)
+        {
+            // all ticket names are valid
+            return;
+        }
+        public bool IsValidTicketName(string ticketName)
+        {
+            // all ticket names are valid
+            return true;
+        }
+        public void NavigateToTicket(string ticketName)
+        {
+            // if the user has added a URL just append the ticket name to it
+            // and open e.g. "www.myticketing?q=" + "HDD-123"
+            if(!string.IsNullOrWhiteSpace(Url))
+                UsefulStuff.OpenUrl(Url + ticketName);
+        }
+
+        public TicketingReleaseabilityEvaluation GetDataReleaseabilityOfTicket(string masterTicket, string requestTicket, string releaseTicket, out string reason, out Exception exception)
+        {
+            reason = null;
+            exception = null;
+            // No restrictions on releasability
+            return TicketingReleaseabilityEvaluation.Releaseable;
+        }
+        public string GetProjectFolderName(string masterTicket)
+        {
+            return UsefulStuff.RegexThingsThatAreNotNumbersOrLettersOrUnderscores.Replace(masterTicket, "");
+        }
+    }
+}
