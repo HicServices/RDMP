56642ad2c8b97ee209af0ee6999753adf2065e37
Thomas Nind <TZNind@dundee.ac.uk>
2022-06-24 14:23:27 +0100

    Changed default state of 'failed to execute top 1' to Warning

--- Reusable/ReusableLibraryCode/Checks/ErrorCodes.cs
+++ Reusable/ReusableLibraryCode/Checks/ErrorCodes.cs
@@ -29,7 +29,7 @@ namespace ReusableLibraryCode.Checks
         public static ErrorCode CohortAndExtractableDatasetsAreOnDifferentServers = new ErrorCode("R010","Cohort is on server '{0}' ({1}) but dataset '{2}' is on '{3}' ({4})", CheckResult.Warning);
         public static ErrorCode CouldNotReachCohort = new ErrorCode("R011", "Could not reach cohort '{0}' (it may be slow responding or inaccessible due to user permissions)",CheckResult.Warning);
         
-        public static ErrorCode ExtractionFailedToExecuteTop1 = new ErrorCode("R012", "Failed to execute Top 1 on dataset '{0}'", CheckResult.Fail);
+        public static ErrorCode ExtractionFailedToExecuteTop1 = new ErrorCode("R012", "Failed to execute Top 1 on dataset '{0}'", CheckResult.Warning);
         public static ErrorCode TextColumnsInExtraction = new ErrorCode("R013", "The following columns are data type ntext or text and so may be incompatible with the DISTINCT keyword({0}).  Ensure that PipelineSources are set to use extraction strategy 'OrderByAndDistinctInMemory' (ignore this message if you have already enabled this setting)", CheckResult.Warning);
 
         static ErrorCodes()
