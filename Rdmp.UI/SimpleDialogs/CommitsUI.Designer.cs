f5350f3f024109aaaeb2cbc30a5e6be2c4ec1d7c
tznind <tznind@dundee.ac.uk>
2022-08-12 15:40:32 +0100

    Added CommitsUI

---
+++ Rdmp.UI/SimpleDialogs/CommitsUI.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Rdmp.UI.SimpleDialogs
+{
+    partial class CommitsUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeListView1 = new BrightIdeasSoftware.TreeListView();
+            this.olvName = new BrightIdeasSoftware.OLVColumn();
+            this.olvUser = new BrightIdeasSoftware.OLVColumn();
+            this.olvDescription = new BrightIdeasSoftware.OLVColumn();
+            this.taskDescriptionLabel1 = new Rdmp.UI.SimpleDialogs.TaskDescriptionLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.treeListView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeListView1
+            // 
+            this.treeListView1.CellEditUseWholeCell = false;
+            this.treeListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.olvName,
+            this.olvUser,
+            this.olvDescription});
+            this.treeListView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeListView1.Location = new System.Drawing.Point(0, 42);
+            this.treeListView1.Name = "treeListView1";
+            this.treeListView1.ShowGroups = false;
+            this.treeListView1.Size = new System.Drawing.Size(800, 408);
+            this.treeListView1.TabIndex = 1;
+            this.treeListView1.View = System.Windows.Forms.View.Details;
+            this.treeListView1.VirtualMode = true;
+            // 
+            // olvName
+            // 
+            this.olvName.Text = "Name";
+            // 
+            // olvUser
+            // 
+            this.olvUser.Text = "User";
+            // 
+            // olvDescription
+            // 
+            this.olvDescription.Text = "Description";
+            this.olvDescription.Width = 92;
+            // 
+            // taskDescriptionLabel1
+            // 
+            this.taskDescriptionLabel1.AutoSize = true;
+            this.taskDescriptionLabel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.taskDescriptionLabel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.taskDescriptionLabel1.Location = new System.Drawing.Point(0, 0);
+            this.taskDescriptionLabel1.Name = "taskDescriptionLabel1";
+            this.taskDescriptionLabel1.Size = new System.Drawing.Size(800, 42);
+            this.taskDescriptionLabel1.TabIndex = 2;
+            // 
+            // CommitsUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeListView1);
+            this.Controls.Add(this.taskDescriptionLabel1);
+            this.Name = "CommitsUI";
+            this.Text = "CommitsUI";
+            ((System.ComponentModel.ISupportInitialize)(this.treeListView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private BrightIdeasSoftware.TreeListView treeListView1;
+        private BrightIdeasSoftware.OLVColumn olvName;
+        private BrightIdeasSoftware.OLVColumn olvUser;
+        private BrightIdeasSoftware.OLVColumn olvDescription;
+        private TaskDescriptionLabel taskDescriptionLabel1;
+    }
+}
---
+++ Rdmp.UI/SimpleDialogs/CommitsUI.cs
@@ -0,0 +1,36 @@
+﻿using MapsDirectlyToDatabaseTable;
+using Rdmp.Core.Curation.Data;
+using Rdmp.UI.ItemActivation;
+using Rdmp.UI.TestsAndSetup.ServicePropogation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Rdmp.UI.SimpleDialogs
+{
+    public partial class CommitsUI : RDMPForm
+    {
+        private List<Commit> _commits;
+
+        public CommitsUI(IActivateItems activator, IMapsDirectlyToDatabaseTable o)
+        {
+            InitializeComponent();
+
+            SetItemActivator(activator);
+
+            // TODO: move this to a helper class
+            var commitsInvolvingObject = activator.RepositoryLocator.CatalogueRepository
+                .GetAllObjectsWhere<Memento>(nameof(Memento.ReferencedObjectID), o.ID)
+                .Where((m) => m.IsReferenceTo(o))
+                .Select(m=>m.Commit_ID)
+                .Distinct()
+                .ToList();
+
+            _commits = activator.RepositoryLocator.CatalogueRepository
+                .GetAllObjectsInIDList<Commit>(commitsInvolvingObject)
+                .ToList();
+
+            treeListView1.AddObjects(_commits);
+        }
+    }
+}
