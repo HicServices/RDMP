7d8d80dc33c25232230b9f075cbf8eec6131b5c5
tznind <tznind@dundee.ac.uk>
2022-08-26 11:20:54 +0100

    Remove calls to `GETDATE()` in LoggingDatabasePatcher for MySql support

--- Rdmp.Core/Databases/LoggingDatabasePatcher.cs
+++ Rdmp.Core/Databases/LoggingDatabasePatcher.cs
@@ -230,8 +230,8 @@ INSERT INTO DataSet (dataSetID, name, description, time_period, SLA_required, su
 INSERT INTO DataSet (dataSetID, name, description, time_period, SLA_required, supplier_name, supplier_tel_no, supplier_email, contact_name, contact_position, currentContactInstitutions, contact_tel_no, contact_email, frequency, method) VALUES(N'Internal', 'Internal', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 /*create tasks*/
-INSERT INTO DataLoadTask(ID, description, name, createTime, userAccount, statusID, isTest, dataSetID) VALUES(1, 'Internal', 'Internal', GETDATE(), 'Thomas', 1, 0, 'Internal');
-INSERT INTO DataLoadTask (ID, description, name, createTime, userAccount, statusID, isTest, dataSetID) VALUES(2, 'DataExtraction', 'DataExtraction', GETDATE(), 'Thomas', 1, 0, 'DataExtraction');
+INSERT INTO DataLoadTask(ID, description, name, userAccount, statusID, isTest, dataSetID) VALUES(1, 'Internal', 'Internal', 'Thomas', 1, 0, 'Internal');
+INSERT INTO DataLoadTask (ID, description, name, userAccount, statusID, isTest, dataSetID) VALUES(2, 'DataExtraction', 'DataExtraction', 'Thomas', 1, 0, 'DataExtraction');
 ");
 
 
