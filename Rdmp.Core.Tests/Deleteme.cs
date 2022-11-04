cb2865382aac9e57c4662dc75a93119972d583b7
tznind <tznind@dundee.ac.uk>
2022-11-04 14:49:55 +0000

    ignore this commit

---
+++ Rdmp.Core.Tests/Deleteme.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Rdmp.Core.Curation.Data;
+using Tests.Common;
+
+namespace Rdmp.Core.Tests
+{
+    public class CatalogueTests : UnitTests
+    {
+        [Test]
+        public void Test_GetObjects_Catalogue()
+        {
+            CatalogueItem ci = WhenIHaveA<CatalogueItem>();
+
+            ci.Name = "fish";
+
+            Assert.AreEqual("fish", ci.Name);
+            ci.RevertToDatabaseState();
+            Assert.AreNotEqual("fish", ci.Name);
+        }
+    }
+    
+}
---
+++ Rdmp.UI.Tests/Deleteme2.cs
@@ -0,0 +1,26 @@
+﻿using MapsDirectlyToDatabaseTable.Revertable;
+using NUnit.Framework;
+using Rdmp.Core.Curation.Data;
+using Rdmp.UI.DataViewing;
+using Rdmp.UI.MainFormUITabs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.UI.Tests
+{
+    public class CatalogueUITests2 : UITests
+    {
+        [Test, UITimeout(20000)]
+        public void Test_CatalogueUI_NormalState()
+        {
+            //Get a new platform object
+            var cata = WhenIHaveA<Catalogue>();
+                        
+            //Get an instance of the UI
+            AndLaunch<CatalogueUI>(cata);
+        }
+    }
+}
