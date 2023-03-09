a0194d06a9d1bc53755e4dc24f4cf6ea480ea2cc
jas88 <j.a.sutherland@dundee.ac.uk>
2023-03-09 10:13:06 -0600

    Update NuspecIsCorrectTests.cs
    Nuspec/Packages.md test fixup, be more flexible about paths

--- Rdmp.Core.Tests/ReusableLibraryCode/NuspecIsCorrectTests.cs
+++ Rdmp.Core.Tests/ReusableLibraryCode/NuspecIsCorrectTests.cs
@@ -4,6 +4,8 @@
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -17,26 +19,45 @@ namespace Rdmp.Core.Tests.ReusableLibraryCode
     /// </summary>
     class NuspecIsCorrectTests
     {
-        static string[] Analyzers = new string[]{ "coverlet.collector", "SecurityCodeScan.VS2019" };
+        private static readonly string[] Analyzers = new string[]{ "coverlet.collector", "SecurityCodeScan.VS2019" };
+        //<PackageReference Include="NUnit3TestAdapter" Version="3.13.0" />
+        private static readonly Regex RPackageRef = new Regex(@"<PackageReference\s+Include=""(.*)""\s+Version=""([^""]*)""", RegexOptions.IgnoreCase|RegexOptions.Compiled|RegexOptions.CultureInvariant);
+
+        //<dependency id="CsvHelper" version="12.1.2" />
+        private static readonly Regex RDependencyRef = new Regex(@"<dependency\s+id=""(.*)""\s+version=""([^""]*)""", RegexOptions.IgnoreCase|RegexOptions.Compiled|RegexOptions.CultureInvariant);
+        private static readonly Dictionary<string, string> PathCache;
+
+        static NuspecIsCorrectTests()
+        {
+            var solutionRoot = AppDomain.CurrentDomain.BaseDirectory;
+            while (!Directory.EnumerateFiles(solutionRoot, "*.sln").Any())
+                solutionRoot = Directory.GetParent(solutionRoot)?.FullName ?? throw new Exception($"No VS Solution file found above {AppDomain.CurrentDomain.BaseDirectory}");
+            PathCache=Directory.EnumerateFiles(solutionRoot,"*.*", SearchOption.AllDirectories)
+                .Where(p => p.EndsWith(".csproj") || p.EndsWith("Packages.md") || p.EndsWith(".nuspec"))
+                .ToDictionary(Path.GetFileName, p => p);
+        }
 
         //test dependencies should be in Plugin.Test.nuspec
-        [TestCase("../../../../../../../Tests.Common/Tests.Common.csproj","../../../../../../../Plugins/Plugin.Test/Plugin.Test.nuspec","../../../../../../../Documentation/CodeTutorials/Packages.md")]
+        [TestCase("Tests.Common.csproj","Plugin.Test.nuspec","Packages.md")]
         
         //core dependencies should be in all nuspec files
-        [TestCase("../../../../../../../Rdmp.Core/Rdmp.Core.csproj","../../../../../../../Plugins/Plugin.Test/Plugin.Test.nuspec","../../../../../../../Documentation/CodeTutorials/Packages.md")]
-        [TestCase("../../../../../../../Rdmp.Core/Rdmp.Core.csproj","../../../../../../../Plugins/Plugin/Plugin.nuspec","../../../../../../../Documentation/CodeTutorials/Packages.md")]
-        [TestCase("../../../../../../../Rdmp.Core/Rdmp.Core.csproj","../../../../../../../Plugins/Plugin.UI/Plugin.UI.nuspec","../../../../../../../Documentation/CodeTutorials/Packages.md")]
+        [TestCase("Rdmp.Core.csproj","Plugin.Test.nuspec","Packages.md")]
+        [TestCase("Rdmp.Core.csproj","Plugin.nuspec","Packages.md")]
+        [TestCase("Rdmp.Core.csproj","Plugin.UI.nuspec","Packages.md")]
 
         //ui dependencies should be in Plugin.UI.nuspec
-        [TestCase("../../../../../../../Rdmp.UI/Rdmp.UI.csproj","../../../../../../../Plugins/Plugin.UI/Plugin.UI.nuspec","../../../../../../../Documentation/CodeTutorials/Packages.md")]
+        [TestCase("Rdmp.UI.csproj","Plugin.UI.nuspec","Packages.md")]
         public void TestDependencyCorrect( string csproj, string nuspec, string packagesMarkdown)
         {
-            if(csproj != null && !Path.IsPathRooted(csproj))
-                csproj = Path.Combine(TestContext.CurrentContext.TestDirectory,csproj);
-            if(nuspec != null && !Path.IsPathRooted(nuspec))
-                nuspec = Path.Combine(TestContext.CurrentContext.TestDirectory,nuspec);
-            if(packagesMarkdown != null && !Path.IsPathRooted(packagesMarkdown))
-                packagesMarkdown = Path.Combine(TestContext.CurrentContext.TestDirectory,packagesMarkdown);
+            string nuspecContent=null;
+            string[] packagesMarkdownContent=null;
+
+            if (csproj != null)
+                csproj = PathCache[csproj];
+            if (nuspec != null)
+                nuspec = PathCache[nuspec];
+            if (packagesMarkdown != null)
+                packagesMarkdown = PathCache[packagesMarkdown];
 
             if (!File.Exists(csproj))
                 Assert.Fail("Could not find file {0}", csproj);
@@ -46,38 +67,33 @@ namespace Rdmp.Core.Tests.ReusableLibraryCode
             if (packagesMarkdown != null && !File.Exists(packagesMarkdown))
                 Assert.Fail("Could not find file {0}", packagesMarkdown);
 
-            //<PackageReference Include="NUnit3TestAdapter" Version="3.13.0" />
-            Regex rPackageRef = new Regex(@"<PackageReference\s+Include=""(.*)""\s+Version=""([^""]*)""", RegexOptions.IgnoreCase);
-
-            //<dependency id="CsvHelper" version="12.1.2" />
-            Regex rDependencyRef = new Regex(@"<dependency\s+id=""(.*)""\s+version=""([^""]*)""", RegexOptions.IgnoreCase);
+            if (nuspec !=null) nuspecContent = File.ReadAllText(nuspec);
+            if (packagesMarkdown != null) packagesMarkdownContent = File.ReadAllLines(packagesMarkdown);
 
             //For each dependency listed in the csproj
-            foreach (Match p in rPackageRef.Matches(File.ReadAllText(csproj)))
+            foreach (Match p in RPackageRef.Matches(File.ReadAllText(csproj)))
             {
-                string package = p.Groups[1].Value;
-                string version = p.Groups[2].Value;
+                var package = p.Groups[1].Value;
+                var version = p.Groups[2].Value;
 
-                bool found = false;
+                var found = false;
 
                 // Not one we need to pass on to the package consumers
                     if(package.Contains("Microsoft.NETFramework.ReferenceAssemblies.net461"))
                         continue;
 
                 //analyzers do not have to be listed as a dependency in nuspec (but we should document them in packages.md)
-                if (!Analyzers.Contains(package) && nuspec != null)
+                if (!Analyzers.Contains(package) && nuspecContent != null)
                 {
                     //make sure it appears in the nuspec
-                    foreach (Match d in rDependencyRef.Matches(File.ReadAllText(nuspec)))
+                    foreach (Match d in RDependencyRef.Matches(nuspecContent))
                     {
-                        string packageDependency = d.Groups[1].Value;
-                        string versionDependency = d.Groups[2].Value;
+                        var packageDependency = d.Groups[1].Value;
+                        var versionDependency = d.Groups[2].Value;
 
-                        if (packageDependency.Equals(package))
-                        {
-                            Assert.AreEqual(version, versionDependency, "Package {0} is version {1} in {2} but version {3} in {4}", package, version, csproj, versionDependency, nuspec);
-                            found = true;
-                        }
+                        if (!packageDependency.Equals(package)) continue;
+                        Assert.AreEqual(version, versionDependency, "Package {0} is version {1} in {2} but version {3} in {4}", package, version, csproj, versionDependency, nuspec);
+                        found = true;
                     }
 
                     if (!found)
@@ -86,36 +102,23 @@ namespace Rdmp.Core.Tests.ReusableLibraryCode
                 }
 
 
-                //And make sure it appears in the packages.md file
-                if (packagesMarkdown != null)
+                // End early if not checking Markdown
+                if (packagesMarkdown == null) continue;
+                found = false;
+                foreach (var count in from line in packagesMarkdownContent where Regex.IsMatch(line, $@"[\s[]{Regex.Escape(package)}[\s\]]", RegexOptions.IgnoreCase) select new Regex(Regex.Escape(version)).Matches(line).Count)
                 {
-                    found = false;
-                    foreach (string line in File.ReadAllLines(packagesMarkdown))
-                    {
-                        if (Regex.IsMatch(line, @"[\s[]" + Regex.Escape(package) + @"[\s\]]", RegexOptions.IgnoreCase))
-                        {
-                            int count = new Regex(Regex.Escape(version)).Matches(line).Count;
-
-                            Assert.AreEqual(2, count, "Markdown file {0} did not contain 2 instances of the version {1} for package {2} in {3}", packagesMarkdown, version, package, csproj);
-                            found = true;
-                        }
-                    }
-
-                    if (!found)
-                        Assert.Fail("Package {0} in {1} is not documented in {2}. Recommended line is:\r\n{3}", package, csproj, packagesMarkdown,
-                            BuildRecommendedMarkdownLine(package, version));
+                    Assert.AreEqual(2, count, "Markdown file {0} did not contain 2 instances of the version {1} for package {2} in {3}", packagesMarkdown, version, package, csproj);
+                    found = true;
                 }
+
+                if (!found)
+                    Assert.Fail("Package {0} in {1} is not documented in {2}. Recommended line is:\r\n{3}", package, csproj, packagesMarkdown,
+                        BuildRecommendedMarkdownLine(package, version));
             }
         }
 
-        private object BuildRecommendedDependencyLine(string package, string version)
-        {
-            return string.Format("<dependency id=\"{0}\" version=\"{1}\" />",package,version);
-        }
+        private static object BuildRecommendedDependencyLine(string package, string version) => $"<dependency id=\"{package}\" version=\"{version}\" />";
 
-        private object BuildRecommendedMarkdownLine(string package, string version)
-        {
-            return string.Format("| {0} | [GitHub]() | [{1}](https://www.nuget.org/packages/{0}/{1}) | | | |",package,version);
-        }
+        private static object BuildRecommendedMarkdownLine(string package, string version) => $"| {package} | [GitHub]() | [{version}](https://www.nuget.org/packages/{package}/{version}) | | | |";
     }
 }
