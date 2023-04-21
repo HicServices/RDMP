3e9151975407fdf165c03f2bb02c6ad68417ef11
James A Sutherland <>
2023-04-21 08:49:24 -0500

    Namespace fix

--- Rdmp.Core/ReusableLibraryCode/Helpers/Settings.cs
+++ Rdmp.Core/ReusableLibraryCode/Helpers/Settings.cs
@@ -9,7 +9,7 @@
 using Plugin.Settings;
 using Plugin.Settings.Abstractions;
 
-namespace ReusableLibraryCode.Helpers;
+namespace Rdmp.Core.ReusableLibraryCode.Helpers;
 
 /// <summary>
 /// This is the Settings static class that can be used in your Core solution or in any
