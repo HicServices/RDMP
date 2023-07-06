79165f1879226d296dad38448791812ad520e9bd
jas88 <j.a.sutherland@dundee.ac.uk>
2023-07-06 11:06:11 -0500

    Annotation cleanup
    Remove disused, add Usage annotation where needed

--- Rdmp.Core/ReusableLibraryCode/Annotations/Annotations.cs
+++ Rdmp.Core/ReusableLibraryCode/Annotations/Annotations.cs
@@ -6,14 +6,6 @@
 
 using System;
 
-#pragma warning disable 1591
-// ReSharper disable UnusedMember.Global
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable UnusedAutoPropertyAccessor.Global
-// ReSharper disable IntroduceOptionalParameters.Global
-// ReSharper disable MemberCanBeProtected.Global
-// ReSharper disable InconsistentNaming
-
 namespace Rdmp.Core.ReusableLibraryCode.Annotations;
 
 /// <summary>
@@ -46,48 +38,6 @@ public sealed class CanBeNullAttribute : Attribute { }
     AttributeTargets.Property | AttributeTargets.Delegate |
     AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
 public sealed class NotNullAttribute : Attribute { }
-
-/// <summary>
-/// Indicates that the marked method builds string by format pattern and (optional) arguments.
-/// Parameter, which contains format string, should be given in constructor. The format string
-/// should be in <see cref="string.Format(IFormatProvider,string,object[])"/>-like form
-/// </summary>
-/// <example><code>
-/// [StringFormatMethod("message")]
-/// public void ShowError(string message, params object[] args) { /* do something */ }
-/// public void Foo() {
-///   ShowError("Failed: {0}"); // Warning: Non-existing argument in format string
-/// }
-/// </code></example>
-[AttributeUsage(
-    AttributeTargets.Constructor | AttributeTargets.Method,
-    AllowMultiple = false, Inherited = true)]
-public sealed class StringFormatMethodAttribute : Attribute
-{
-    /// <param name="formatParameterName">
-    /// Specifies which parameter of an annotated method should be treated as format-string
-    /// </param>
-    public StringFormatMethodAttribute(string formatParameterName)
-    {
-        FormatParameterName = formatParameterName;
-    }
-
-    public string FormatParameterName { get; private set; }
-}
-
-/// <summary>
-/// Indicates that the function argument should be string literal and match one
-/// of the parameters of the caller function. For example, ReSharper annotates
-/// the parameter of <see cref="System.ArgumentNullException"/>
-/// </summary>
-/// <example><code>
-/// public void Foo(string param) {
-///   if (param == null)
-///     throw new ArgumentNullException("par"); // Warning: Cannot resolve symbol
-/// }
-/// </code></example>
-[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
-public sealed class InvokerParameterNameAttribute : Attribute { }
 
 /// <summary>
 /// Indicates that the method is contained in a type that implements
@@ -138,131 +88,6 @@ public sealed class NotifyPropertyChangedInvocatorAttribute : Attribute
 }
 
 /// <summary>
-/// Describes dependency between method input and output
-/// </summary>
-/// <syntax>
-/// <p>Function Definition Table syntax:</p>
-/// <list>
-/// <item>FDT      ::= FDTRow [;FDTRow]*</item>
-/// <item>FDTRow   ::= Input =&gt; Output | Output &lt;= Input</item>
-/// <item>Input    ::= ParameterName: Value [, Input]*</item>
-/// <item>Output   ::= [ParameterName: Value]* {halt|stop|void|nothing|Value}</item>
-/// <item>Value    ::= true | false | null | notnull | canbenull</item>
-/// </list>
-/// If method has single input parameter, its name could be omitted.<br/>
-/// Using <c>halt</c> (or <c>void</c>/<c>nothing</c>, which is the same)
-/// for method output means that the methos doesn't return normally.<br/>
-/// <c>canbenull</c> annotation is only applicable for output parameters.<br/>
-/// You can use multiple <c>[ContractAnnotation]</c> for each FDT row,
-/// or use single attribute with rows separated by semicolon.<br/>
-/// </syntax>
-/// <examples><list>
-/// <item><code>
-/// [ContractAnnotation("=> halt")]
-/// public void TerminationMethod()
-/// </code></item>
-/// <item><code>
-/// [ContractAnnotation("halt &lt;= condition: false")]
-/// public void Assert(bool condition, string text) // regular assertion method
-/// </code></item>
-/// <item><code>
-/// [ContractAnnotation("s:null => true")]
-/// public bool IsNullOrEmpty(string s) // string.IsNullOrEmpty()
-/// </code></item>
-/// <item><code>
-/// // A method that returns null if the parameter is null, and not null if the parameter is not null
-/// [ContractAnnotation("null => null; notnull => notnull")]
-/// public object Transform(object data) 
-/// </code></item>
-/// <item><code>
-/// [ContractAnnotation("s:null=>false; =>true,result:notnull; =>false, result:null")]
-/// public bool TryParse(string s, out Person result)
-/// </code></item>
-/// </list></examples>
-[AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
-public sealed class ContractAnnotationAttribute : Attribute
-{
-    public ContractAnnotationAttribute([NotNull] string contract)
-        : this(contract, false) { }
-
-    public ContractAnnotationAttribute([NotNull] string contract, bool forceFullStates)
-    {
-        Contract = contract;
-        ForceFullStates = forceFullStates;
-    }
-
-    public string Contract { get; private set; }
-    public bool ForceFullStates { get; private set; }
-}
-
-/// <summary>
-/// Indicates that marked element should be localized or not
-/// </summary>
-/// <example><code>
-/// [LocalizationRequiredAttribute(true)]
-/// public class Foo {
-///   private string str = "my string"; // Warning: Localizable string
-/// }
-/// </code></example>
-[AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
-public sealed class LocalizationRequiredAttribute : Attribute
-{
-    public LocalizationRequiredAttribute() : this(true) { }
-    public LocalizationRequiredAttribute(bool required)
-    {
-        Required = required;
-    }
-
-    public bool Required { get; private set; }
-}
-
-/// <summary>
-/// Indicates that the value of the marked type (or its derivatives)
-/// cannot be compared using '==' or '!=' operators and <c>Equals()</c>
-/// should be used instead. However, using '==' or '!=' for comparison
-/// with <c>null</c> is always permitted.
-/// </summary>
-/// <example><code>
-/// [CannotApplyEqualityOperator]
-/// class NoEquality { }
-/// class UsesNoEquality {
-///   public void Test() {
-///     var ca1 = new NoEquality();
-///     var ca2 = new NoEquality();
-///     if (ca1 != null) { // OK
-///       bool condition = ca1 == ca2; // Warning
-///     }
-///   }
-/// }
-/// </code></example>
-[AttributeUsage(
-    AttributeTargets.Interface | AttributeTargets.Class |
-    AttributeTargets.Struct, AllowMultiple = false, Inherited = true)]
-public sealed class CannotApplyEqualityOperatorAttribute : Attribute { }
-
-/// <summary>
-/// When applied to a target attribute, specifies a requirement for any type marked
-/// with the target attribute to implement or inherit specific type or types.
-/// </summary>
-/// <example><code>
-/// [BaseTypeRequired(typeof(IComponent)] // Specify requirement
-/// public class ComponentAttribute : Attribute { }
-/// [Component] // ComponentAttribute requires implementing IComponent interface
-/// public class MyComponent : IComponent { }
-/// </code></example>
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
-[BaseTypeRequired(typeof(Attribute))]
-public sealed class BaseTypeRequiredAttribute : Attribute
-{
-    public BaseTypeRequiredAttribute([NotNull] Type baseType)
-    {
-        BaseType = baseType;
-    }
-
-    [NotNull] public Type BaseType { get; private set; }
-}
-
-/// <summary>
 /// Indicates that the marked symbol is used implicitly
 /// (e.g. via reflection, in external library), so this symbol
 /// will not be marked as unused (as well as by other usage inspections)
@@ -270,54 +95,18 @@ public sealed class BaseTypeRequiredAttribute : Attribute
 [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
 public sealed class UsedImplicitlyAttribute : Attribute
 {
-    public UsedImplicitlyAttribute()
-        : this(ImplicitUseKindFlags.Default, ImplicitUseTargetFlags.Default) { }
-
-    public UsedImplicitlyAttribute(ImplicitUseKindFlags useKindFlags)
-        : this(useKindFlags, ImplicitUseTargetFlags.Default) { }
-
-    public UsedImplicitlyAttribute(ImplicitUseTargetFlags targetFlags)
-        : this(ImplicitUseKindFlags.Default, targetFlags) { }
-
     public UsedImplicitlyAttribute(
-        ImplicitUseKindFlags useKindFlags, ImplicitUseTargetFlags targetFlags)
+        ImplicitUseKindFlags useKindFlags=ImplicitUseKindFlags.Default,
+        ImplicitUseTargetFlags targetFlags=ImplicitUseTargetFlags.Default)
     {
         UseKindFlags = useKindFlags;
         TargetFlags = targetFlags;
     }
 
-    public ImplicitUseKindFlags UseKindFlags { get; private set; }
-    public ImplicitUseTargetFlags TargetFlags { get; private set; }
+    public ImplicitUseKindFlags UseKindFlags { get; }
+    public ImplicitUseTargetFlags TargetFlags { get; }
 }
 
-/// <summary>
-/// Should be used on attributes and causes ReSharper
-/// to not mark symbols marked with such attributes as unused
-/// (as well as by other usage inspections)
-/// </summary>
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
-public sealed class MeansImplicitUseAttribute : Attribute
-{
-    public MeansImplicitUseAttribute() 
-        : this(ImplicitUseKindFlags.Default, ImplicitUseTargetFlags.Default) { }
-
-    public MeansImplicitUseAttribute(ImplicitUseKindFlags useKindFlags)
-        : this(useKindFlags, ImplicitUseTargetFlags.Default) { }
-
-    public MeansImplicitUseAttribute(ImplicitUseTargetFlags targetFlags)
-        : this(ImplicitUseKindFlags.Default, targetFlags) { }
-
-    public MeansImplicitUseAttribute(
-        ImplicitUseKindFlags useKindFlags, ImplicitUseTargetFlags targetFlags)
-    {
-        UseKindFlags = useKindFlags;
-        TargetFlags = targetFlags;
-    }
-
-    [UsedImplicitly] public ImplicitUseKindFlags UseKindFlags { get; private set; }
-    [UsedImplicitly] public ImplicitUseTargetFlags TargetFlags { get; private set; }
-}
-  
 [Flags]
 public enum ImplicitUseKindFlags
 {
@@ -337,8 +126,6 @@ public enum ImplicitUseKindFlags
 
 /// <summary>
 /// Specify what is considered used implicitly
-/// when marked with <see cref="MeansImplicitUseAttribute"/>
-/// or <see cref="UsedImplicitlyAttribute"/>
 /// </summary>
 [Flags]
 public enum ImplicitUseTargetFlags
@@ -349,61 +136,4 @@ public enum ImplicitUseTargetFlags
     Members = 2,
     /// <summary>Entity marked with attribute and all its members considered used</summary>
     WithMembers = Itself | Members
-}
-
-/// <summary>
-/// This attribute is intended to mark publicly available API
-/// which should not be removed and so is treated as used
-/// </summary>
-[MeansImplicitUse]
-public sealed class PublicAPIAttribute : Attribute
-{
-    public PublicAPIAttribute() { }
-    public PublicAPIAttribute([NotNull] string comment)
-    {
-        Comment = comment;
-    }
-
-    [NotNull] public string Comment { get; private set; }
-}
-
-/// <summary>
-/// Tells code analysis engine if the parameter is completely handled
-/// when the invoked method is on stack. If the parameter is a delegate,
-/// indicates that delegate is executed while the method is executed.
-/// If the parameter is an enumerable, indicates that it is enumerated
-/// while the method is executed
-/// </summary>
-[AttributeUsage(AttributeTargets.Parameter, Inherited = true)]
-public sealed class InstantHandleAttribute : Attribute { }
-
-/// <summary>
-/// Indicates that a method does not make any observable state changes.
-/// The same as <c>System.Diagnostics.Contracts.PureAttribute</c>
-/// </summary>
-/// <example><code>
-/// [Pure] private int Multiply(int x, int y) { return x * y; }
-/// public void Foo() {
-///   const int a = 2, b = 2;
-///   Multiply(a, b); // Waring: Return value of pure method is not used
-/// }
-/// </code></example>
-[AttributeUsage(AttributeTargets.Method, Inherited = true)]
-public sealed class PureAttribute : Attribute { }
-
-/// <summary>
-/// Indicates that a parameter is a path to a file or a folder
-/// within a web project. Path can be relative or absolute,
-/// starting from web root (~)
-/// </summary>
-[AttributeUsage(AttributeTargets.Parameter)]
-public class PathReferenceAttribute : Attribute
-{
-    public PathReferenceAttribute() { }
-    public PathReferenceAttribute([PathReference] string basePath)
-    {
-        BasePath = basePath;
-    }
-
-    [NotNull] public string BasePath { get; private set; }
 }
