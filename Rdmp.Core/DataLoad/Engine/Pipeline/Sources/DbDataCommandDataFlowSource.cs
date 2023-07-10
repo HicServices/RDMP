7cb07a58546093b2f653629af3ff6455b7323fa9
jas88 <j.a.sutherland@dundee.ac.uk>
2023-07-10 16:01:40 -0500

    Update DbDataCommandDataFlowSource.cs
    Cache schema chunk and clone as needed, begin/end dataload explicitly

--- Rdmp.Core/DataLoad/Engine/Pipeline/Sources/DbDataCommandDataFlowSource.cs
+++ Rdmp.Core/DataLoad/Engine/Pipeline/Sources/DbDataCommandDataFlowSource.cs
@@ -51,7 +51,9 @@ public class DbDataCommandDataFlowSource : IDbDataCommandDataFlowSource
 
     private int _numberOfColumns;
 
-    private bool firstChunk = true;
+    private bool _firstChunk = true;
+
+    private DataTable schema = null;
 
     public DataTable GetChunk(IDataLoadEventListener job, GracefulCancellationToken cancellationToken)
     {
@@ -69,14 +71,17 @@ public class DbDataCommandDataFlowSource : IDbDataCommandDataFlowSource
 
             _reader = cmd.ExecuteReaderAsync(cancellationToken.AbortToken).Result;
             _numberOfColumns = _reader.FieldCount;
+
+            schema = GetChunkSchema(_reader);
         }
 
         var readThisBatch = 0;
         timer.Start();
         try
         {
-            DataTable chunk = GetChunkSchema(_reader);
+            var chunk = schema.Clone();
             chunk.BeginLoadData();
+
             while (_reader.HasRows && _reader.Read())
             {
                 cancellationToken.ThrowIfCancellationRequested();
@@ -84,26 +89,32 @@ public class DbDataCommandDataFlowSource : IDbDataCommandDataFlowSource
                 AddRowToDataTable(chunk, _reader);
                 readThisBatch++;
 
-                //we reached batch limit
-                if (readThisBatch == BatchSize)
-                {
-                    chunk.EndLoadData();
-                    return chunk;
-                }
+                // loop until we reach the batch limit
+                if (readThisBatch != BatchSize) continue;
+
+                chunk.EndLoadData();
+                return chunk;
             }
             chunk.EndLoadData();
 
             //if data was read
             if (readThisBatch > 0)
+            {
+                chunk.EndLoadData();
                 return chunk;
+            }
 
             //data is exhausted
 
             //if data was exhausted on first read and we are allowing empty result sets
-            if (firstChunk && AllowEmptyResultSets)
-                return chunk; //return the empty chunk
+            if (_firstChunk && AllowEmptyResultSets)
+            {
+                chunk.EndLoadData();
+                return chunk;//return the empty chunk
+            }
 
             //data exhausted
+            schema.Dispose();
             return null;
         }
         catch (Exception e)
@@ -113,7 +124,7 @@ public class DbDataCommandDataFlowSource : IDbDataCommandDataFlowSource
         }
         finally
         {
-            firstChunk = false;
+            _firstChunk = false;
             timer.Stop();
             job.OnProgress(this,
                 new ProgressEventArgs(_taskBeingPerformed, new ProgressMeasurement(TotalRowsRead, ProgressType.Records),
