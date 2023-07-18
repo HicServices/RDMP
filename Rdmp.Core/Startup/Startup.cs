432624959e369eba02093bf6990351699f0295e2
jas88 <j.a.sutherland@dundee.ac.uk>
2023-07-18 17:37:07 -0500

    Update Startup.cs
    Read plugins from exe directory not db

--- Rdmp.Core/Startup/Startup.cs
+++ Rdmp.Core/Startup/Startup.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.Loader;
 using FAnsi.Discovery;
@@ -236,8 +237,10 @@ public class Startup
     /// <param name="notifier"></param>
     private static void LoadMEF(ICatalogueRepository catalogueRepository, ICheckNotifier notifier)
     {
-        foreach (var (name, body) in catalogueRepository.PluginManager.GetCompatiblePlugins()
-                     .SelectMany(static p => p.LoadModuleAssemblies).SelectMany(static a => a.GetContents()))
+        /*foreach (var (name, body) in catalogueRepository.PluginManager.GetCompatiblePlugins()
+                     .SelectMany(static p => p.LoadModuleAssemblies).SelectMany(static a => a.GetContents()))*/
+        // Ignore tiny nupkg files from  old 'unit test'
+        foreach(var (name,body) in  Directory.EnumerateFiles(AppDomain.CurrentDomain.BaseDirectory,"*.nupkg").Where(path=>new FileInfo(path).Length>100).Select(File.OpenRead).SelectMany(LoadModuleAssembly.GetContents))
         {
             try
             {
