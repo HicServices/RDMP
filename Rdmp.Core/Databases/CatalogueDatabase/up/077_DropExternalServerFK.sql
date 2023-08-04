83502721ba88c13ac4bd24a3a13ce55bd2a07d35
James Friel <JFriel001@dundee.ac.uk>
2023-08-04 16:45:00 +0100

    added todo reminder

--- Rdmp.Core/Databases/CatalogueDatabase/up/077_DropExternalServerFK.sql
+++ Rdmp.Core/Databases/CatalogueDatabase/up/077_DropExternalServerFK.sql
@@ -3,3 +3,5 @@
  
 -- Add to LoadMetadata table
 	alter table Catalogue drop constraint FK_Catalogue_ExternalDatabaseServer
+-- this might be better by making specific serveres ignorable rather than deleting the FK
+-- would have to check if a logging server is always generated on launch
