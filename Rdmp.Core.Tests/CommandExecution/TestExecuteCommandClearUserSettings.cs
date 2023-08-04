7a3da2a538f59cb7300b598d74166b32d74bf669
James Friel <JFriel001@dundee.ac.uk>
2023-08-04 13:52:06 +0100

    Add basic test

---
+++ Rdmp.Core.Tests/CommandExecution/TestExecuteCommandClearUserSettings.cs
@@ -0,0 +1,32 @@
+// Copyright (c) The University of Dundee 2018-2019
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
+using NUnit.Framework;
+using Rdmp.Core.CommandExecution.AtomicCommands;
+using Rdmp.Core.CommandLine.Interactive.Picking;
+using Rdmp.Core.ReusableLibraryCode.Checks;
+using Rdmp.Core.ReusableLibraryCode.Settings;
+
+namespace Rdmp.Core.Tests.CommandExecution;
+
+internal class TestExecuteCommandClearUserSettings : CommandCliTests
+{
+    [Test]
+    public void Test_ClearUserSettings()
+    {
+        UserSettings.Wait5SecondsAfterStartupUI = false;
+
+        GetInvoker().ExecuteCommand(typeof(ExecuteCommandSetUserSetting),new CommandLineObjectPicker(new []{ "Wait5SecondsAfterStartupUI", "true"}, GetActivator()));
+
+        Assert.IsTrue(UserSettings.Wait5SecondsAfterStartupUI);
+        GetInvoker().ExecuteCommand(typeof(ExecuteCommandSetUserSetting),new CommandLineObjectPicker(new []{ "Wait5SecondsAfterStartupUI", "false"}, GetActivator()));
+        Assert.IsFalse(UserSettings.Wait5SecondsAfterStartupUI);
+        GetInvoker().ExecuteCommand(typeof(ExecuteCommandClearUserSettings),new CommandLineObjectPicker(new string[]{ } ,GetActivator()));
+            
+        Assert.IsTrue(UserSettings.Wait5SecondsAfterStartupUI);
+
+    }
+}
