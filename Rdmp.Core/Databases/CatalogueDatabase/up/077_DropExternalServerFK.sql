76c5cc870be5f88f63c5d46c5e1e228b1098dc22
James Friel <JFriel001@dundee.ac.uk>
2023-08-07 09:53:39 +0100

    remove fk drop

--- Rdmp.Core/Databases/CatalogueDatabase/up/077_DropExternalServerFK.sql
+++
@@ -1,7 +0,0 @@
-﻿--Version:7.0.0
---Description: Updates database to support custom folders on CohortIdentificationConfiguration and LoadMetadata
- 
--- Add to LoadMetadata table
-	alter table Catalogue drop constraint FK_Catalogue_ExternalDatabaseServer
--- this might be better by making specific serveres ignorable rather than deleting the FK
--- would have to check if a logging server is always generated on launch
