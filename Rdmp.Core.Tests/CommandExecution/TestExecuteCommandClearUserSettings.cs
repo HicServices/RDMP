cc0d3972e914fcdec1b156ae504ebba066f3362d
jas88 <j.a.sutherland@dundee.ac.uk>
2023-08-11 08:32:01 -0500

    Update TestExecuteCommandClearUserSettings.cs
    Address review comments

--- Rdmp.Core.Tests/CommandExecution/TestExecuteCommandClearUserSettings.cs
+++ Rdmp.Core.Tests/CommandExecution/TestExecuteCommandClearUserSettings.cs
@@ -1,4 +1,4 @@
-// Copyright (c) The University of Dundee 2018-2019
+// Copyright (c) The University of Dundee 2018-2023
 // This file is part of the Research Data Management Platform (RDMP).
 // RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -12,21 +12,23 @@ using Rdmp.Core.ReusableLibraryCode.Settings;
 
 namespace Rdmp.Core.Tests.CommandExecution;
 
-internal class TestExecuteCommandClearUserSettings : CommandCliTests
+internal sealed class TestExecuteCommandClearUserSettings : CommandCliTests
 {
     [Test]
     public void Test_ClearUserSettings()
     {
+        var invoker = GetInvoker();
+        var activator = GetActivator();
+
         UserSettings.Wait5SecondsAfterStartupUI = false;
 
-        GetInvoker().ExecuteCommand(typeof(ExecuteCommandSetUserSetting), new CommandLineObjectPicker(new[] { nameof(UserSettings.Wait5SecondsAfterStartupUI), "true" }, GetActivator()));
+        invoker.ExecuteCommand(typeof(ExecuteCommandSetUserSetting), new CommandLineObjectPicker(new[] { nameof(UserSettings.Wait5SecondsAfterStartupUI), "true" }, activator));
 
         Assert.IsTrue(UserSettings.Wait5SecondsAfterStartupUI);
-        GetInvoker().ExecuteCommand(typeof(ExecuteCommandSetUserSetting), new CommandLineObjectPicker(new[] { nameof(UserSettings.Wait5SecondsAfterStartupUI), "false" }, GetActivator()));
+        invoker.ExecuteCommand(typeof(ExecuteCommandSetUserSetting), new CommandLineObjectPicker(new[] { nameof(UserSettings.Wait5SecondsAfterStartupUI), "false" }, activator));
         Assert.IsFalse(UserSettings.Wait5SecondsAfterStartupUI);
-        GetInvoker().ExecuteCommand(typeof(ExecuteCommandClearUserSettings), new CommandLineObjectPicker(new string[] { }, GetActivator()));
+        invoker.ExecuteCommand(typeof(ExecuteCommandClearUserSettings), new CommandLineObjectPicker(System.Array.Empty<string>(), activator));
 
         Assert.IsTrue(UserSettings.Wait5SecondsAfterStartupUI);
-
     }
 }
