127b55ed2efb26e243dc3f86a022475ab7ad16aa
James Friel <jfriel001@dundee.ac.uk>
2023-08-14 15:43:08 +0100

    add documentation

--- Rdmp.Core/Logging/LogEntry.cs
+++ Rdmp.Core/Logging/LogEntry.cs
@@ -1,8 +1,17 @@
+// Copyright (c) The University of Dundee 2018-2023
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
-
-namespace Rdmp.Core.Logging;
 using System;
+namespace Rdmp.Core.Logging;
 
+
+/// <summary>
+/// Internal Object used to store log entries while there are inthe queue for batch insert into a database
+/// Currently used for batching of the progressLog logs
+/// </summary>
 public class LogEntry
 {
     public LogEntry(string eventType, string description, string source, DateTime time)
