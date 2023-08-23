dd65d749b4e9ab1622bc5a8d7ab4e9fa7f6d0d1d
jas88 <j.a.sutherland@dundee.ac.uk>
2023-08-23 18:57:53 -0500

    Tidyup

--- Rdmp.Core/ReusableLibraryCode/Checks/IgnoreAllErrorsCheckNotifier.cs
+++ Rdmp.Core/ReusableLibraryCode/Checks/IgnoreAllErrorsCheckNotifier.cs
@@ -10,11 +10,11 @@ namespace Rdmp.Core.ReusableLibraryCode.Checks;
 /// ICheckNotifier which ignores all check messages completely (including failures) but responds true to any ProposedFixes.  Use this ICheckNotifier
 /// when you want to run the Check method on an ICheckable but don't care whether it passes or not.
 /// </summary>
-public class IgnoreAllErrorsCheckNotifier : ICheckNotifier
+public sealed class IgnoreAllErrorsCheckNotifier : ICheckNotifier
 {
     public static readonly IgnoreAllErrorsCheckNotifier Instance = new();
 
-    public IgnoreAllErrorsCheckNotifier()
+    private IgnoreAllErrorsCheckNotifier()
     {
     }
 
