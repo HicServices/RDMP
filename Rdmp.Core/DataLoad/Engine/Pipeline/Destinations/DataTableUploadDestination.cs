3d7c032e80384c46e60c7774987bfb3318549357
James A Sutherland <j@sutherland.pw>
2023-08-28 18:31:47 -0500

    Revert "bugfix/RDMP 31 extraction delect ui issue (#1609)"
    This reverts commit a18976dcbf5b2fd8eacf386b901ffeb1da6a5120.

--- Rdmp.Core/DataLoad/Engine/Pipeline/Destinations/DataTableUploadDestination.cs
+++ Rdmp.Core/DataLoad/Engine/Pipeline/Destinations/DataTableUploadDestination.cs
@@ -237,8 +237,6 @@ public class DataTableUploadDestination : IPluginDataFlowComponent<DataTable>, I
             throw new Exception($"Failed to write rows (in transaction) to table {TargetTableName}", e);
         }
 
-
-        _dataLoadInfo?.CloseAndMarkComplete();
         return null;
     }
 
