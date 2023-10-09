5d89d925e79db0c1096c12e86aaa163f056737da
James Friel <jfriel001@dundee.ac.uk>
2023-10-09 10:57:08 +0100

    store holdout information

--- Rdmp.Core/DataExport/DataExtraction/Pipeline/ExtractionHoldout.cs
+++ Rdmp.Core/DataExport/DataExtraction/Pipeline/ExtractionHoldout.cs
@@ -7,6 +7,7 @@ using Rdmp.Core.ReusableLibraryCode.Progress;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,9 @@ public class ExtractionHoldout : IPluginDataFlowComponent<DataTable>, IPipelineR
 
     [DemandsInitialization("Use a % as holdout value. If unselected, the actual number will be used.")]
     public bool isPercentage { get; set; }
+
+    [DemandsInitialization("Write the holdout data to disk.")]
+    public string holdoutStorageLocation { get; set; }
 
 
     public IExtractDatasetCommand Request { get; private set; }
@@ -55,6 +59,23 @@ public class ExtractionHoldout : IPluginDataFlowComponent<DataTable>, IPipelineR
 
     }
 
+    private void writeDataTabletoCSV(DataTable dt)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().
+                                          Select(column => column.ColumnName);
+        sb.AppendLine(string.Join(",", columnNames));
+
+        foreach (DataRow row in dt.Rows)
+        {
+            IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+            sb.AppendLine(string.Join(",", fields));
+        }
+        String filename = Request.ToString();
+        File.WriteAllText($"{holdoutStorageLocation}/holdout_{filename}.csv", sb.ToString());
+    }
+
     public DataTable ProcessPipelineData(DataTable toProcess, IDataLoadEventListener listener, GracefulCancellationToken cancellationToken)
     {
         if (toProcess.Rows.Count == 0)
@@ -64,16 +85,27 @@ public class ExtractionHoldout : IPluginDataFlowComponent<DataTable>, IPipelineR
         DataTable holdoutData = toProcess.Clone();
         int holdoutCount = getHoldoutRowCount(toProcess, listener);
         var rand = new Random();
-        Console.WriteLine("Hello world!");
+        holdoutData.BeginLoadData();
+        toProcess.BeginLoadData();
         var rowsToMove = toProcess.AsEnumerable().OrderBy(r => rand.Next()).Take(holdoutCount);
         foreach (DataRow row in rowsToMove)
         {
             holdoutData.ImportRow(row);
             row.Delete();
         }
-        holdoutData.AcceptChanges();
-        toProcess.AcceptChanges();
+        holdoutData.EndLoadData();
+        toProcess.EndLoadData();
+        //todo need to sdo something with the holdoutCount
+        //maybe reimport it as a catalog?
+        if (holdoutStorageLocation is not null && holdoutStorageLocation.Length > 0)
+        {
+            //write this data somewhere
+            writeDataTabletoCSV(holdoutData);
+        }
+        {
 
+        }
+        Catalogue x = new Catalogue();
         return toProcess;
     }
 
