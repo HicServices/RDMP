4fc22a37ae2ae63730ce0e74ef00a37ce4434210
James Friel <jfriel001@dundee.ac.uk>
2023-10-13 11:42:17 +0100

    add copyright

--- Rdmp.Core/DataExport/DataExtraction/Pipeline/ExtractionHoldout.cs
+++ Rdmp.Core/DataExport/DataExtraction/Pipeline/ExtractionHoldout.cs
@@ -1,7 +1,9 @@
-﻿using NPOI.SS.Formula.Functions;
-using Org.BouncyCastle.Asn1.X509.Qualified;
-using Rdmp.Core.CommandExecution;
-using Rdmp.Core.CommandExecution.AtomicCommands.CatalogueCreationCommands;
+﻿// Copyright (c) The University of Dundee 2018-2023
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
 using Rdmp.Core.Curation.Data;
 using Rdmp.Core.DataExport.DataExtraction.Commands;
 using Rdmp.Core.DataFlowPipeline;
@@ -15,7 +17,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Rdmp.Core.DataExport.DataExtraction.Pipeline;
 
