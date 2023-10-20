bbea3ee5ad4fb3ca847a1a36ec1b81647ad0c5e5
James Friel <jfriel001@dundee.ac.uk>
2023-10-20 12:47:54 +0100

    add description

--- Rdmp.Core/CohortCommitting/Pipeline/ICohortHoldoutLookupRequest.cs
+++ Rdmp.Core/CohortCommitting/Pipeline/ICohortHoldoutLookupRequest.cs
@@ -1,14 +1,17 @@
-﻿using Rdmp.Core.Curation.Data.Cohort;
+﻿// Copyright (c) The University of Dundee 2018-2023
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 using Rdmp.Core.Curation.Data.Pipelines;
 using Rdmp.Core.ReusableLibraryCode.Checks;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rdmp.Core.CohortCommitting.Pipeline;
 
+
+/// <summary>
+/// See CohortHoldoutLookupRequest
+///  </summary>
 public interface ICohortHoldoutLookupRequest : ICheckable, IHasDesignTimeMode, IPipelineUseCase
 {
 
