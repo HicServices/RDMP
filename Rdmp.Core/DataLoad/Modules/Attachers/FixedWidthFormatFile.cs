4d6525e52e472e4900afa732ef34aa62de07c427
jas88 <j.a.sutherland@dundee.ac.uk>
2023-10-26 15:12:03 -0500

    Use var

--- Rdmp.Core/DataLoad/Modules/Attachers/FixedWidthFormatFile.cs
+++ Rdmp.Core/DataLoad/Modules/Attachers/FixedWidthFormatFile.cs
@@ -77,7 +77,7 @@ public class FixedWidthFormatFile
     public DataTable GetDataTableFromFlatFile(FileInfo f)
     {
         //setup the table
-        DataTable toReturn = new DataTable();
+        var toReturn = new DataTable();
 
         toReturn.BeginLoadData();
         foreach (var fixedWidthColumn in FormatColumns)
