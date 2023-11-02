1024d001d5a7b20edb5d2014650e0a1bf6a9cc23
James Friel <jfriel001@dundee.ac.uk>
2023-11-02 13:52:56 +0000

    add export platform db command

---
+++ Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandExportPlatformDatabasesToYaml.cs
@@ -0,0 +1,44 @@
+﻿using Rdmp.Core.CommandLine.Options;
+using Rdmp.Core.Curation.Data;
+using Rdmp.Core.MapsDirectlyToDatabaseTable;
+using Rdmp.Core.ReusableLibraryCode;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace Rdmp.Core.CommandExecution.AtomicCommands
+{
+    internal class ExecuteCommandExportPlatformDatabasesToYaml : BasicCommandExecution, IAtomicCommand
+    {
+        private TableRepository _catalogueRepository;
+        private TableRepository _dataExportRepository;
+        private string _outputFile;
+        public ExecuteCommandExportPlatformDatabasesToYaml(IBasicActivateItems activator, [DemandsInitialization("Where the yaml file should be created")] string outputfile)
+        {
+
+            _catalogueRepository = activator.RepositoryLocator.CatalogueRepository as TableRepository;
+            _dataExportRepository = activator.RepositoryLocator.DataExportRepository as TableRepository;
+            _outputFile = outputfile;
+            Execute();
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            var toSerialize = new ConnectionStringsYamlFile
+            {
+                CatalogueConnectionString = _catalogueRepository?.ConnectionString,
+                DataExportConnectionString = _dataExportRepository?.ConnectionString
+            };
+
+            var serializer = new Serializer();
+            var yaml = serializer.Serialize(toSerialize);
+            File.WriteAllText(_outputFile, yaml);
+        }
+    }
+}
