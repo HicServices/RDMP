b16cb8c786800131a3bb5b02e58b3d1328b28673
James A Sutherland <>
2023-11-03 17:50:48 -0500

    Implement first pass at platform export tool

---
+++ Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandExportDatabaseToDir.cs
@@ -0,0 +1,37 @@
+// Copyright (c) The University of Dundee 2018-2023
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
+using System.IO;
+using System.Linq;
+using Rdmp.Core.Curation.Data;
+using Rdmp.Core.Repositories;
+
+namespace Rdmp.Core.CommandExecution.AtomicCommands;
+
+// Dump all compatible objects from the current platform database(s) into a
+// YAML/JSON directory for SQL-free operation.
+public class ExecuteCommandExportDatabaseToDir : BasicCommandExecution
+{
+    private readonly IBasicActivateItems _activator;
+    private readonly DirectoryInfo _target;
+
+    public ExecuteCommandExportDatabaseToDir(IBasicActivateItems activator, [DemandsInitialization("Where the platform directory should be created")] string target)
+    {
+        _target = new DirectoryInfo(target);
+        _activator = activator;
+    }
+
+    public override void Execute()
+    {
+        base.Execute();
+        var repo = new YamlRepository(_target);
+        var types = repo.GetCompatibleTypes();
+        foreach (var o in repo.GetCompatibleTypes().SelectMany(t=>_activator.GetRepositoryFor(t).GetAllObjects(t)))
+        {
+            repo.SaveToDatabase(o);
+        }
+    }
+}
