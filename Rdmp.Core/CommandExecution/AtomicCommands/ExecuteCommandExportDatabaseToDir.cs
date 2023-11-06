0fee40236c78f4a5dfb8a1a12ffcf9fe37d5a0b0
James A Sutherland <>
2023-11-06 09:39:33 -0600

    Syntax fix

--- Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandExportDatabaseToDir.cs
+++ Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandExportDatabaseToDir.cs
@@ -4,6 +4,7 @@
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
+using System;
 using System.IO;
 using System.Linq;
 using Rdmp.Core.Curation.Data;
@@ -31,7 +32,7 @@ public class ExecuteCommandExportDatabaseToDir : BasicCommandExecution
         foreach (var t in repo.GetCompatibleTypes())
         {
             Console.WriteLine(t.FullName);
-            foreach (var o in t.SelectMany(t=>_activator.GetRepositoryFor(t).GetAllObjects(t)))
+            foreach (var o in _activator.GetRepositoryFor(t).GetAllObjects(t))
               repo.SaveToDatabase(o);
         }
     }
