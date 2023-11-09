aa8d8ce04d2e7cfd6935672fec4694cd8180e548
James Friel <jfriel001@dundee.ac.uk>
2023-11-09 09:16:54 +0000

    get catalogue data

---
+++ RDMP.Avalonia.UI/ViewModels/CatalogueTreeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using RDMP.Avalonia.UI.Services.BootstrapService;
+using Rdmp.Core.Curation.Data;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using DynamicData.Binding;
+
+namespace RDMP.Avalonia.UI.ViewModels
+{
+
+    public class CatalogueTreeViewModel : ReactiveObject
+    {
+
+        private ObservableCollection<Catalogue> _catalogues = new();
+
+
+        public ObservableCollection<Catalogue> Catalogues { get => _catalogues; set => this.RaiseAndSetIfChanged(ref _catalogues, value); }
+
+        public CatalogueTreeViewModel()
+        {
+            var catalogues = BootstrapService._startup.RepositoryLocator.CatalogueRepository.GetAllObjects<Catalogue>();
+            foreach (Catalogue catalogue in catalogues)
+            {
+                _catalogues.Add(catalogue);
+            }
+
+        }
+    }
+}
--- RDMP.Avalonia.UI/ViewModels/LeftHandPanelViewModel.cs
+++ RDMP.Avalonia.UI/ViewModels/LeftHandPanelViewModel.cs
@@ -14,15 +14,29 @@ namespace RDMP.Avalonia.UI.ViewModels
         public MainAppViewModel.Tabs SelectedTab { get => _selectedTab; private set => this.RaiseAndSetIfChanged(ref _selectedTab,value); }
 
 
+        private ReactiveObject _currentViewModel;
+        public ReactiveObject CurrentViewModel { get => _currentViewModel; private set => this.RaiseAndSetIfChanged(ref _currentViewModel,value); }
+
+        private ReactiveObject GetCurrentViewModel(MainAppViewModel.Tabs t)
+        {
+            return new CatalogueTreeViewModel();
+            //switch (t)
+            //{
+            //    case MainAppViewModel.Tabs.Catalogue: return new CatalogueTreeViewModel(); default :null;
+            //}
+        }
+
         private void SelectedTabChange(MainAppViewModel.Tabs st)
         {
             SelectedTab = st;
+            CurrentViewModel = GetCurrentViewModel(st);
+
         }
 
         public LeftHandPanelViewModel()
         {
-            //SelectedTab = Globals.SelectedTab;
             Globals.SelectedTabChange += SelectedTabChange;
+            _currentViewModel = new ReactiveObject();
         }
     }
 }
