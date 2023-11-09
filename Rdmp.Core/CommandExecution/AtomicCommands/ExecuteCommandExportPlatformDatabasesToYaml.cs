ee1d1920229abf616fa6735f25db2f4259a068c4
James Friel <jfriel001@dundee.ac.uk>
2023-11-09 09:35:52 +0000

    tidy up

--- Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandExportPlatformDatabasesToYaml.cs
+++ Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandExportPlatformDatabasesToYaml.cs
@@ -18,6 +18,7 @@ using Rdmp.Core.Curation.Data.Remoting;
 using Rdmp.Core.DataExport.Data;
 using Rdmp.Core.MapsDirectlyToDatabaseTable;
 using System.Linq;
+using static Rdmp.Core.ReusableLibraryCode.Diff;
 
 namespace Rdmp.Core.CommandExecution.AtomicCommands
 {
@@ -27,14 +28,16 @@ namespace Rdmp.Core.CommandExecution.AtomicCommands
     {
         private readonly TableRepository _catalogueRepository;
         private readonly TableRepository _dataExportRepository;
-        private readonly string _outputFile;
+        private readonly string _outputDirectory;
+        private readonly IBasicActivateItems _activator;
 
-        public ExecuteCommandExportPlatformDatabasesToYaml(IBasicActivateItems activator, [DemandsInitialization("Where the yaml file should be created")] string outputfile)
+        public ExecuteCommandExportPlatformDatabasesToYaml(IBasicActivateItems activator, [DemandsInitialization("Where the yaml file should be created")] string outputDirectory)
         {
 
             _catalogueRepository = activator.RepositoryLocator.CatalogueRepository as TableRepository;
             _dataExportRepository = activator.RepositoryLocator.DataExportRepository as TableRepository;
-            _outputFile = outputfile;
+            _outputDirectory = outputDirectory;
+            _activator = activator;
 
         }
 
@@ -43,79 +46,26 @@ namespace Rdmp.Core.CommandExecution.AtomicCommands
             base.Execute();
             Args args = new Args()
             {
-                Dir = _outputFile,
+                Dir = _outputDirectory,
             };
             var yamlRespositoryLocator = args.GetRepositoryLocator();
-            _catalogueRepository.GetAllObjects<Catalogue>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<CohortAggregateContainer>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<CohortIdentificationConfiguration>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<GovernanceDocument>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<GovernancePeriod>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<StandardRegex>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<AnyTableSqlParameter>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ANOTable>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<AggregateConfiguration>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<AggregateContinuousDateAxis>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<AggregateDimension>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<AggregateFilter>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<AggregateFilterContainer>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<AggregateFilterParameter>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<CatalogueItem>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ColumnInfo>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<JoinableCohortAggregateConfiguration>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<JoinableCohortAggregateConfigurationUse>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ExternalDatabaseServer>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ExtractionFilter>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ExtractionFilterParameter>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ExtractionInformation>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ExtractionFilterParameterSet>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<LoadMetadata>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ExtractionFilterParameterSetValue>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<LoadProgress>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<Favourite>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<Pipeline>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<Lookup>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<AggregateTopX>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<PipelineComponent>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<LookupCompositeJoinInfo>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<PipelineComponentArgument>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<PreLoadDiscardedColumn>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ProcessTask>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<DashboardLayout>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ProcessTaskArgument>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<DashboardControl>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<DataAccessCredentials>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<SupportingDocument>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<DashboardObjectUse>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<SupportingSQLTable>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<TableInfo>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<RemoteRDMP>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ObjectImport>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ObjectExport>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<CacheProgress>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<ConnectionStringKeyword>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<WindowLayout>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<PermissionWindow>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<TicketingSystemConfiguration>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
-            _catalogueRepository.GetAllObjects<CacheFetchFailure>().ToList().ForEach(item => yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(item));
+            foreach (var t in _catalogueRepository.GetCompatibleTypes())
+            {
+                foreach(var o in _activator.GetRepositoryFor(t).GetAllObjects(t))
+                {
+                    yamlRespositoryLocator.CatalogueRepository.SaveToDatabase(o);
 
+                }
+            }
+            foreach (var t in _dataExportRepository.GetCompatibleTypes())
+            {
+                foreach (var o in _activator.GetRepositoryFor(t).GetAllObjects(t))
+                {
+                    yamlRespositoryLocator.DataExportRepository.SaveToDatabase(o);
 
-            _dataExportRepository.GetAllObjects<SupplementalExtractionResults>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<CumulativeExtractionResults>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<DeployedExtractionFilter>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<DeployedExtractionFilterParameter>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<ExternalCohortTable>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<ExtractableCohort>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<ExtractableColumn>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<ExtractableDataSet>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<ExtractionConfiguration>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<FilterContainer>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<GlobalExtractionFilterParameter>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<Project>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<SelectedDataSets>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<ExtractableDataSetPackage>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<ProjectCohortIdentificationConfigurationAssociation>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
-            _dataExportRepository.GetAllObjects<SelectedDataSetsForcedJoin>().ToList().ForEach(item => yamlRespositoryLocator.DataExportRepository.SaveToDatabase(item));
+                }
+            }
+
         }
     }
 }
