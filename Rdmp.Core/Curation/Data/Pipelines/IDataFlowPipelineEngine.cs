2a5262e6ed3c55eb4fe6d0c7ac3a11132ab4aa02
James Friel <jfriel001@dundee.ac.uk>
2023-11-09 21:37:22 +0000

    RDMP-108 Unified UI Alerting from Processing Chunks (#1663)
    * batched ui
    
    * tidy up
    
    * attempt to fix codeql
    
    * fix linting
    
    * add if check
    
    * fix test
    
    * add pragma
    
    * fix up test
    
    * update from codeql
    
    * Update DataFlowPipelineEngine.cs
    
    Tidy thread logic, fix double-disposal of objects
    
    * Put chunk disposal in a finally block to avoid leaks
    
    ---------
    
    Co-authored-by: James A Sutherland <j.a.sutherland@dundee.ac.uk>
    Co-authored-by: James A Sutherland <>

--- Rdmp.Core/Curation/Data/Pipelines/IDataFlowPipelineEngine.cs
+++ Rdmp.Core/Curation/Data/Pipelines/IDataFlowPipelineEngine.cs
@@ -4,7 +4,9 @@
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
+using System;
 using System.Collections.Generic;
+using Rdmp.Core.CommandExecution;
 using Rdmp.Core.DataFlowPipeline;
 using Rdmp.Core.ReusableLibraryCode.Checks;
 
@@ -31,8 +33,9 @@ public interface IDataFlowPipelineEngine : ICheckable
     /// Runs the source GetChunk once and passes it down through the other components to the destination
     /// </summary>
     /// <param name="cancellationToken"></param>
+    /// <param name="completionUIAlerts"></param>
     /// <returns></returns>
-    bool ExecuteSinglePass(GracefulCancellationToken cancellationToken);
+    bool ExecuteSinglePass(GracefulCancellationToken cancellationToken, List<Tuple<string,IBasicActivateItems>> completionUIAlerts = null);
 
     /// <summary>
     /// Components can declare IPipelineRequirement, calling this method will PreInitialize all components with compatible IPipelineRequirements with the values
