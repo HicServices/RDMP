fd78d7436bd65c56a883f4d7fe55468872cb648e
James Friel <jfriel001@dundee.ac.uk>
2023-11-13 15:14:24 +0000

    add nav button

--- Application/ResearchDataManagementPlatform/WindowManagement/WindowManager.cs
+++ Application/ResearchDataManagementPlatform/WindowManagement/WindowManager.cs
@@ -148,6 +148,12 @@ public class WindowManager
                     Image.Load<Rgba32>(CatalogueIcons.Favourite));
                 break;
 
+            case RDMPCollection.Datasets:
+                collection = new FavouritesCollectionUI();
+                toReturn = Show(RDMPCollection.Datasets, collection, "Datasets",
+                    Image.Load<Rgba32>(CatalogueIcons.Favourite));
+                break;
+
             default: throw new ArgumentOutOfRangeException(nameof(collectionToCreate));
         }
 
