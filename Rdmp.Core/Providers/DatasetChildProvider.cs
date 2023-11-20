b44457af7aa02eac2fdce3dd4aa7009cc26c8089
James Friel <JFriel001@dundee.ac.uk>
2023-11-20 08:22:28 +0000

    add missing file

---
+++ Rdmp.Core/Providers/DatasetChildProvider.cs
@@ -0,0 +1,16 @@
+﻿using Rdmp.Core.Repositories;
+using Rdmp.Core.ReusableLibraryCode.Checks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.Core.Providers;
+public class DatasetChildProvider : CatalogueChildProvider
+{
+    public DatasetChildProvider(ICatalogueRepository repository, IChildProvider[] pluginChildProviders, ICheckNotifier errorsCheckNotifier, CatalogueChildProvider previousStateIfKnown) : base(repository, pluginChildProviders, errorsCheckNotifier, previousStateIfKnown)
+    {
+    }
+}
+
