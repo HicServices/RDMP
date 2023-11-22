733b32176110170fef8c4489fdc0a46a37b42ddb
James Friel <JFriel001@dundee.ac.uk>
2023-11-22 09:34:02 +0000

    add charts

---
+++ Rdmp.UI/PieCharts/CatalogueToDatasetLinkagePieChart.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Rdmp.UI.PieCharts
+{
+    partial class CatalogueToDatasetLinkagePieChart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "CatalogueToDatasetLinkagePieChart";
+            this.gbWhatThisIs = new System.Windows.Forms.GroupBox();//TODO
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.DarkGray;
+            this.chart1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+            this.chart1.BorderSkin.PageColor = System.Drawing.Color.Black;
+            chartArea1.Area3DStyle.Enable3D = true;
+            chartArea1.Area3DStyle.WallWidth = 10;
+            chartArea1.BackColor = System.Drawing.Color.DarkGray;
+            chartArea1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.BackColor = System.Drawing.Color.Gainsboro;
+            legend1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+            legend1.BorderColor = System.Drawing.Color.White;
+            legend1.BorderWidth = 2;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 16);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chart1.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Red,
+        System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))))};
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.CustomProperties = "PieDrawingStyle=SoftEdge, PieLabelStyle=Disabled";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(387, 203);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // gbWhatThisIs
+            // 
+            this.gbWhatThisIs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            //this.gbWhatThisIs.Controls.Add(this.btnViewDataTable);
+            //this.gbWhatThisIs.Controls.Add(this.lblNoIssues);
+            this.gbWhatThisIs.Controls.Add(this.chart1);
+            this.gbWhatThisIs.Location = new System.Drawing.Point(0, 28);
+            this.gbWhatThisIs.Name = "gbWhatThisIs";
+            this.gbWhatThisIs.Size = new System.Drawing.Size(393, 222);
+            this.gbWhatThisIs.TabIndex = 1;
+            this.gbWhatThisIs.TabStop = false;
+            this.gbWhatThisIs.Text = "What This Is";
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbWhatThisIs;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        //private System.Windows.Forms.Label lblNoIssues;
+    }
+}
---
+++ Rdmp.UI/PieCharts/CatalogueToDatasetLinkagePieChart.cs
@@ -0,0 +1,112 @@
+﻿using Rdmp.Core.Curation.Data.Dashboarding;
+using Rdmp.UI.DashboardTabs.Construction;
+using Rdmp.UI.ItemActivation;
+using Rdmp.UI.Refreshing;
+using Rdmp.UI.TestsAndSetup.ServicePropogation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rdmp.UI.PieCharts;
+
+public partial class CatalogueToDatasetLinkagePieChart : RDMPUserControl, IDashboardableControl
+{
+    private DashboardControl _dashboardControlDatabaseRecord;
+    private CatalogueToDatasetObjectCollection _collection;
+    public CatalogueToDatasetLinkagePieChart()
+    {
+        InitializeComponent();
+    }
+
+    public IPersistableObjectCollection ConstructEmptyCollection(DashboardControl databaseRecord)
+    {
+        _dashboardControlDatabaseRecord = databaseRecord;
+        return new CatalogueToDatasetObjectCollection();
+    }
+
+    public IPersistableObjectCollection GetCollection()
+    {
+        return _collection;
+    }
+
+    public string GetTabName()
+    {
+        return Text;
+    }
+
+    public string GetTabToolTip()
+    {
+        return null;
+    }
+
+    public void NotifyEditModeChange(bool isEditModeOn)
+    {
+        var l = new Point(Margin.Left, Margin.Right);
+        var s = new Size(Width - Margin.Horizontal, Height - Margin.Vertical);
+
+        CommonFunctionality.ToolStrip.Visible = isEditModeOn;
+
+        if (isEditModeOn)
+        {
+            gbWhatThisIs.Location = l with { Y = l.Y + 25 }; //move it down 25 to allow space for tool bar
+            gbWhatThisIs.Size = s with { Height = s.Height - 25 }; //and adjust height accordingly
+        }
+        else
+        {
+            gbWhatThisIs.Location = l;
+            gbWhatThisIs.Size = s;
+        }
+    }
+
+    public void RefreshBus_RefreshObject(object sender, RefreshObjectEventArgs e)
+    {
+        //TOD this does nothing right now
+    }
+
+    //private bool _bLoading;
+    //private bool firstTime = true;
+
+    public void SetCollection(IActivateItems activator, IPersistableObjectCollection collection)
+    {
+        SetItemActivator(activator);
+
+        _collection = (CatalogueToDatasetObjectCollection)collection;
+
+        //if (firstTime)
+        //    SetupFlags();
+
+        //btnAllCatalogues.Checked = !_collection.IsSingleCatalogueMode;
+        //btnSingleCatalogue.Checked = _collection.IsSingleCatalogueMode;
+        //btnShowLabels.Checked = _collection.ShowLabels;
+
+        //CommonFunctionality.Add(btnAllCatalogues);
+        //CommonFunctionality.Add(toolStripLabel1);
+        //CommonFunctionality.Add(btnAllCatalogues);
+        //CommonFunctionality.Add(btnSingleCatalogue);
+        //CommonFunctionality.Add(btnShowLabels);
+        //CommonFunctionality.Add(btnRefresh);
+
+        //foreach (var mi in _flagOptions)
+        //    CommonFunctionality.AddToMenu(mi);
+
+        GenerateChart();
+        //_bLoading = false;
+    }
+    private void GenerateChart()
+    {
+        chart1.Visible = false;
+        //lblNoIssues.Visible = false;
+        gbWhatThisIs.Text = "Hello1";
+        //gbWhatThisIs.Text = _collection.IsSingleCatalogueMode
+        //    ? $"Column Descriptions in {_collection.GetSingleCatalogueModeCatalogue()}"
+        //    : "Column Descriptions";
+
+    }
+
+}
