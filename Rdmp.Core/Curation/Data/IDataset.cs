2f66320b24a1eb6857476bfb3c447ce3e57ee022
James Friel <JFriel001@dundee.ac.uk>
2023-11-23 12:45:40 +0000

    tidy up documentation

--- Rdmp.Core/Curation/Data/IDataset.cs
+++ Rdmp.Core/Curation/Data/IDataset.cs
@@ -1,4 +1,10 @@
-﻿using Rdmp.Core.Repositories;
+﻿// Copyright (c) The University of Dundee 2018-2023
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+using Rdmp.Core.MapsDirectlyToDatabaseTable;
+using Rdmp.Core.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +13,7 @@ using System.Threading.Tasks;
 
 namespace Rdmp.Core.Curation.Data;
 
-public interface IDataset
+public interface IDataset: IMapsDirectlyToDatabaseTable
 {
     /// <summary>
     /// Returns where the object exists (e.g. database) as <see cref="ICatalogueRepository"/> or null if the object does not exist in a catalogue repository.
--- Rdmp.UI/Menus/DatasetMenu.cs
+++ Rdmp.UI/Menus/DatasetMenu.cs
@@ -1,4 +1,8 @@
-﻿using Rdmp.Core.Curation.Data;
+﻿// Copyright (c) The University of Dundee 2018-2023
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.using Rdmp.Core.Curation.Data;
 using Rdmp.UI.CommandExecution.AtomicCommands;
 using System;
 using System.Collections.Generic;
@@ -13,6 +17,6 @@ public class DatasetMenu: RDMPContextMenuStrip
 
     public DatasetMenu(RDMPContextMenuStripArgs args, Dataset dataset): base(args, dataset)
     {
-        Add(new ExecuteCommandDeleteDataset(_activator,dataset));
+        Add(new ExecuteCommandDeleteDatasetUI(_activator,dataset));
     }
 }
