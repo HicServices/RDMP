6710183d9da243d2cd37e7cedfdd1df9d354591c
James Friel <JFriel001@dundee.ac.uk>
2023-11-23 16:10:53 +0000

    working patch

--- ReactUI/ReactUI.Server/Controllers/CatalogueController.cs
+++ ReactUI/ReactUI.Server/Controllers/CatalogueController.cs
@@ -3,6 +3,8 @@ using Rdmp.Core.CommandExecution;
 using Rdmp.Core.Curation.Data;
 using Rdmp.Core.Repositories;
 using ReactUI.Server.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ReactUI.Server.Controllers
 {
@@ -16,13 +18,6 @@ namespace ReactUI.Server.Controllers
         {
             _logger = logger;
         }
-
-        //[HttpGet(Name = "GetCatalogues")]
-        //public IEnumerable<object> Get()
-        //{
-        //    var catalogues = RDMPInitialiser._startup.RepositoryLocator.CatalogueRepository.GetAllObjects<Catalogue>();
-        //    return catalogues.Select(x => new CatalogueModel(x.ID,x.Name,x.Description));
-        //}
         [HttpGet(Name = "GetCatalogueById")]
         public CatalogueModel GetCatalogueById(int id)
         {
@@ -32,6 +27,21 @@ namespace ReactUI.Server.Controllers
                 return new CatalogueModel(catalogue);
             }
             return null;
+        }
+
+        [HttpPatch(Name = "UpdateCatalogue")]
+        public CatalogueModel UpdateCatalogue(int id, string body) {
+            var catalogue = RDMPInitialiser._startup.RepositoryLocator.CatalogueRepository.GetAllObjects<Catalogue>().Where(c => c.ID == id).First();
+            var x = JsonSerializer.Deserialize<Dictionary<string, string>>(body);
+
+            if (catalogue is not null)
+            {
+                catalogue.Description = x["description"] is not null ? x["description"] : catalogue.Description;
+                catalogue.SaveToDatabase();
+                return new CatalogueModel(catalogue);
+            }
+            return null;
+
         }
     }
 }
