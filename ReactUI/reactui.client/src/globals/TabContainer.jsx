10669da47230125c1e99bda50fabbffabdc1e1a8
James Friel <JFriel001@dundee.ac.uk>
2023-11-23 15:58:23 +0000

    add logo

--- ReactUI/reactui.client/src/globals/TabContainer.jsx
+++ ReactUI/reactui.client/src/globals/TabContainer.jsx
@@ -3,6 +3,7 @@
 function TabContainer(props) {
     return (
         <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <img src="https://raw.githubusercontent.com/HicServices/RDMP/develop/Application/ResearchDataManagementPlatform/Icon/mainsmall.png" style={{ width: '50px',height:'50px',marginLeft: '5px', marginRight:'5px' } } />
             <button onClick={()=>props.onClick(0) }> Home</button>
             <button onClick={() => props.onClick(1)}>Catalogues</button>
       </div>
