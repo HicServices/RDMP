57958f75515c1a7e98882e7e7689ad2c57216892
James Friel <JFriel001@dundee.ac.uk>
2023-11-23 15:16:00 +0000

    add missing files

---
+++ ReactUI/ReactUI.Server/Models/Catalogue.cs
@@ -0,0 +1,19 @@
+﻿namespace ReactUI.Server.Models
+{
+    public class Catalogue
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public string Description { get; set; }
+
+        public Catalogue() { }
+        public Catalogue(int id, string name, string description)
+        {
+            Name = name;
+            ID = id;
+            Description = description;
+        }
+
+
+    }
+}
