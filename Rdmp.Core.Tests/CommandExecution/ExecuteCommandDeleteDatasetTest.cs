493f03bb9bf22cb1f546cff6c5e6c0a782db0110
jas88 <j.a.sutherland@dundee.ac.uk>
2023-12-05 10:09:42 -0600

    Update ExecuteCommandDeleteDatasetTest.cs
    NUnit 4 API fixup

--- Rdmp.Core.Tests/CommandExecution/ExecuteCommandDeleteDatasetTest.cs
+++ Rdmp.Core.Tests/CommandExecution/ExecuteCommandDeleteDatasetTest.cs
@@ -16,21 +16,21 @@ namespace Rdmp.Core.Tests.CommandExecution
         {
             var cmd = new ExecuteCommandCreateDataset(GetMockActivator(), "dataset");
             Assert.DoesNotThrow(() => cmd.Execute());
-            Assert.That(GetMockActivator().RepositoryLocator.CatalogueRepository.GetAllObjects<Rdmp.Core.Curation.Data.Dataset>().Length,Is.EqualTo(1));
+            Assert.That(GetMockActivator().RepositoryLocator.CatalogueRepository.GetAllObjects<Rdmp.Core.Curation.Data.Dataset>(), Has.Length.EqualTo(1));
             var founddataset = GetMockActivator().RepositoryLocator.CatalogueRepository.GetAllObjects<Rdmp.Core.Curation.Data.Dataset>().Where(ds => ds.Name == "dataset").First();
             var delCmd = new ExecuteCommandDeleteDataset(GetMockActivator(), founddataset);
             Assert.DoesNotThrow(() => delCmd.Execute());
-            Assert.That(GetMockActivator().RepositoryLocator.CatalogueRepository.GetAllObjects<Rdmp.Core.Curation.Data.Dataset>().Length, Is.EqualTo(0));
+            Assert.That(GetMockActivator().RepositoryLocator.CatalogueRepository.GetAllObjects<Rdmp.Core.Curation.Data.Dataset>(), Is.Empty);
         }
 
         [Test]
         public void TestDeleteNonExistantDataset()
         {
-            Assert.That(GetMockActivator().RepositoryLocator.CatalogueRepository.GetAllObjects<Rdmp.Core.Curation.Data.Dataset>().Length, Is.EqualTo(0));
+            Assert.That(GetMockActivator().RepositoryLocator.CatalogueRepository.GetAllObjects<Rdmp.Core.Curation.Data.Dataset>(), Is.Empty);
             var founddataset = new Core.Curation.Data.Dataset();
             var delCmd = new ExecuteCommandDeleteDataset(GetMockActivator(), founddataset);
             Assert.Throws<NullReferenceException>(() => delCmd.Execute());
-            Assert.That(GetMockActivator().RepositoryLocator.CatalogueRepository.GetAllObjects<Rdmp.Core.Curation.Data.Dataset>().Length, Is.EqualTo(0));
+            Assert.That(GetMockActivator().RepositoryLocator.CatalogueRepository.GetAllObjects<Rdmp.Core.Curation.Data.Dataset>(), Is.Empty);
         }
     }
 }
