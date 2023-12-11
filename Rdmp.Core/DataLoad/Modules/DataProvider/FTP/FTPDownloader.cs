d43e6c2fcdb3d1e11cd68fadfb0ea58c37329acf
James Friel <JFriel001@dundee.ac.uk>
2023-12-11 09:22:38 +0000

    readd missing files

---
+++ Rdmp.Core/DataLoad/Modules/DataProvider/FTP/FTPDownloader.cs
@@ -0,0 +1,194 @@
+// Copyright (c) The University of Dundee 2018-2019
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Threading;
+using FAnsi.Discovery;
+using FluentFTP;
+using Rdmp.Core.Curation;
+using Rdmp.Core.Curation.Data;
+using Rdmp.Core.DataFlowPipeline;
+using Rdmp.Core.DataLoad.Engine.DataProvider;
+using Rdmp.Core.DataLoad.Engine.Job;
+using Rdmp.Core.ReusableLibraryCode.Checks;
+using Rdmp.Core.ReusableLibraryCode.Progress;
+
+namespace Rdmp.Core.DataLoad.Modules.DataProvider.FTP;
+
+/// <summary>
+/// load component which downloads files from a remote FTP server to the ForLoading directory
+/// 
+/// <para>Attempts to connect to the FTP server and download all files in the landing folder of the FTP (make sure you really want everything in the
+///  root folder - if not then configure redirection on the FTP, so you land in the correct directory).  Files are downloaded into the ForLoading folder</para>
+/// </summary>
+public class FTPDownloader : IPluginDataProvider
+{
+    private readonly Lazy<FtpClient> _connection;
+    protected readonly List<string> _filesRetrieved = new();
+    private ILoadDirectory? _directory;
+
+    public FTPDownloader()
+    {
+        _connection = new Lazy<FtpClient>(SetupFtp, LazyThreadSafetyMode.ExecutionAndPublication);
+    }
+
+    [DemandsInitialization(
+        "Determines the behaviour of the system when no files are found on the server.  If true the entire data load process immediately stops with exit code LoadNotRequired, if false then the load proceeds as normal (useful if for example if you have multiple Attachers and some files are optional)")]
+    public bool SendLoadNotRequiredIfFileNotFound { get; set; }
+
+    [DemandsInitialization(
+        "The Regex expression to validate files on the FTP server against, only files matching the expression will be downloaded")]
+    public Regex? FilePattern { get; set; }
+
+    [DemandsInitialization("The timeout to use when connecting to the FTP server in SECONDS")]
+    public int TimeoutInSeconds { get; set; }
+
+    [DemandsInitialization(
+        "Tick to delete files from the FTP server when the load is successful (ends with .Success not .OperationNotRequired - which happens when LoadNotRequired state).  This will only delete the files if they were actually fetched from the FTP server.  If the files were already in forLoading then the remote files are not deleted")]
+    public bool DeleteFilesOffFTPServerAfterSuccesfulDataLoad { get; set; }
+
+    [DemandsInitialization(
+        "The FTP server to connect to.  Server should be specified with only IP:Port e.g. 127.0.0.1:20.  You do not have to specify ftp:// at the start",
+        Mandatory = true)]
+    public ExternalDatabaseServer? FTPServer { get; set; }
+
+    [DemandsInitialization("The directory on the FTP server that you want to download files from")]
+    public string? RemoteDirectory { get; set; }
+
+    [DemandsInitialization("True to set keep alive", DefaultValue = true)]
+    public bool KeepAlive { get; set; }
+
+
+    public void Initialize(ILoadDirectory directory, DiscoveredDatabase dbInfo)
+    {
+        _directory = directory;
+    }
+
+    public ExitCodeType Fetch(IDataLoadJob job, GracefulCancellationToken cancellationToken)
+    {
+        return DownloadFilesOnFTP(_directory ?? throw new InvalidOperationException("No output directory set"), job);
+    }
+
+    private FtpClient SetupFtp()
+    {
+        var host = FTPServer?.Server ?? throw new NullReferenceException("FTP server not set");
+        var username = FTPServer.Username ?? "anonymous";
+        var password = string.IsNullOrWhiteSpace(FTPServer.Password) ? "guest" : FTPServer.GetDecryptedPassword();
+        var c = new FtpClient(host, username, password);
+        c.AutoConnect();
+        return c;
+    }
+
+    private ExitCodeType DownloadFilesOnFTP(ILoadDirectory destination, IDataLoadEventListener listener)
+    {
+        var files = GetFileList().ToArray();
+
+        listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information,
+            $"Identified the following files on the FTP server:{string.Join(',', files)}"));
+
+        var forLoadingContainedCachedFiles = false;
+
+        foreach (var file in files)
+        {
+            var action = GetSkipActionForFile(file, destination);
+
+            listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information,
+                $"File {file} was evaluated as {action}"));
+
+            switch (action)
+            {
+                case SkipReason.DoNotSkip:
+                    listener.OnNotify(this,
+                        new NotifyEventArgs(ProgressEventType.Information, $"About to download {file}"));
+                    Download(file, destination);
+                    break;
+                case SkipReason.InForLoading:
+                    forLoadingContainedCachedFiles = true;
+                    break;
+            }
+        }
+
+        // it was a success - even if no files were actually retrieved... hey that's what the user said, otherwise he would have set SendLoadNotRequiredIfFileNotFound
+        if (forLoadingContainedCachedFiles || _filesRetrieved.Any() || !SendLoadNotRequiredIfFileNotFound)
+            return ExitCodeType.Success;
+
+        // if no files were downloaded (and there were none skipped because they were in forLoading) and in that eventuality we have our flag set to return LoadNotRequired then do so
+        listener.OnNotify(this,
+            new NotifyEventArgs(ProgressEventType.Information,
+                "Could not find any files on the remote server worth downloading, so returning LoadNotRequired"));
+        return ExitCodeType.OperationNotRequired;
+    }
+
+    protected enum SkipReason
+    {
+        DoNotSkip,
+        InForLoading,
+        DidNotMatchPattern,
+        IsImaginaryFile
+    }
+
+    protected SkipReason GetSkipActionForFile(string file, ILoadDirectory destination)
+    {
+        if (file.StartsWith(".", StringComparison.Ordinal))
+            return SkipReason.IsImaginaryFile;
+
+        //if there is a regex pattern
+        if (FilePattern?.IsMatch(file) == false) //and it does not match
+            return SkipReason.DidNotMatchPattern; //skip because it did not match pattern
+
+        //if the file on the FTP already exists in the forLoading directory, skip it
+        return destination.ForLoading.GetFiles(file).Any() ? SkipReason.InForLoading : SkipReason.DoNotSkip;
+    }
+
+
+    private static bool ValidateServerCertificate(object _1, X509Certificate _2, X509Chain _3,
+        SslPolicyErrors _4) => true; //any cert will do! yay
+
+
+    protected virtual IEnumerable<string> GetFileList()
+    {
+        return _connection.Value.GetNameListing().ToList().Where(_connection.Value.FileExists);
+    }
+
+    protected virtual void Download(string file, ILoadDirectory destination)
+    {
+        var remotePath = !string.IsNullOrWhiteSpace(RemoteDirectory)
+            ? $"{RemoteDirectory}/{file}"
+            : file;
+
+        var destinationFileName = Path.Combine(destination.ForLoading.FullName, file);
+        _connection.Value.DownloadFile(destinationFileName, remotePath);
+        _filesRetrieved.Add(remotePath);
+    }
+
+    public virtual void LoadCompletedSoDispose(ExitCodeType exitCode, IDataLoadEventListener postLoadEventListener)
+    {
+        if (exitCode != ExitCodeType.Success || !DeleteFilesOffFTPServerAfterSuccesfulDataLoad) return;
+
+        foreach (var file in _filesRetrieved) _connection.Value.DeleteFile(file);
+    }
+
+
+    public void Check(ICheckNotifier notifier)
+    {
+        try
+        {
+            SetupFtp();
+        }
+        catch (Exception e)
+        {
+            notifier.OnCheckPerformed(new CheckEventArgs("Failed to SetupFTP", CheckResult.Fail, e));
+        }
+    }
+}
---
+++ Rdmp.Core/DataLoad/Modules/DataProvider/FTP/SFTPDownloader.cs
@@ -0,0 +1,88 @@
+// Copyright (c) The University of Dundee 2018-2019
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Rdmp.Core.Curation;
+using Rdmp.Core.Curation.Data;
+using Rdmp.Core.ReusableLibraryCode.Progress;
+using Renci.SshNet;
+
+namespace Rdmp.Core.DataLoad.Modules.DataProvider.FTP;
+
+/// <summary>
+/// load component which downloads files from a remote SFTP (Secure File Transfer Protocol) server to the ForLoading directory
+/// 
+/// <para>Operates in the same way as <see cref="FTPDownloader"/> except that it uses SSH.  In addition, this
+/// class will not bother downloading any files that already exist in the forLoading directory (have the same name - file size is NOT checked)</para>
+/// </summary>
+public class SFTPDownloader : FTPDownloader
+{
+    [DemandsInitialization("The keep-alive interval.  In milliseconds.  Requires KeepAlive to be set to take effect.")]
+    public int KeepAliveIntervalMilliseconds { get; set; }
+
+    private readonly Lazy<SftpClient> _connection;
+
+    public SFTPDownloader(Lazy<SftpClient> connection)
+    {
+        _connection = new Lazy<SftpClient>(SetupSftp, LazyThreadSafetyMode.ExecutionAndPublication);
+    }
+
+    private SftpClient SetupSftp()
+    {
+        var host = FTPServer?.Server ?? throw new NullReferenceException("FTP server not set");
+        var username = FTPServer.Username ?? "anonymous";
+        var password = string.IsNullOrWhiteSpace(FTPServer.Password) ? "guest" : FTPServer.GetDecryptedPassword();
+        var c = new SftpClient(host, username, password);
+        c.Connect();
+        return c;
+    }
+
+
+    protected override void Download(string file, ILoadDirectory destination)
+    {
+        if (file.Contains('/') || file.Contains('\\'))
+            throw new Exception("Was not expecting a relative path here");
+
+        //if there is a specified remote directory then reference it otherwise reference it locally (or however we were told about it from GetFileList())
+        var fullFilePath = !string.IsNullOrWhiteSpace(RemoteDirectory) ? Path.Combine(RemoteDirectory, file) : file;
+
+        var destinationFilePath = Path.Combine(destination.ForLoading.FullName, file);
+
+        using (var dest = File.Create(destinationFilePath))
+            _connection.Value.DownloadFile(fullFilePath, dest);
+        _filesRetrieved.Add(fullFilePath);
+    }
+
+
+    public override void LoadCompletedSoDispose(ExitCodeType exitCode, IDataLoadEventListener postLoadEventListener)
+    {
+        if (exitCode != ExitCodeType.Success) return;
+
+        foreach (var retrievedFiles in _filesRetrieved)
+            try
+            {
+                _connection.Value.DeleteFile(retrievedFiles);
+                postLoadEventListener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information,
+                    $"Deleted SFTP file {retrievedFiles} from SFTP server"));
+            }
+            catch (Exception e)
+            {
+                postLoadEventListener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Error,
+                    $"Could not delete SFTP file {retrievedFiles} from SFTP server", e));
+            }
+    }
+
+
+    protected override string[] GetFileList()
+    {
+        var directory = string.IsNullOrWhiteSpace(RemoteDirectory) ? "." : RemoteDirectory;
+
+        return _connection.Value.ListDirectory(directory).Select(static d => d.Name).ToArray();
+    }
+}
