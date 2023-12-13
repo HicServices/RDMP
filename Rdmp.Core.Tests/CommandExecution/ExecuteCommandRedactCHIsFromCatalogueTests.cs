65c1500243d838c78a4524c32b67b991a825fa42
James Friel <JFriel001@dundee.ac.uk>
2023-12-13 11:32:35 +0000

    redact chi test

---
+++ Rdmp.Core.Tests/CommandExecution/ExecuteCommandRedactCHIsFromCatalogueTests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using Rdmp.Core.CommandExecution;
+using Rdmp.Core.CommandExecution.AtomicCommands;
+using Rdmp.Core.CommandLine.DatabaseCreation;
+using Rdmp.Core.Curation.Data;
+using Rdmp.Core.ReusableLibraryCode.Checks;
+using Spectre.Console.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Common;
+
+namespace Rdmp.Core.Tests.CommandExecution;
+
+internal class ExecuteCommandRedactCHIsFromCatalogueTests: DatabaseTests
+{
+
+    [Test]
+    public void RedactCHIsFromCatalogue_ValidSingle()
+    {
+        var db = GetCleanedServer(FAnsi.DatabaseType.MicrosoftSQLServer);
+        var pipes = new CataloguePipelinesAndReferencesCreation(RepositoryLocator, null, null);
+        pipes.CreatePipelines(new PlatformDatabaseCreationOptions());
+        var creator = new ExampleDatasetsCreation(new ThrowImmediatelyActivator(RepositoryLocator), RepositoryLocator);
+        creator.Create(db, ThrowImmediatelyCheckNotifier.Quiet,
+            new PlatformDatabaseCreationOptions { Seed = 500, DropDatabases = true });
+        var biochemistry = CatalogueRepository.GetAllObjects<Catalogue>().Where(c => c.Name == "Biochemistry").First();
+        var tbl = db.DiscoverTables(true).Where(dt => dt.GetRuntimeName().Contains("Biochemistry")).First();
+        var updateSQL = $"UPDATE top (2) {tbl.GetRuntimeName()} set SampleType = 'F1111111111'";
+        using (var con = tbl.Database.Server.GetConnection())
+        {
+            con.Open();
+            var updateCmd = tbl.Database.Server.GetCommand(updateSQL, con);
+            updateCmd.ExecuteNonQuery();
+        }
+        var cmd = new ExecuteCommandRedactCHIsFromCatalogue(new ThrowImmediatelyActivator(RepositoryLocator), biochemistry, null);
+        Assert.DoesNotThrow(() => cmd.Execute());
+        var dt = new DataTable();
+        using (var con = tbl.Database.Server.GetConnection())
+        {
+            using (var findCmd = tbl.Database.Server.GetCommand($"select * from {tbl.GetRuntimeName()} where SampleType = 'F#########1'", con))
+            {
+                using var da = tbl.Database.Server.GetDataAdapter(findCmd);
+                da.Fill(dt);
+            }
+        }
+        Assert.That(dt.Rows.Count, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void RedactCHIsFromCatalogue_Allowlist()
+    {
+        var db = GetCleanedServer(FAnsi.DatabaseType.MicrosoftSQLServer);
+        var pipes = new CataloguePipelinesAndReferencesCreation(RepositoryLocator, null, null);
+        pipes.CreatePipelines(new PlatformDatabaseCreationOptions());
+        var creator = new ExampleDatasetsCreation(new ThrowImmediatelyActivator(RepositoryLocator), RepositoryLocator);
+        creator.Create(db, ThrowImmediatelyCheckNotifier.Quiet,
+            new PlatformDatabaseCreationOptions { Seed = 500, DropDatabases = true });
+        var biochemistry = CatalogueRepository.GetAllObjects<Catalogue>().Where(c => c.Name == "Biochemistry").First();
+        var tbl = db.DiscoverTables(true).Where(dt => dt.GetRuntimeName().Contains("Biochemistry")).First();
+        var updateSQL = $"UPDATE top (2) {tbl.GetRuntimeName()} set SampleType = 'F1111111111'";
+        using (var con = tbl.Database.Server.GetConnection())
+        {
+            con.Open();
+            var updateCmd = tbl.Database.Server.GetCommand(updateSQL, con);
+            updateCmd.ExecuteNonQuery();
+        }
+        var tempFileToCreate = Path.Combine(TestContext.CurrentContext.TestDirectory, "allowList.yaml");
+        var allowList = File.Create(tempFileToCreate);
+        allowList.Close();
+        File.WriteAllLines(tempFileToCreate, new string[] { "RDMP_ALL:", "- SampleType" });
+        var cmd = new ExecuteCommandRedactCHIsFromCatalogue(new ThrowImmediatelyActivator(RepositoryLocator), biochemistry, tempFileToCreate);
+        Assert.DoesNotThrow(() => cmd.Execute());
+        var dt = new DataTable();
+        using (var con = tbl.Database.Server.GetConnection())
+        {
+            using (var findCmd = tbl.Database.Server.GetCommand($"select * from {tbl.GetRuntimeName()} where SampleType = 'F#########1'", con))
+            {
+                using var da = tbl.Database.Server.GetDataAdapter(findCmd);
+                da.Fill(dt);
+            }
+        }
+        Assert.That(dt.Rows.Count, Is.EqualTo(0));
+    }
+}
--- Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandRedactCHIsFromCatalogue.cs
+++ Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandRedactCHIsFromCatalogue.cs
@@ -38,38 +38,8 @@ public class ExecuteCommandRedactCHIsFromCatalogue : BasicCommandExecution, IAto
         _catalogue = catalogue;
         _activator = activator;
         _allowListLocation = allowListLocation;
-        //if (!string.IsNullOrWhiteSpace(allowListLocation))
-        //{
-        //    var allowListFileContent = File.ReadAllText(allowListLocation);
-        //    var deserializer = new DeserializerBuilder().Build();
-        //    var yamlObject = deserializer.Deserialize<Dictionary<string, List<string>>>(allowListFileContent);
-        //    foreach (var (cat, columns) in yamlObject)
-        //    {
-        //        _allowLists.Add(cat, columns);
-        //    }
-        //}
     }
-    //private void handleFoundCHI(string foundChi, string table, string column, string columnValue)
-    //{
-    //    //check for PK
-    //    Console.WriteLine("Found CHI!");
-    //    redactionCount++;
-    //    var rc = new RedactedCHI(_activator.RepositoryLocator.CatalogueRepository, foundChi, ExecuteCommandIdentifyCHIInCatalogue.WrapCHIInContext(foundChi, columnValue, 20), $"{table}.{column}");
-    //    rc.SaveToDatabase();
-    //    var redactedValue = columnValue.Replace(foundChi, $"REDACTED_CHI_{rc.ID}");
-    //    //TODO can be smarted about how we wrote tothe db, can share a connection etc
-    //    var sql = $"UPDATE {table} SET {column}='{redactedValue}' where {column}='{columnValue}'";
-    //    var server = _catalouge.GetDistinctLiveDatabaseServer(DataAccessContext.InternalDataProcessing, false);
-    //    var conn = server.GetConnection();
-    //    conn.Open();
-    //    using (var cmd = server.GetCommand(sql, conn))
-    //    {
-    //        cmd.ExecuteNonQuery();
-    //        conn.Close();
-    //    }
-    //}
-
-
+   
     public override void Execute()
     {
         base.Execute();
@@ -112,39 +82,5 @@ public class ExecuteCommandRedactCHIsFromCatalogue : BasicCommandExecution, IAto
                 }
             }
         }
-        //List<string> columnAllowList = new();
-        //if (_allowLists.TryGetValue("RDMP_ALL", out var _extractionSpecificAllowances))
-        //    columnAllowList.AddRange(_extractionSpecificAllowances);
-        //if (_allowLists.TryGetValue(_catalouge.Name, out var _catalogueSpecificAllowances))
-        //    columnAllowList.AddRange(_catalogueSpecificAllowances.ToList());
-        //foreach (var item in _catalouge.CatalogueItems)
-        //{
-        //    if (columnAllowList.Contains(item.Name)) continue;
-
-        //    var column = item.ColumnInfo.Name;
-        //    int idxOfLastSplit = column.LastIndexOf('.');
-        //    string table = column[..idxOfLastSplit];
-        //    var columnName = column[(idxOfLastSplit + 1)..];
-        //    var server = _catalouge.GetDistinctLiveDatabaseServer(DataAccessContext.InternalDataProcessing, false);
-        //    var sql = $"SELECT {columnName} from {table}";
-        //    var dt = new DataTable();
-        //    dt.BeginLoadData();
-        //    using (var cmd = server.GetCommand(sql, server.GetConnection()))
-        //    {
-        //        using var da = server.GetDataAdapter(cmd);
-        //        da.Fill(dt);
-        //    }
-        //    dt.EndLoadData();
-        //    foreach (DataRow row in dt.Rows)
-        //    {
-
-        //        var value = row[dt.Columns[0].ColumnName].ToString();
-        //        var potentialCHI = CHIColumnFinder.GetPotentialCHI(value);
-        //        if (!string.IsNullOrWhiteSpace(potentialCHI))
-        //        {
-        //            handleFoundCHI(potentialCHI, table,columnName,value);
-        //        }
-        //    }
-        //}
     }
 }
