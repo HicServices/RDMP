020c11f97e07403b056e9c082b7b226e477393af
James Friel <JFriel001@dundee.ac.uk>
2023-12-13 10:28:19 +0000

    add find tests

--- Rdmp.Core.Tests/CommandExecution/ExecuteCommandIdentifyCHIInCatalogueTests.cs
+++ Rdmp.Core.Tests/CommandExecution/ExecuteCommandIdentifyCHIInCatalogueTests.cs
@@ -1,7 +1,13 @@
-﻿using NUnit.Framework;
+﻿using FAnsi;
+using NUnit.Framework;
+using Rdmp.Core.CommandExecution;
+using Rdmp.Core.CommandExecution.AtomicCommands;
+using Rdmp.Core.CommandLine.DatabaseCreation;
 using Rdmp.Core.Curation.Data;
+using Rdmp.Core.ReusableLibraryCode.Checks;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,24 +15,102 @@ using Tests.Common;
 
 namespace Rdmp.Core.Tests.CommandExecution;
 
-internal class ExecuteCommandIdentifyCHIInCatalogueTests: DatabaseTests
+internal class ExecuteCommandIdentifyCHIInCatalogueTests : DatabaseTests
 {
-    //bail out early
-    //allow list
 
     [Test]
     public void IdentifyCHIInCatalogue_Correct()
     {
-        // how do we create a catalogue with data?
-        //var _catalogue1 = new Catalogue(CatalogueRepository, "Dataset1");
-        //var _catalogueItem1 = new CatalogueItem(CatalogueRepository, _catalogue1, "Some Value");
-        //var _table1 = new TableInfo(CatalogueRepository, "Table1");
-        //var _columnInfo1 = new ColumnInfo(CatalogueRepository, "Some Value", "varchar(10)", _table1);
-        //_catalogue1.SaveToDatabase();
-        //_catalogueItem1.SaveToDatabase();
-        //_columnInfo1.SaveToDatabase();
+        var db = GetCleanedServer(FAnsi.DatabaseType.MicrosoftSQLServer);
+        var pipes = new CataloguePipelinesAndReferencesCreation(RepositoryLocator, null, null);
+        pipes.CreatePipelines(new PlatformDatabaseCreationOptions());
+        var creator = new ExampleDatasetsCreation(new ThrowImmediatelyActivator(RepositoryLocator), RepositoryLocator);
+        creator.Create(db, ThrowImmediatelyCheckNotifier.Quiet,
+            new PlatformDatabaseCreationOptions { Seed = 500, DropDatabases = true });
+        var biochemistry = CatalogueRepository.GetAllObjects<Catalogue>().Where(c => c.Name == "Biochemistry").First();
+        var cmd = new ExecuteCommandIdentifyCHIInCatalogue(new ThrowImmediatelyActivator(RepositoryLocator), biochemistry, false, null);
+        Assert.DoesNotThrow(() => cmd.Execute());
+        Assert.That(cmd.foundChis.Rows.Count, Is.EqualTo(0));
+    }
 
-              
+    [Test]
+    public void IdentifyCHIInCatalogue_FoundCHI()
+    {
+        var db = GetCleanedServer(FAnsi.DatabaseType.MicrosoftSQLServer);
+        var creator = new ExampleDatasetsCreation(new ThrowImmediatelyActivator(RepositoryLocator), RepositoryLocator);
+        creator.Create(db, ThrowImmediatelyCheckNotifier.Quiet,
+            new PlatformDatabaseCreationOptions { Seed = 500, DropDatabases = true });
+
+
+        var tbl = db.DiscoverTables(true).Where(dt => dt.GetRuntimeName().Contains("Biochemistry")).First();
+        var updateSQL = $"UPDATE top (2) {tbl.GetRuntimeName()} set SampleType = '1111111111'";
+        using (var con = tbl.Database.Server.GetConnection())
+        {
+            con.Open();
+            var updateCmd = tbl.Database.Server.GetCommand(updateSQL, con);
+            updateCmd.ExecuteNonQuery();
+        }
+
+        var biochemistry = CatalogueRepository.GetAllObjects<Catalogue>().Where(c => c.Name == "Biochemistry").First();
+        var cmd = new ExecuteCommandIdentifyCHIInCatalogue(new ThrowImmediatelyActivator(RepositoryLocator), biochemistry, false, null);
+        Assert.DoesNotThrow(() => cmd.Execute());
+        Assert.That(cmd.foundChis.Rows.Count, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void IdentifyCHIInCatalogue_FoundCHI_BailOuit()
+    {
+        var db = GetCleanedServer(FAnsi.DatabaseType.MicrosoftSQLServer);
+        var creator = new ExampleDatasetsCreation(new ThrowImmediatelyActivator(RepositoryLocator), RepositoryLocator);
+        creator.Create(db, ThrowImmediatelyCheckNotifier.Quiet,
+            new PlatformDatabaseCreationOptions { Seed = 500, DropDatabases = true });
+
+
+        var tbl = db.DiscoverTables(true).Where(dt => dt.GetRuntimeName().Contains("Biochemistry")).First();
+        var updateSQL = $"UPDATE top (2) {tbl.GetRuntimeName()} set SampleType = '1111111111'";
+        using (var con = tbl.Database.Server.GetConnection())
+        {
+            con.Open();
+            var updateCmd = tbl.Database.Server.GetCommand(updateSQL, con);
+            updateCmd.ExecuteNonQuery();
+        }
+
+        var biochemistry = CatalogueRepository.GetAllObjects<Catalogue>().Where(c => c.Name == "Biochemistry").First();
+        var cmd = new ExecuteCommandIdentifyCHIInCatalogue(new ThrowImmediatelyActivator(RepositoryLocator), biochemistry, true, null);
+        Assert.DoesNotThrow(() => cmd.Execute());
+        Assert.That(cmd.foundChis.Rows.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void IdentifyCHIInCatalogue_FoundCHI_AllowList()
+    {
+        var db = GetCleanedServer(FAnsi.DatabaseType.MicrosoftSQLServer);
+
+        //var pipes = new CataloguePipelinesAndReferencesCreation(RepositoryLocator, null, null);
+        //pipes.CreatePipelines(new PlatformDatabaseCreationOptions());
+
+        var creator = new ExampleDatasetsCreation(new ThrowImmediatelyActivator(RepositoryLocator), RepositoryLocator);
+        creator.Create(db, ThrowImmediatelyCheckNotifier.Quiet,
+            new PlatformDatabaseCreationOptions { Seed = 500, DropDatabases = true });
+
+
+        var tbl = db.DiscoverTables(true).Where(dt => dt.GetRuntimeName().Contains("Biochemistry")).First();
+        var updateSQL = $"UPDATE top (2) {tbl.GetRuntimeName()} set SampleType = '1111111111'";
+        using (var con = tbl.Database.Server.GetConnection())
+        {
+            con.Open();
+            var updateCmd = tbl.Database.Server.GetCommand(updateSQL, con);
+            updateCmd.ExecuteNonQuery();
+        }
+
+        var biochemistry = CatalogueRepository.GetAllObjects<Catalogue>().Where(c => c.Name == "Biochemistry").First();
+        var tempFileToCreate = Path.Combine(TestContext.CurrentContext.TestDirectory, "allowList.yaml");
+        var allowList = File.Create(tempFileToCreate);
+        allowList.Close();
+        File.WriteAllLines(tempFileToCreate, new string[] { "RDMP_ALL:", "- SampleType" });
+        var cmd = new ExecuteCommandIdentifyCHIInCatalogue(new ThrowImmediatelyActivator(RepositoryLocator), biochemistry, false, tempFileToCreate);
+        Assert.DoesNotThrow(() => cmd.Execute());
+        Assert.That(cmd.foundChis.Rows.Count, Is.EqualTo(0));
     }
 
 }
--- Rdmp.Core.Tests/CommandExecution/ExecuteCommandRevertRedactedCHITests.cs
+++ Rdmp.Core.Tests/CommandExecution/ExecuteCommandRevertRedactedCHITests.cs
@@ -1,39 +1,39 @@
-﻿//using NUnit.Framework;
-//using Rdmp.Core.CommandExecution.AtomicCommands;
-//using Rdmp.Core.CommandExecution;
-//using Rdmp.Core.Curation.Data;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Tests.Common;
+﻿using NUnit.Framework;
+using Rdmp.Core.CommandExecution.AtomicCommands;
+using Rdmp.Core.CommandExecution;
+using Rdmp.Core.Curation.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Common;
 
-//namespace Rdmp.Core.Tests.CommandExecution;
+namespace Rdmp.Core.Tests.CommandExecution;
 
-//internal class ExecuteCommandRevertRedactedCHITests: DatabaseTests
-//{
-//    [Test]
-//    public void RevertRedactedCHI_Valid()
-//    {
-//        var redactedChisCount = CatalogueRepository.GetAllObjects<RedactedCHI>().Length;
-//        var rCHI = new RedactedCHI(CatalogueRepository, "1111111111", 0, "[fake],[table]", "pkValue", "[pkColumn]", "[columnName]");
-//        rCHI.SaveToDatabase();
-//        //needs a columinfo and read data to update
+internal class ExecuteCommandRevertRedactedCHITests : DatabaseTests
+{
+    //[Test]
+    //public void RevertRedactedCHI_Valid()
+    //{
+    //    var redactedChisCount = CatalogueRepository.GetAllObjects<RedactedCHI>().Length;
+    //    var rCHI = new RedactedCHI(CatalogueRepository, "1111111111", 0, "[fake],[table]", "pkValue", "[pkColumn]", "[columnName]");
+    //    rCHI.SaveToDatabase();
+    //    //needs a columinfo and read data to update
 
-//        var cmd = new ExecuteCommandRevertRedactedCHI(new ThrowImmediatelyActivator(RepositoryLocator), rCHI);
-//        Assert.DoesNotThrow(() => cmd.Execute());
-//        Assert.That(redactedChisCount, Is.EqualTo(CatalogueRepository.GetAllObjects<RedactedCHI>().Length));
-//    }
+    //    var cmd = new ExecuteCommandRevertRedactedCHI(new ThrowImmediatelyActivator(RepositoryLocator), rCHI);
+    //    Assert.DoesNotThrow(() => cmd.Execute());
+    //    Assert.That(redactedChisCount, Is.EqualTo(CatalogueRepository.GetAllObjects<RedactedCHI>().Length));
+    //}
 
-//    [Test]
-//    public void RevertRedactedCHI_InValid()
-//    {
-//        var redactedChisCount = CatalogueRepository.GetAllObjects<RedactedCHI>().Length;
-//        var rCHI = new RedactedCHI(CatalogueRepository, "1111111111", 0, "[fake],[table]", "pkValue", "[pkColumn]", "[columnName]");
-//        rCHI.DeleteInDatabase();
-//        var cmd = new ExecuteCommandRevertRedactedCHI(new ThrowImmediatelyActivator(RepositoryLocator), rCHI);
-//        Assert.DoesNotThrow(() => cmd.Execute());
-//        Assert.That(redactedChisCount, Is.EqualTo(CatalogueRepository.GetAllObjects<RedactedCHI>().Length));
-//    }
-//}
+    //[Test]
+    //public void RevertRedactedCHI_InValid()
+    //{
+    //    var redactedChisCount = CatalogueRepository.GetAllObjects<RedactedCHI>().Length;
+    //    var rCHI = new RedactedCHI(CatalogueRepository, "1111111111", 0, "[fake],[table]", "pkValue", "[pkColumn]", "[columnName]");
+    //    rCHI.DeleteInDatabase();
+    //    var cmd = new ExecuteCommandRevertRedactedCHI(new ThrowImmediatelyActivator(RepositoryLocator), rCHI);
+    //    Assert.DoesNotThrow(() => cmd.Execute());
+    //    Assert.That(redactedChisCount, Is.EqualTo(CatalogueRepository.GetAllObjects<RedactedCHI>().Length));
+    //}
+}
