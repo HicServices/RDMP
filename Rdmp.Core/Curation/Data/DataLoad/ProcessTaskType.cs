01aca9d3f8195bc3782e4daedc36f77700266f0b
James Friel <JFriel001@dundee.ac.uk>
2023-12-14 09:22:39 +0000

    update enum

--- Rdmp.Core/Curation/Data/DataLoad/ProcessTaskType.cs
+++ Rdmp.Core/Curation/Data/DataLoad/ProcessTaskType.cs
@@ -22,10 +22,6 @@ public enum ProcessTaskType
     /// </summary>
     SQLFile,
 
-    /// <summary>
-    /// ProcessTask is to import a SQL backup file directly to the server
-    /// </summary>
-    SQLBakFile,
 
     /// <summary>
     /// ProcessTask is to instantiate the IAttacher class Type specified in Path and hydrate its [DemandsInitialization] properties with values matching
@@ -43,5 +39,10 @@ public enum ProcessTaskType
     /// ProcessTask is to instantiate the IMutilateDataTables class Type specified in Path and hydrate its [DemandsInitialization] properties with values matching
     /// ProcessTaskArguments and run it in the specified load stage in an MutilateDataTablesRuntimeTask wrapper.
     /// </summary>
-    MutilateDataTable
+    MutilateDataTable,
+
+    /// <summary>
+    /// ProcessTask is to import a SQL backup file directly to the server
+    /// </summary>
+    SQLBakFile,
 }
