b7fd5d3f8e7bad74712699b63f5b7b3f6f77d683
James Friel <JFriel001@dundee.ac.uk>
2024-01-18 15:07:04 +0000

    update sql file

--- Rdmp.Core/Databases/CatalogueDatabase/up/078_AddLastLoadTimeToLoadMetadata.sql
+++ Rdmp.Core/Databases/CatalogueDatabase/up/078_AddLastLoadTimeToLoadMetadata.sql
@@ -1,4 +1,5 @@
-﻿--Version:8.1.2
+﻿gs
+--Version:8.1.3
 --Description: Adds LastLoadTime to LoadMetadata table
  GO
   if exists (select 1 from sys.columns where name = 'LoadLastTime' and object_id = (select object_id from sys.tables where name = 'LoadMetadata'))
