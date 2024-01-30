01e0f2fc4e0db8ddd9673930c5cf9723a18e23e1
James Friel <JFriel001@dundee.ac.uk>
2024-01-30 08:15:41 +0000

    fix build

--- Rdmp.Core.Tests/Caching/Integration/CustomDateCachingTests.cs
+++ Rdmp.Core.Tests/Caching/Integration/CustomDateCachingTests.cs
@@ -141,7 +141,7 @@ public class TestCacheSource : CacheSource<TestCacheChunk>
         GracefulCancellationToken cancellationToken)
     {
         var c = new TestCacheChunk(Request.Start);
-        listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Info, $"!!{request.Start:g}"));
+        listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information, $"!!{request.Start:g}"));
         return c;
     }
 
