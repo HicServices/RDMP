ef0afe83e100c76e446324ed38847f0ddc3a4671
James Friel <jfriel001@dundee.ac.uk>
2024-02-05 14:54:12 +0000

    Feature/rdmp 119 data load deltas (#1726)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## Changed
 
 - Update Excel Attacher to read data from arbitrary start points within sheets
+- Add Time based filtering of remote table and database attachers
 
 ## [8.1.3] - 2024-01-15
 
--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Health Informatics Centre, University of Dundee")]
 [assembly: AssemblyProduct("Research Data Management Platform (RDMP)")]
-[assembly: AssemblyCopyright("Copyright (c) 2018 - 2023")]
+[assembly: AssemblyCopyright("Copyright (c) 2018 - 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
