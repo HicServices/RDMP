05dcfc1781a678e195d12a04bdaaa994ea4c8fbb
James Friel <JFriel001@dundee.ac.uk>
2024-02-06 08:38:56 +0000

    add env connection strings

--- Rdmp.Web.Server/RDMPInitialiser.cs
+++ Rdmp.Web.Server/RDMPInitialiser.cs
@@ -15,10 +15,8 @@ namespace Rdmp.Web.Server
     public static class RDMPInitialiser
     {
 
-        private static string _CatalogueConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=RDMP_Catalogue;Integrated Security=True;Trust Server Certificate=True";
-        private static string _DataExportConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=RDMP_DataExport;Integrated Security=True;Trust Server Certificate=True";
-        //private static string _CatalogueConnectionString = "Data Source=host.docker.internal\\sqlexpress01;Initial Catalog=RDMP4_Catalogue;Integrated Security=True;Trust Server Certificate=True";
-        //private static string _DataExportConnectionString = "Data Source=host.docker.internal\\sqlexpress01;Initial Catalog=RDMP4_DataExport;Integrated Security=True;Trust Server Certificate=True";
+        private static string _CatalogueConnectionString =  Environment.GetEnvironmentVariable("CATALOGUE_CONNECTION_STRING") ?? "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=RDMP_Catalogue;Integrated Security=True;Trust Server Certificate=True";
+        private static string _DataExportConnectionString = Environment.GetEnvironmentVariable("DATA_EXPORT_CONNECTION_STRING") ?? "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=RDMP_DataExport;Integrated Security=True;Trust Server Certificate=True";
 
         public static void Init(RDMPBootstrapOptions args)
         {
