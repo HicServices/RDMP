cb379fe8b8d79419a860ca4c3decff66c1a625a0
James Friel <JFriel001@dundee.ac.uk>
2024-02-06 13:07:28 +0000

    add the theme

--- Rdmp.React.UI/src/renderer/src/App.jsx
+++ Rdmp.React.UI/src/renderer/src/App.jsx
@@ -1,39 +1,40 @@
-import Versions from './components/Versions'
 import electronLogo from './assets/electron.svg'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import { ThemeProvider } from '@fluentui/react'
+import defaultTheme from '../../theme/defaultTheme'
+import { PrimaryButton, makeStyles } from '@fluentui/react'
+import { Image, Card } from '@fluentui/react-components'
+
+const style = makeStyles({
+  card: {
+    width: '100%',
+    height: '100%'
+  }
+})
+
 function App() {
-  //const ipcHandle = () => window.electron.ipcRenderer.send('ping')
-  const [catalogues, setCatalogues] = useState(0);
+  const styles = style()
+  const [catalogues, setCatalogues] = useState(0)
   async function populateCatalogues() {
-    const response = await fetch('http://localhost:5067/Catalogues');
-    const data = await response.json();
-    setCatalogues(data.length);
+    const response = await fetch('http://localhost:5067/Catalogues')
+    const data = await response.json()
+    setCatalogues(data.length)
   }
 
   useEffect(() => {
-    populateCatalogues();
-  }, []);
+    populateCatalogues()
+  }, [])
 
   return (
     <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span> - Found {catalogues} catalogues
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-      </div>
+      <ThemeProvider theme={defaultTheme}>
+        <Card className={styles.card}>
+          <Image src={electronLogo} />
+          <PrimaryButton>{catalogues} Catalogues</PrimaryButton>
+        </Card>
+      </ThemeProvider>
     </>
   )
 }
 
 export default App
-
---
+++ Rdmp.React.UI/src/theme/defaultTheme.js
@@ -0,0 +1,31 @@
+import { createTheme } from '@fluentui/react';
+
+const defaultTheme = createTheme({
+  palette: {
+    themePrimary: '#00274e',
+    themeLighterAlt: '#cedcea',
+    themeLighter: '#a5bed7',
+    themeLight: '#81a2c3',
+    themeTertiary: '#6188b0',
+    themeSecondary: '#45719c',
+    themeDarkAlt: '#2d5b89',
+    themeDark: '#1a4876',
+    themeDarker: '#0b3762',
+    neutralLighterAlt: '#eeeeee',
+    neutralLighter: '#eaeaea',
+    neutralLight: '#e1e1e1',
+    neutralQuaternaryAlt: '#d1d1d1',
+    neutralQuaternary: '#c8c8c8',
+    neutralTertiaryAlt: '#c0c0c0',
+    neutralTertiary: '#c2c2c2',
+    neutralSecondary: '#858585',
+    neutralSecondaryAlt: '#858585',
+    neutralPrimaryAlt: '#4b4b4b',
+    neutralPrimary: '#333333',
+    neutralDark: '#272727',
+    black: '#1d1d1d',
+    white: '#f5f5f5',
+  }});
+
+
+export default defaultTheme;
