fcf8f269cf8ac204f688519ae488806e298c6eaa
James Friel <JFriel001@dundee.ac.uk>
2024-02-16 11:00:13 +0000

    fix extra space

--- Rdmp.Core/Curation/Data/DataLoad/ILoadMetadataCatalogueLinkage.cs
+++ Rdmp.Core/Curation/Data/DataLoad/ILoadMetadataCatalogueLinkage.cs
@@ -6,20 +6,19 @@
 using Rdmp.Core.MapsDirectlyToDatabaseTable;
 using Rdmp.Core.Repositories;
 
-namespace Rdmp.Core.Curation.Data.DataLoad
+namespace Rdmp.Core.Curation.Data.DataLoad;
+
+/// <summary>
+/// Linkng table between catalogues and loadmetadatas
+/// </summary>
+public interface ILoadMetadataCatalogueLinkage: IMapsDirectlyToDatabaseTable
 {
     /// <summary>
-    /// Linkng table between catalogues and loadmetadatas
+    /// Returns where the object exists (e.g. database) as <see cref="ICatalogueRepository"/> or null if the object does not exist in a catalogue repository.
     /// </summary>
-    public interface  ILoadMetadataCatalogueLinkage: IMapsDirectlyToDatabaseTable
-    {
-        /// <summary>
-        /// Returns where the object exists (e.g. database) as <see cref="ICatalogueRepository"/> or null if the object does not exist in a catalogue repository.
-        /// </summary>
-        ICatalogueRepository CatalogueRepository { get; }
+    ICatalogueRepository CatalogueRepository { get; }
 
-        int LoadMetatdataID{ get; }
-        int CatalogueID { get; }
+    int LoadMetatdataID{ get; }
+    int CatalogueID { get; }
 
-    }
 }
