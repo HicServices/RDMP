2edf48aa6e77387ecd6d007c038c67af36d9712e
James Friel <JFriel001@dundee.ac.uk>
2024-03-15 09:35:23 +0000

    fix sql patch

--- Rdmp.Core/Databases/CatalogueDatabase/up/079_AllowMultipleCatalogueLoadMetadatas.sql
+++ Rdmp.Core/Databases/CatalogueDatabase/up/079_AllowMultipleCatalogueLoadMetadatas.sql
@@ -2,7 +2,7 @@
 --Description: Adds linking table to allow for multiple load metadatas per catalogue
 if not exists (select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='LoadMetadataCatalogueLinkage')
 BEGIN
-CREATE TABLE [dbo].[LoadMetadataCatalogueLinkage](
+CREATE TABLE [dbo].LoadMetadataCatalogueLinkage(
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[LoadMetadataID] [int] NOT NULL,
 	[CatalogueID] [int] NOT NULL,
@@ -13,26 +13,26 @@ CREATE TABLE [dbo].[LoadMetadataCatalogueLinkage](
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
-CREATE INDEX LoadMetadataCatalogueLinkage_Index
-ON [dbo].[LoadMetadataCatalogueLinkage] ([LoadMetadataID],[CatalogueID])
 END
 
-IF COL_LENGTH('dbo.Catalogue','LoadMetadata_ID') IS NOT NULL
-BEGIN
-insert into [dbo].[LoadMetadataCatalogueLinkage]([CatalogueID], [LoadMetadataID])
-	select ID as CatalogueID, LoadMetadata_ID as LoadMetadataID
+Declare @MigrateLinkageSQL varchar(max) = '
+    insert into [dbo].[LoadMetadataCatalogueLinkage](CatalogueID, LoadMetadataID)
+	select ID as CatalogueID, LoadMetadata_ID
 	from [dbo].[Catalogue]
-	where LoadMetadata_ID is not null
+	where LoadMetadata_ID is not null'
+if exists (select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='Catalogue' and COLUMN_NAME='LoadMetadata_ID')
+BEGIN
+EXEC(@MigrateLinkageSQL)
 END
-GO
-IF COL_LENGTH('dbo.Catalogue','LoadMetadata_ID') IS NOT NULL
+
+if exists (select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='Catalogue' and COLUMN_NAME='LoadMetadata_ID')
 BEGIN
 IF (OBJECT_ID('FK_Catalogue_LoadMetadata') IS NOT NULL)
   ALTER TABLE [dbo].[Catalogue]
   DROP CONSTRAINT [FK_Catalogue_LoadMetadata]
 END
 
-IF COL_LENGTH('dbo.Catalogue','LoadMetadata_ID') IS NOT NULL
+if exists (select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='Catalogue' and COLUMN_NAME='LoadMetadata_ID')
 BEGIN
 	ALTER TABLE [dbo].[Catalogue]
 	DROP column LoadMetadata_ID
