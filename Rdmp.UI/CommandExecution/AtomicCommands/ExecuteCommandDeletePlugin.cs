c623988a70a22af8a3055a2f4703f88ac0716d52
James Friel <JFriel001@dundee.ac.uk>
2024-03-22 09:31:31 +0000

    add keyboard shortcut

--- Rdmp.UI/CommandExecution/AtomicCommands/ExecuteCommandDeletePlugin.cs
+++ Rdmp.UI/CommandExecution/AtomicCommands/ExecuteCommandDeletePlugin.cs
@@ -4,6 +4,7 @@
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
+using Rdmp.Core.CommandExecution.AtomicCommands;
 using Rdmp.Core.Curation.Data;
 using Rdmp.Core.Icons.IconProvision;
 using Rdmp.Core.ReusableLibraryCode.Icons.IconProvision;
@@ -18,10 +19,11 @@ public sealed class ExecuteCommandDeletePlugin : BasicUICommandExecution
 {
     private readonly LoadModuleAssembly _assembly;
 
+
     public ExecuteCommandDeletePlugin(IActivateItems activator, LoadModuleAssembly assembly) : base(activator)
     {
         _assembly = assembly;
-    }
+      }
 
     public override Image<Rgba32> GetImage(IIconProvider iconProvider) =>
         iconProvider.GetImage(RDMPConcept.Plugin, OverlayKind.Delete);
