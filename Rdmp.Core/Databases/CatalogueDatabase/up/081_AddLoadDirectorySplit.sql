17aa0bae0aad4fe04c535c9d9308d2c57e43eab3
James Friel <JFriel001@dundee.ac.uk>
2024-04-04 16:54:32 +0100

    update patch

--- Rdmp.Core/Databases/CatalogueDatabase/up/081_AddLoadDirectorySplit.sql
+++ Rdmp.Core/Databases/CatalogueDatabase/up/081_AddLoadDirectorySplit.sql
@@ -1,4 +1,4 @@
---Version:8.1.5
+--Version:8.2.0
 --Description: Allow for dispirate locations for data load directories
 if not exists (select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='LoadMetaData' and COLUMN_NAME='LocationOfForLoadingDirectory')
 BEGIN       
@@ -7,14 +7,27 @@ BEGIN
 	ALTER TABLE LoadMetadata ADD LocationOfExecutablesDirectory [varchar](3000) NULL;
 	ALTER TABLE LoadMetadata ADD LocationOfCacheDirectory [varchar](3000) NULL;
 END
+GO
 
-
-if exisits(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='LoadMetaData' and COLUMN_NAME='LocationOfFlatFiles')
+if exists(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='LoadMetaData' and COLUMN_NAME='LocationOfFlatFiles')
 BEGIN
 update RDMP_Catalogue.dbo.LoadMetadata 
 set LocationOfForLoadingDirectory = LocationOfFlatFiles +'\Data\ForLoading'
+where LocationOfFlatFiles is not null
+update RDMP_Catalogue.dbo.LoadMetadata 
 set LocationOfForArchivingDirectory = LocationOfFlatFiles +'\Data\ForArchiving'
+where LocationOfFlatFiles is not null
+update RDMP_Catalogue.dbo.LoadMetadata 
 set LocationOfExecutablesDirectory = LocationOfFlatFiles +'\Executables'
+where LocationOfFlatFiles is not null
+update RDMP_Catalogue.dbo.LoadMetadata 
 set LocationOfCacheDirectory = LocationOfFlatFiles +'\Data\Cache'
 where LocationOfFlatFiles is not null
+
+END
+GO
+if exists(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='LoadMetaData' and COLUMN_NAME='LocationOfFlatFiles')
+BEGIN
+	ALTER TABLE LoadMetadata
+	DROP Coulumn LocationOfFlatFiles
 END
