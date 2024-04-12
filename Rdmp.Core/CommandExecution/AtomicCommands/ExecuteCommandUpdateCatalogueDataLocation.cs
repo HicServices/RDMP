7aedbe0f596ce902b011942e2e417f5e35c545c3
James Friel <JFriel001@dundee.ac.uk>
2024-04-12 14:21:06 +0100

    working switch

---
+++ Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandUpdateCatalogueDataLocation.cs
@@ -0,0 +1,82 @@
+﻿using FAnsi.Discovery;
+using NPOI.OpenXmlFormats;
+using Rdmp.Core.Curation.Data;
+using System;
+using System.Linq;
+
+namespace Rdmp.Core.CommandExecution.AtomicCommands;
+
+public class ExecuteCommandUpdateCatalogueDataLocation : BasicCommandExecution, IAtomicCommand
+{
+    private IBasicActivateItems _activator;
+    private readonly DiscoveredTable _table; 
+    private readonly CatalogueItem[] _selectedCatalogueItems;
+    
+    private bool _checksPassed = false;
+    public ExecuteCommandUpdateCatalogueDataLocation(IBasicActivateItems activator, CatalogueItem[] selectedCatalogueItems, DiscoveredTable table)
+    {
+        _activator=activator;
+        _table=table;
+        _selectedCatalogueItems=selectedCatalogueItems;
+    }
+
+    public bool Check()
+    {
+        _checksPassed = true;
+        return true;
+    }
+
+    private string GrabTablequalifier(string name)
+    {
+        return string.Join('.', name.Split('.')[..^1]);
+    }
+
+
+    private string GenerateNewSQLPath(string path)
+    {
+        var qualifier = GrabTablequalifier(path);
+        var updatedName = path.Replace(qualifier,_table.GetFullyQualifiedName());
+        return updatedName;
+    }
+
+    private TableInfo TableIsAlreadyKnown()
+    {
+        return _activator.RepositoryLocator.CatalogueRepository.GetAllObjects<TableInfo>().Where(ti => {
+            return ti.Name == _table.GetFullyQualifiedName() &&
+            ti.Server == _table.Database.Server.Name &&
+            ti.Database == _table.Database.GetRuntimeName();
+            }).FirstOrDefault();
+    }
+
+
+    public override void Execute()
+    {
+        if(!_checksPassed)
+        {
+            throw new Exception("Unable to execute ExecuteCommandUpdateCatalogueDataLocation as checks have not been ran.");
+        }
+        
+
+        foreach(CatalogueItem selectedCatalogueItem in _selectedCatalogueItems)
+        {
+            var existingTable = TableIsAlreadyKnown();
+            if (existingTable is not null)
+            {
+                selectedCatalogueItem.ColumnInfo.TableInfo_ID = existingTable.ID;
+            }
+            else
+            {
+                var tblInfo = new TableInfo(_activator.RepositoryLocator.CatalogueRepository,_table.GetFullyQualifiedName());
+                tblInfo.Server = _table.Database.Server.Name;
+                tblInfo.Database= _table.Database.GetRuntimeName();
+                tblInfo.SaveToDatabase();
+                selectedCatalogueItem.ColumnInfo.TableInfo_ID = tblInfo.ID;
+
+            }
+            selectedCatalogueItem.ColumnInfo.Name = GenerateNewSQLPath(selectedCatalogueItem.ColumnInfo.Name);
+            selectedCatalogueItem.ColumnInfo.SaveToDatabase();
+            selectedCatalogueItem.ExtractionInformation.SelectSQL = GenerateNewSQLPath(selectedCatalogueItem.ExtractionInformation.SelectSQL);
+            selectedCatalogueItem.ExtractionInformation.SaveToDatabase();
+        }
+    }
+}
