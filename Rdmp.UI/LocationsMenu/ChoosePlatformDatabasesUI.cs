45147965486cd6025106d38d7a700604c3946c53
James Friel <JFriel001@dundee.ac.uk>
2024-04-18 16:19:19 +0100

    re-add selectdialog

--- Rdmp.UI/LocationsMenu/ChoosePlatformDatabasesUI.cs
+++ Rdmp.UI/LocationsMenu/ChoosePlatformDatabasesUI.cs
@@ -1,4 +1,4 @@
-// Copyright (c) The University of Dundee 2018-2019
+// Copyright (c) The University of Dundee 2018-2024
 // This file is part of the Research Data Management Platform (RDMP).
 // RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -25,7 +25,6 @@ using Rdmp.Core.Startup;
 using Rdmp.UI.ChecksUI;
 using Rdmp.UI.DataLoadUIs.ModuleUIs;
 using Rdmp.UI.SimpleDialogs;
-using SharpCompress.Common;
 using YamlDotNet.RepresentationModel;
 using YamlDotNet.Serialization;
 
