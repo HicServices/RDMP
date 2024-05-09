28e19b595dc100b9b63f2b638db129545c10ca61
James Friel <JFriel001@dundee.ac.uk>
2024-05-09 16:41:15 +0100

    add missing files

---
+++ Rdmp.Core/Setting/ISetting.cs
@@ -0,0 +1,7 @@
+﻿namespace Rdmp.Core.Setting;
+
+public interface ISetting
+{
+    string Key { get; set; }
+    string Value { get; set; }
+}
---
+++ Rdmp.Core/Setting/Setting.cs
@@ -0,0 +1,52 @@
+﻿using Rdmp.Core.Curation.Data;
+using Rdmp.Core.MapsDirectlyToDatabaseTable.Attributes;
+using Rdmp.Core.Repositories;
+using Rdmp.Core.ReusableLibraryCode.Annotations;
+using Rdmp.Core.Setting;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace Rdmp.Core.Settings;
+
+public class Setting : DatabaseEntity, ISetting
+{
+    #region Database Properties
+
+    private string _key;
+    private string _value;
+
+    [NotNull]
+    [Unique]
+    public string Key
+    {
+        get => _key;
+        set => SetField(ref _key, value);
+    }
+
+    [NotNull]
+    public string Value
+    {
+        get => _value;
+        set => SetField(ref _value, value);
+    }
+
+    #endregion
+
+    public Setting(ICatalogueRepository repository, string key, string value)
+    {
+        Key = key;
+        Value = value;
+        Repository = repository;
+        Repository.InsertAndHydrate(this, new Dictionary<string, object>
+        {
+            { nameof(Key), key },
+            { nameof(Value), value },
+        });
+    }
+
+    public Setting(ICatalogueRepository repository, DbDataReader r): base(repository,r)
+    {
+        Key = r["Key"].ToString();
+        Value = r["Value"].ToString();
+    }
+}
