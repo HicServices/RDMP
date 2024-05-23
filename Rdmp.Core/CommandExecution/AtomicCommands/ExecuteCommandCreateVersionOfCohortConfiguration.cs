01cd3936aff3466ae7ddbab864e2700e1116952a
James Friel <JFriel001@dundee.ac.uk>
2024-05-23 10:13:46 +0100

    interim

--- Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandCreateVersionOfCohortConfiguration.cs
+++ Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandCreateVersionOfCohortConfiguration.cs
@@ -1,5 +1,7 @@
 ﻿using Rdmp.Core.CohortCommitting.Pipeline.Sources;
+using Rdmp.Core.Curation.Data;
 using Rdmp.Core.Curation.Data.Cohort;
+using Rdmp.Core.Repositories.Construction;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +10,13 @@ using System.Threading.Tasks;
 
 namespace Rdmp.Core.CommandExecution.AtomicCommands;
 
-public class ExecuteCommandCreateVersionOfCohortConfiguration: BasicCommandExecution
+public class ExecuteCommandCreateVersionOfCohortConfiguration : BasicCommandExecution, IAtomicCommandWithTarget
 {
     CohortIdentificationConfiguration _cic;
     IBasicActivateItems _activateItems;
 
-    public ExecuteCommandCreateVersionOfCohortConfiguration(IBasicActivateItems activator, CohortIdentificationConfiguration cic)
+    [UseWithObjectConstructor]
+    public ExecuteCommandCreateVersionOfCohortConfiguration(IBasicActivateItems activator, CohortIdentificationConfiguration cic) : base(activator)
     {
         _activateItems = activator;
         _cic = cic;
@@ -24,5 +27,11 @@ public class ExecuteCommandCreateVersionOfCohortConfiguration: BasicCommandExecu
     {
         base.Execute();
         _cic.SaveCurrentConfigurationAsNewVersion();
+    }
+
+    public IAtomicCommandWithTarget SetTarget(DatabaseEntity target)
+    {
+        _cic = (CohortIdentificationConfiguration)target;
+        return this;
     }
 }
