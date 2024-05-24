a5e186440e681ebd635847047048e2822ed74894
James Friel <JFriel001@dundee.ac.uk>
2024-05-24 11:45:32 +0100

    remove old code

--- Rdmp.Core/Databases/CatalogueDatabase/up/082_AddCohortVersioning.sql
+++ Rdmp.Core/Databases/CatalogueDatabase/up/082_AddCohortVersioning.sql
@@ -1,16 +1,6 @@
 ﻿--Version: 8.2.0
 --Description: Adds unified instance settings
-if not exists (select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='CohortConfigurationVersion')
-BEGIN
-CREATE TABLE [dbo].CohortConfigurationVersion(
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [varchar](450) NOT NULL,
-	[CohortIdentificationConfigurationId] [int] NOT NULL
-	)
-END
-
-
-if not exists(select 1 from sys.columns where name = 'Version' and OBJECT_NAME(object_id) = '[CohortIdentificationConfiguration]')
+if not exists(select 1 from sys.columns where name = 'Version' and OBJECT_NAME(object_id) = 'CohortIdentificationConfiguration')
 BEGIN
   alter table [dbo].[CohortIdentificationConfiguration]
   add Version [int]
