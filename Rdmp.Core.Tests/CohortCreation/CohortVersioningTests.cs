f72a5d4c7e73b1335dce09bb9aa976b5edefe565
James Friel <JFriel001@dundee.ac.uk>
2024-05-27 08:47:55 +0100

    add basic test

---
+++ Rdmp.Core.Tests/CohortCreation/CohortVersioningTests.cs
@@ -0,0 +1,40 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Rdmp.Core.CommandExecution;
+using Rdmp.Core.CommandExecution.AtomicCommands;
+using Rdmp.Core.Curation.Data.Cohort;
+using Rdmp.Core.Tests.CohortCreation.QueryTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.Core.Tests.CohortCreation;
+
+public class CohortVersioningTests : CohortQueryBuilderWithCacheTests
+{
+
+    private IBasicActivateItems MakeMockActivator()
+    {
+        var mock = Substitute.For<IBasicActivateItems>();
+        mock.RepositoryLocator.Returns(RepositoryLocator);
+        mock.GetDelegates().Returns(new List<CommandInvokerDelegate>());
+        return mock;
+    }
+
+    [Test]
+    public void TestCreationOfNewVersionOfCohort()
+    {
+        var cic = new CohortIdentificationConfiguration(CatalogueRepository, "mycic")
+        {
+            QueryCachingServer_ID = externalDatabaseServer.ID
+        };
+        cic.SaveToDatabase();
+        var cmd = new ExecuteCommandCreateVersionOfCohortConfiguration(MakeMockActivator(), cic);
+        Assert.DoesNotThrow(() => cmd.Execute());
+        var newCic = CatalogueRepository.GetAllObjectsWhere<CohortIdentificationConfiguration>("ClonedFrom_ID", cic.ID);
+        Assert.That(newCic.Length, Is.EqualTo(1));
+        Assert.That(newCic.First().Version, Is.EqualTo(1));
+    }
+}
