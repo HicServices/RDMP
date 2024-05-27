9ce3ca190623eb6543e6c674ae41ed90e77410a8
James Friel <JFriel001@dundee.ac.uk>
2024-05-27 12:09:20 +0100

    tidy up code

--- Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandSetExtractionConfigurationCohort.cs
+++ Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandSetExtractionConfigurationCohort.cs
@@ -1,36 +1,37 @@
-﻿using Rdmp.Core.Curation.Data;
+﻿// Copyright (c) The University of Dundee 2024-2024
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
 using Rdmp.Core.DataExport.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Rdmp.Core.Icons.IconProvision;
+using Rdmp.Core.ReusableLibraryCode.Icons.IconProvision;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
 
-namespace Rdmp.Core.CommandExecution.AtomicCommands
+namespace Rdmp.Core.CommandExecution.AtomicCommands;
+
+public class ExecuteCommandSetExtractionConfigurationCohort : BasicCommandExecution, IAtomicCommand
 {
-    internal class ExecuteCommandSetExtractionConfigurationCohort : BasicCommandExecution
+    private ExtractionConfiguration _extractionConfiguration;
+    private ExtractableCohort _extractableCohort;
+
+    public ExecuteCommandSetExtractionConfigurationCohort(IBasicActivateItems activator, ExtractionConfiguration extractionConfiguration, ExtractableCohort cohort) : base(activator)
     {
-        private ExtractionConfiguration _extractionConfiguration;
-        private ExtractableCohort _extractableCohort;
-
-        public ExecuteCommandSetExtractionConfigurationCohort(IBasicActivateItems activator) : base(activator)
-        {
-            Weight = 100.1f;
-        }
-
-        public ExecuteCommandSetExtractionConfigurationCohort(IBasicActivateItems activator, ExtractionConfiguration extractionConfiguration, ExtractableCohort cohort) : this(activator)
-        {
-            _extractionConfiguration = extractionConfiguration;
-            _extractableCohort = cohort;
-        }
-
-
-        public override void Execute()
-        {
-            base.Execute();
-            _extractionConfiguration.Cohort_ID = _extractableCohort.ID;
-            _extractionConfiguration.SaveToDatabase();
-        }
-
+        _extractionConfiguration = extractionConfiguration;
+        _extractableCohort = cohort;
     }
+
+
+    public override void Execute()
+    {
+        base.Execute();
+        _extractionConfiguration.Cohort_ID = _extractableCohort.ID;
+        _extractionConfiguration.SaveToDatabase();
+    }
+
+    public override Image<Rgba32> GetImage(IIconProvider iconProvider) =>
+    iconProvider.GetImage(RDMPConcept.ExtractionConfiguration, OverlayKind.None);
+
 }
