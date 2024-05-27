8f40a2be89a5c48c4ab03c6609b6fd31ee76b2ed
James Friel <JFriel001@dundee.ac.uk>
2024-05-27 12:02:18 +0100

    add basic command

---
+++ Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandSetExtractionConfigurationCohort.cs
@@ -0,0 +1,36 @@
+﻿using Rdmp.Core.Curation.Data;
+using Rdmp.Core.DataExport.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.Core.CommandExecution.AtomicCommands
+{
+    internal class ExecuteCommandSetExtractionConfigurationCohort : BasicCommandExecution
+    {
+        private ExtractionConfiguration _extractionConfiguration;
+        private ExtractableCohort _extractableCohort;
+
+        public ExecuteCommandSetExtractionConfigurationCohort(IBasicActivateItems activator) : base(activator)
+        {
+            Weight = 100.1f;
+        }
+
+        public ExecuteCommandSetExtractionConfigurationCohort(IBasicActivateItems activator, ExtractionConfiguration extractionConfiguration, ExtractableCohort cohort) : this(activator)
+        {
+            _extractionConfiguration = extractionConfiguration;
+            _extractableCohort = cohort;
+        }
+
+
+        public override void Execute()
+        {
+            base.Execute();
+            _extractionConfiguration.Cohort_ID = _extractableCohort.ID;
+            _extractionConfiguration.SaveToDatabase();
+        }
+
+    }
+}
