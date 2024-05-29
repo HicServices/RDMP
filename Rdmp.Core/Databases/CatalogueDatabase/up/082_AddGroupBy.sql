19de0ffc343f26396d587526f6707d03d031938f
James Friel <JFriel001@dundee.ac.uk>
2024-05-29 10:03:20 +0100

    ad dmissing file

---
+++ Rdmp.Core/Databases/CatalogueDatabase/up/082_AddGroupBy.sql
@@ -0,0 +1,12 @@
+﻿--Version: 8.2.0
+--Description: Adds linking table to allow for multiple load metadatas per catalogue
+ if not exists (select 1 from sys.columns where name = 'GroupBy' and OBJECT_NAME(object_id) = 'AggregateDimension')
+BEGIN
+ALTER TABLE AggregateDimension ADD GroupBy int DEFAULT 1
+END
+
+ if not exists (select 1 from sys.columns where name = 'GroupBy' and OBJECT_NAME(object_id) = 'ExtractionInformation')
+BEGIN
+ALTER TABLE ExtractionInformation ADD GroupBy int DEFAULT 1
+END
+
