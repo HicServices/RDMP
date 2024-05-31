cc0b2aef0dc3ca2d3c0c935571e03c213ec45b59
James Friel <JFriel001@dundee.ac.uk>
2024-05-31 16:26:08 +0100

    works

--- Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandCloneExtractionConfiguration.cs
+++ Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandCloneExtractionConfiguration.cs
@@ -4,6 +4,8 @@
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Rdmp.Core.Curation.Data;
 using Rdmp.Core.DataExport.Data;
@@ -17,12 +19,37 @@ namespace Rdmp.Core.CommandExecution.AtomicCommands;
 public class ExecuteCommandCloneExtractionConfiguration : BasicCommandExecution, IAtomicCommand
 {
     private readonly ExtractionConfiguration _extractionConfiguration;
+    private IBasicActivateItems _activeItems;
+    List<IExtractableDataSet> toRemove = new();
+    List<Catalogue> toAdd = new();
+    private void CheckForDepricatedCatalogues()
+    {
+        if (_extractionConfiguration.SelectedDataSets.Any(sd => sd.GetCatalogue().IsDeprecated) && _activeItems.IsInteractive)
+        {
+            if (YesNo("Replace Depricated Catalogues", "There are depricated catalogues in this Extraction Configuration. Would you like to replace them with their replacement (where available)?"))
+            {
+                var depricatedDatasets = _extractionConfiguration.SelectedDataSets.Where(sd => sd.GetCatalogue().IsDeprecated).ToList();
+                var replacedBy = _activeItems.RepositoryLocator.CatalogueRepository.GetExtendedProperties(ExtendedProperty.ReplacedBy);
+                foreach (ISelectedDataSets ds in depricatedDatasets)
+                {
+                    var replacement = replacedBy.Where(rb => rb.ReferencedObjectID == ds.GetCatalogue().ID).FirstOrDefault();
+                    if (replacement is not null)
+                    {
+                        toRemove.Add(ds.ExtractableDataSet);
+                        toAdd.Add(_activeItems.RepositoryLocator.CatalogueRepository.GetObjectByID<Catalogue>(Int32.Parse(replacement.Value)));
+                    }
+                }
+
+
+            }
+        }
+    }
 
     public ExecuteCommandCloneExtractionConfiguration(IBasicActivateItems activator,
         ExtractionConfiguration extractionConfiguration) : base(activator)
     {
         _extractionConfiguration = extractionConfiguration;
-
+        _activeItems = activator;
         if (!_extractionConfiguration.SelectedDataSets.Any())
             SetImpossible("ExtractionConfiguration does not have any selected datasets");
     }
@@ -36,9 +63,26 @@ public class ExecuteCommandCloneExtractionConfiguration : BasicCommandExecution,
     public override void Execute()
     {
         base.Execute();
+        CheckForDepricatedCatalogues();
 
         var clone = _extractionConfiguration.DeepCloneWithNewIDs();
-
+        foreach (ExtractableDataSet ds in toRemove)
+        {
+            clone.RemoveDatasetFromConfiguration(ds);
+        }
+        foreach (Catalogue c in toAdd)
+        {
+            //check if the eds already exis
+            var eds = _activeItems.RepositoryLocator.DataExportRepository.GetAllObjectsWhere<ExtractableDataSet>("Catalogue_ID", c.ID).FirstOrDefault();
+            if (eds is null)
+            {
+                eds = new ExtractableDataSet(_activeItems.RepositoryLocator.DataExportRepository, c);
+                eds.SaveToDatabase();
+            }
+            clone.AddDatasetToConfiguration(eds);
+        }
+        Console.WriteLine('a');
+        //cataRepo.GetExtendedProperties(ExtendedProperty.ReplacedBy, Deprecated))
         Publish((DatabaseEntity)clone.Project);
         Emphasise(clone);
     }
