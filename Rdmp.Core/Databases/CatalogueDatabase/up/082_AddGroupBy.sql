6252289fb12962829a054a144e31afa4fa274fac
James Friel <JFriel001@dundee.ac.uk>
2024-06-21 11:56:28 +0100

    updtae patch

--- Rdmp.Core/Databases/CatalogueDatabase/up/082_AddGroupBy.sql
+++ Rdmp.Core/Databases/CatalogueDatabase/up/082_AddGroupBy.sql
@@ -2,14 +2,22 @@
 --Description: Adds linking table to allow for multiple load metadatas per catalogue
  if not exists (select 1 from sys.columns where name = 'GroupBy' and OBJECT_NAME(object_id) = 'AggregateDimension')
 BEGIN
-ALTER TABLE AggregateDimension ADD GroupBy int DEFAULT 1
-UPDATE AggregateDimension Set GroupBy = 1
+ALTER TABLE [dbo].[AggregateDimension] ADD GroupBy int DEFAULT 1
+END
 
+if exists( select 1 from [dbo].[AggregateDimension] where GroupBy is NULL)
+BEGIN
+UPDATE [dbo].[AggregateDimension] Set GroupBy = 1 WHERE GroupBy is NULL
 END
 
  if not exists (select 1 from sys.columns where name = 'GroupBy' and OBJECT_NAME(object_id) = 'ExtractionInformation')
 BEGIN
-ALTER TABLE ExtractionInformation ADD GroupBy int DEFAULT 1
-UPDATE ExtractionInformation Set GroupBy = 1
+ALTER TABLE [dbo].[ExtractionInformation] ADD GroupBy int DEFAULT 1
+END
+
+
+if exists( select 1 from [dbo].[ExtractionInformation] where GroupBy is NULL)
+BEGIN
+UPDATE [dbo].[ExtractionInformation] Set GroupBy = 1 WHERE GroupBy is NULL
 END
 
