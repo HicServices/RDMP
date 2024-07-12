35847d4c47d247256651afb6f1b118adf4b99b5c
James Friel <JFriel001@dundee.ac.uk>
2024-07-12 13:17:04 +0100

    sort catalogues by name

--- Rdmp.UI/SimpleDialogs/Reports/MetadataReportUI.cs
+++ Rdmp.UI/SimpleDialogs/Reports/MetadataReportUI.cs
@@ -49,6 +49,7 @@ public partial class MetadataReportUI : RDMPForm
         InitializeComponent();
 
         _catalogues = Activator.CoreChildProvider.AllCatalogues;
+        Array.Sort(_catalogues, delegate (Catalogue x, Catalogue y) { return x.Name.CompareTo(y.Name); });
         cbxCatalogues.Items.AddRange(_catalogues);
 
         if (initialSelection != null)
