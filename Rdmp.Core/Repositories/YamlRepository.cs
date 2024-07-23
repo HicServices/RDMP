4747d99646238cf88e71abbcd214c976a45ae1fd
James A Sutherland <j@sutherland.pw>
2024-07-23 09:04:14 -0500

    Fix issue with whitespace confusing encryption key paths

--- Rdmp.Core/Repositories/YamlRepository.cs
+++ Rdmp.Core/Repositories/YamlRepository.cs
@@ -51,7 +51,18 @@ public class YamlRepository : MemoryDataExportRepository
         LoadObjects();
 
         if (File.Exists(GetEncryptionKeyPathFile()))
-            EncryptionKeyPath = File.ReadAllText(GetEncryptionKeyPathFile());
+        {
+            EncryptionKeyPath = File.ReadLines(GetEncryptionKeyPathFile()).First();
+            if (!File.Exists(EncryptionKeyPath))
+            {
+                // Check if the file does exist but we were confused by stray whitespace:
+                if (char.IsWhiteSpace(EncryptionKeyPath[^1]))
+                {
+                    var trimmed = EncryptionKeyPath.TrimEnd();
+                    if (File.Exists(trimmed)) EncryptionKeyPath = trimmed;
+                }
+            }
+        }
 
         // Don't create new objects with the ID of existing objects
         NextObjectId = Objects.IsEmpty ? 0 : Objects.Max(o => o.Key.ID);
