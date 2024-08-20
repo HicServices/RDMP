093dffbcade114f5209969f7d27a271ce31bbc54
James Friel <JFriel001@dundee.ac.uk>
2024-08-20 08:17:16 +0100

    add missing interface

---
+++ Rdmp.Core/Curation/DataHelper/RegexRedaction/IRegexRedaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.Core.Curation.DataHelper.RegexRedaction
+{
+    public interface IRegexRedaction
+    {
+
+        int RedactionConfiguration_ID { get; protected set; }
+        string RedactedValue { get; protected set; }
+        string ReplacementValue { get; protected set; }
+
+        int startingIndex { get; protected set; }
+    }
+}
