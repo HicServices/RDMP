b534ddba975f2c36799d93f6d56c7e3c07c47f7f
James Friel <JFriel001@dundee.ac.uk>
2024-08-20 11:41:58 +0100

    add helper

---
+++ Rdmp.Core/Curation/DataHelper/RegexRedaction/RegexRedactionHelper.cs
@@ -0,0 +1,25 @@
+﻿using Rdmp.Core.Curation.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.Core.Curation.DataHelper.RegexRedaction
+{
+    public static class RegexRedactionHelper
+    {
+
+        public static string ConvertPotentialDateTimeObject(string value, string currentColumnType)
+        {
+            var matchValue = $"'{value}'";
+            if (currentColumnType == "datetime2" || currentColumnType == "datetime")
+            {
+                var x = DateTime.Parse(value);
+                var format = "yyyy-MM-dd HH:mm:ss:fff";
+                matchValue = $"'{x.ToString(format)}'";
+            }
+            return matchValue;
+        }
+    }
+}
