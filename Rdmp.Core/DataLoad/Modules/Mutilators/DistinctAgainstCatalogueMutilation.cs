7234cce6a81af0dc716e5b49330a60309a8f11e3
James Friel <JFriel001@dundee.ac.uk>
2024-09-17 10:57:44 +0100

    update mutilator

--- Rdmp.Core/DataLoad/Modules/Mutilators/DistinctAgainstCatalogueMutilation.cs
+++ Rdmp.Core/DataLoad/Modules/Mutilators/DistinctAgainstCatalogueMutilation.cs
@@ -1,46 +1,36 @@
-﻿using FAnsi.Discovery;
-using FAnsi.Discovery.QuerySyntax;
+﻿// Copyright (c) The University of Dundee 2024-2024
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
+using FAnsi.Discovery;
 using Rdmp.Core.Curation.Data.DataLoad;
 using Rdmp.Core.DataLoad.Engine.Job;
 using Rdmp.Core.DataLoad.Engine.Mutilators;
-using Rdmp.Core.QueryBuilding;
 using Rdmp.Core.ReusableLibraryCode.Checks;
 using Rdmp.Core.ReusableLibraryCode.Progress;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rdmp.Core.DataLoad.Modules.Mutilators;
 
-//<!-- new stuff-->
-//select* from rdmp248_new as t1
-//where not exists(select*
-//from rdmp248 as t2
-//where t1.chi = t2.chi)
 
-//<!-- updates
-//select* from rdmp248_new as t1
-//inner join rdmp248 as t2
-//on t2.chi = t1.chi
-//where t1.count != t2.count or t1.hic_drugID != t2.hic_drugID #do all of the columns
-
+/// <summary>
+/// Deletes entries that exist in the catalogue already that are not being updated
+/// Used to save moving large amounts of data between Raw/Staging/Live when we can figure out which entries already exist
+/// </summary>
 public class DistinctAgainstCatalogueMutilation : IMutilateDataTables
 {
     private DiscoveredDatabase _db;
-    private LoadStage _stage;
 
     public void Check(ICheckNotifier notifier)
     {
-        //throw new NotImplementedException();
     }
 
     public void Initialize(DiscoveredDatabase dbInfo, LoadStage loadStage)
     {
-        //throw new NotImplementedException();
         _db = dbInfo;
-        _stage = loadStage;
     }
 
     public void LoadCompletedSoDispose(ExitCodeType exitCode, IDataLoadEventListener postLoadEventsListener)
@@ -54,10 +44,10 @@ public class DistinctAgainstCatalogueMutilation : IMutilateDataTables
         var catalogues = job.LoadMetadata.GetAllCatalogues();
         var tableInfos = catalogues.SelectMany(c => c.CatalogueItems).Select(c => c.ColumnInfo.TableInfo).ToList();
         var tables = _db.DiscoverTables(false);
-        foreach ( var table in tables)
+        foreach (var table in tables)
         {
             var matchingTables = tableInfos.Where(t => t.GetRuntimeName() == table.GetRuntimeName()).Distinct();
-            if(matchingTables.Count() > 1)
+            if (matchingTables.Count() > 1)
             {
                 throw new Exception("too Many tables");
             }
@@ -70,21 +60,24 @@ public class DistinctAgainstCatalogueMutilation : IMutilateDataTables
             var tableColumnNames = table.DiscoverColumns().Select(c => c.GetRuntimeName());
 
             var pks = matchingTable.ColumnInfos.Where(ci => ci.IsPrimaryKey);
-            var nonPks = matchingTable.ColumnInfos.Where(ci => !ci.IsPrimaryKey).Select(c => c.GetRuntimeName()).Where(c=> tableColumnNames.Contains(c));
+            var nonPks = matchingTable.ColumnInfos.Where(ci => !ci.IsPrimaryKey).Select(c => c.GetRuntimeName()).Where(c => tableColumnNames.Contains(c));
             var pkMatchClause = String.Join(" AND ", pks.Select(pk => $"t1.{pk.GetRuntimeName()} = t2.{pk.GetRuntimeName()}"));
-            var nonPksNoMatch = String.Join(" AND ", nonPks.Select(pk => $"t1.{pk} != t2.{pk}"));
+            var nonPksNoMatch = String.Join(" OR ", nonPks.Select(pk => $"t1.{pk} != t2.{pk}"));
+            var subqueryPksNull = String.Join(" AND ", pks.Select(pk => $"t1.{pk.GetRuntimeName()} is null"));
             var newAndUpdateSql = $@"
-            DELETE from {table.GetFullyQualifiedName()}
-			where NOT exists(
-            SELECT * FROM {table.GetFullyQualifiedName()} as t1
-            WHERE NOT EXISTS( SELECT *
-            FROM {matchingTable.GetFullyQualifiedName()} as t2
-            WHERE {pkMatchClause} )
-            UNION
-            SELECT t1.* FROM {table.GetFullyQualifiedName()} as t1
-            INNER JOIN {matchingTable.GetFullyQualifiedName()} as t2
-            on {pkMatchClause}
-            where {nonPksNoMatch})
+                DELETE t2 from {table.GetFullyQualifiedName()} t2
+                LEFT JOIN(
+                     SELECT * FROM {table.GetFullyQualifiedName()} as t1
+                     WHERE NOT EXISTS( SELECT *
+                     FROM {matchingTable.GetFullyQualifiedName()} as t2
+                     WHERE {pkMatchClause} )
+                     UNION
+                     SELECT t1.* FROM {table.GetFullyQualifiedName()} as t1
+                     INNER JOIN {matchingTable.GetFullyQualifiedName()} as t2
+                     on {pkMatchClause}
+                     where {nonPksNoMatch}
+                ) t1 on {pkMatchClause}
+                where {subqueryPksNull}
             ";
             var conn = _db.Server.GetConnection();
             conn.Open();
@@ -92,7 +85,6 @@ public class DistinctAgainstCatalogueMutilation : IMutilateDataTables
             cmd.ExecuteNonQuery();
             conn.Close();
         }
-
-        throw new NotImplementedException();
+        return ExitCodeType.Success;
     }
 }
