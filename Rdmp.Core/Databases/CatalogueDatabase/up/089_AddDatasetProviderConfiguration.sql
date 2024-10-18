f962319201ea1dc1bf03e6a5efc6ea2313221d0f
James Friel <JFriel001@dundee.ac.uk>
2024-10-18 08:33:22 +0100

    add update sql

---
+++ Rdmp.Core/Databases/CatalogueDatabase/up/089_AddDatasetProviderConfiguration.sql
@@ -0,0 +1,17 @@
+﻿if not exists (select 1 from sys.tables where name = 'DatasetProviderConfiguration')
+BEGIN
+CREATE TABLE [dbo].[DatasetProviderConfiguration](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	Type [varchar(256)] NOT NULL,
+	Url [varchar(500)] NOT NULL,
+	Name [varchar(256)] NOT NULL,
+	DataAccessCredentials_ID [int] NOT NULL,
+	Organisation_ID [varchar(256)] NOT NULL
+	CONSTRAINT FK_DatasetProviderConfiguration_DataAccessCredentials FOREIGN KEY (DataAccessCredentials_ID) REFERENCES DataAccessCredentials(ID)  ON DELETE CASCADE,
+CONSTRAINT [PK_RegexRedactionKey] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
