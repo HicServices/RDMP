656cb6c8f7d9831e84a5005badfb01cf5ef445c5
James Friel <JFriel001@dundee.ac.uk>
2024-10-28 16:34:18 +0000

    add summary

--- Rdmp.Core/Curation/Data/Overview/OverviewModel.cs
+++ Rdmp.Core/Curation/Data/Overview/OverviewModel.cs
@@ -1,4 +1,10 @@
-﻿using MongoDB.Driver;
+﻿// Copyright (c) The University of Dundee 2024-2024
+// This file is part of the Research Data Management Platform (RDMP).
+// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
+
+using MongoDB.Driver;
 using NPOI.OpenXmlFormats.Dml;
 using NPOI.SS.Formula.Functions;
 using Rdmp.Core.CommandExecution;
@@ -20,6 +26,9 @@ using System.Threading.Tasks;
 
 namespace Rdmp.Core.Curation.Data.Overview;
 
+/// <summary>
+/// Used to populate information about a catalogue for use in the overview UI
+/// </summary>
 public class OverviewModel
 {
 
