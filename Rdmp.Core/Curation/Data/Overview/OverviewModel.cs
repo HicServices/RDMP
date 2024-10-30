c8f89d02b3add38e9e2cfe5df1b291c37d556c34
James Friel <JFriel001@dundee.ac.uk>
2024-10-30 10:37:04 +0000

    tidy up

--- Rdmp.Core/Curation/Data/Overview/OverviewModel.cs
+++ Rdmp.Core/Curation/Data/Overview/OverviewModel.cs
@@ -4,10 +4,6 @@
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
-using FAnsi.Discovery;
-using MongoDB.Driver;
-using NPOI.OpenXmlFormats.Dml;
-using NPOI.SS.Formula.Functions;
 using Rdmp.Core.CommandExecution;
 using Rdmp.Core.DataExport.Data;
 using Rdmp.Core.DataLoad.Triggers;
@@ -16,7 +12,6 @@ using Rdmp.Core.Logging;
 using Rdmp.Core.QueryBuilding;
 using Rdmp.Core.Repositories;
 using Rdmp.Core.ReusableLibraryCode.DataAccess;
-using SynthEHR.Datasets;
 using System;
 using System.Collections.Generic;
 using System.Data;
