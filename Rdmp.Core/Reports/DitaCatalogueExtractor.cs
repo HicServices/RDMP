1691d23bd2fe86213816616b0f93641b215d00c9
James A Sutherland <j@sutherland.pw>
2025-02-14 09:47:55 -0600

    Redundant string interpolations

--- Rdmp.Core/Reports/DitaCatalogueExtractor.cs
+++ Rdmp.Core/Reports/DitaCatalogueExtractor.cs
@@ -53,20 +53,20 @@ public class DitaCatalogueExtractor : ICheckable
     public void Extract(IDataLoadEventListener listener)
     {
         var xml = new StringBuilder();
-        xml.Append($"""
-                    <?xml version="1.0" encoding="UTF-8"?>
-                    <!DOCTYPE map PUBLIC "-//OASIS//DTD DITA Map//EN"
-                    "map.dtd">
-                    <map>
-                    <title>HIC Data Catalogue</title>
-                    <topicmeta product="hicdc" rev="1">
-                    <author>Wilfred Bonney; Thomas Nind; Mikhail Ghattas</author>
-                    <publisher>Health Informatics Centre (HIC), University of Dundee</publisher
-                    </topicmeta>
-                    <topicref href="introduction.dita"/>
-                    <topicref href="dataset.dita">
+        xml.Append("""
+                   <?xml version="1.0" encoding="UTF-8"?>
+                   <!DOCTYPE map PUBLIC "-//OASIS//DTD DITA Map//EN"
+                   "map.dtd">
+                   <map>
+                   <title>HIC Data Catalogue</title>
+                   <topicmeta product="hicdc" rev="1">
+                   <author>Wilfred Bonney; Thomas Nind; Mikhail Ghattas</author>
+                   <publisher>Health Informatics Centre (HIC), University of Dundee</publisher
+                   </topicmeta>
+                   <topicref href="introduction.dita"/>
+                   <topicref href="dataset.dita">
 
-                    """);
+                   """);
         GenerateIntroductionFile("introduction.dita");
         GenerateDataSetFile("dataset.dita");
 
