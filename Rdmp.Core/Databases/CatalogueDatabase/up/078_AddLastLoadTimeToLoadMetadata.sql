a5fdffd379f81349dee7fee81f8912c556aeea1a
James Friel <JFriel001@dundee.ac.uk>
2025-03-24 08:34:28 +0000

    fix patch

--- Rdmp.Core/Databases/CatalogueDatabase/up/078_AddLastLoadTimeToLoadMetadata.sql
+++ Rdmp.Core/Databases/CatalogueDatabase/up/078_AddLastLoadTimeToLoadMetadata.sql
@@ -1,7 +1,7 @@
 ﻿--Version:8.1.4
 --Description: Adds LastLoadTime to LoadMetadata table
  GO
-if not exists (select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='LoadMetadata' and COLUMN_NAME='LastLoadtime')
+if not exists (select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='LoadMetadata' and COLUMN_NAME='LastLoadTime')
 BEGIN       
 ALTER TABLE LoadMetadata ADD LastLoadTime DATETIME NULL;
 END
