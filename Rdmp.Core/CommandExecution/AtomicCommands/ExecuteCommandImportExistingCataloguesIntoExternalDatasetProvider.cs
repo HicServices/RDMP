1196e88d2ef6a6ca21ba43d7f4db14626d3f2226
James Friel <JFriel001@dundee.ac.uk>
2025-04-28 09:23:06 +0100

    add missing command

---
+++ Rdmp.Core/CommandExecution/AtomicCommands/ExecuteCommandImportExistingCataloguesIntoExternalDatasetProvider.cs
@@ -0,0 +1,64 @@
+﻿using Rdmp.Core.Curation.Data.Datasets;
+using Rdmp.Core.Curation.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.OpenXmlFormats.Vml.Office;
+
+namespace Rdmp.Core.CommandExecution.AtomicCommands
+{
+    public class ExecuteCommandImportExistingCataloguesIntoExternalDatasetProvider : BasicCommandExecution, IAtomicCommand
+    {
+
+        private readonly IBasicActivateItems _activator;
+        private readonly PluginDatasetProvider _provider;
+        private readonly bool _includeExtractable;
+        private readonly bool _includeInternal;
+        private readonly bool _includeProjectSpecific;
+        private readonly bool _includeDeprecated;
+        private readonly string _id;
+
+        public ExecuteCommandImportExistingCataloguesIntoExternalDatasetProvider(IBasicActivateItems activator, PluginDatasetProvider provider, string id, bool includeExtractable, bool includeInternal, bool includeProjectSpecific, bool includeDeprecated)
+        {
+            _activator = activator;
+            _provider = provider;
+            _id = id;
+            _includeExtractable = includeExtractable;
+            _includeInternal = includeInternal;
+            _includeProjectSpecific = includeProjectSpecific;
+            _includeDeprecated = includeDeprecated;
+        }
+
+
+        public override void Execute()
+        {
+            var catalogues = _activator.RepositoryLocator.CatalogueRepository.GetAllObjects<Catalogue>().ToList();
+            if (!_includeInternal)
+            {
+                catalogues = catalogues.Where(c => !c.IsInternalDataset).ToList();
+            }
+            if (!_includeProjectSpecific)
+            {
+                catalogues = catalogues.Where(c => !c.IsProjectSpecific(_activator.RepositoryLocator.DataExportRepository)).ToList();
+            }
+            if (!_includeDeprecated)
+            {
+                catalogues = catalogues.Where(c => !c.IsDeprecated).ToList();
+            }
+            if (!_includeExtractable)
+            {
+                catalogues = catalogues.Where(c => !c.GetExtractabilityStatus(_activator.RepositoryLocator.DataExportRepository).IsExtractable).ToList();
+            }
+            //todo check this catalogue filtering works
+            foreach (var catalogue in catalogues)
+            {
+                var dataset = _provider.Create(catalogue);
+                    var ds = _provider.AddExistingDatasetWithReturn(null, _id);
+                    var cmd = new ExecuteCommandLinkCatalogueToDataset(_activator, catalogue, ds);
+                    cmd.Execute();
+            }
+        }
+    }
+}
