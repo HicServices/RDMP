e99cc19a3eb9519b03da4e48fff2716d280b62c7
James Friel <JFriel001@dundee.ac.uk>
2025-05-06 10:36:51 +0100

    add view

--- Rdmp.Core/Curation/Data/Datasets/PluginDatasetProvider.cs
+++ Rdmp.Core/Curation/Data/Datasets/PluginDatasetProvider.cs
@@ -40,4 +40,9 @@ public abstract class PluginDatasetProvider : IDatasetProvider
     {
         throw new NotImplementedException();
     }
+
+    public string GetRemoteURL(Dataset dataset)
+    {
+        throw new NotImplementedException();
+    }
 }
