157410f055bcb7f332d9fa999043b9f4ced14ae7
James Friel <JFriel001@dundee.ac.uk>
2025-05-12 10:46:17 +0100

    add missing files

---
+++ Rdmp.UI/SimpleDialogs/SetProjectsForCatalogueUI.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Rdmp.UI.SimpleDialogs
+{
+    partial class SetProjectsForCatalogueUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            label1 = new System.Windows.Forms.Label();
+            button2 = new System.Windows.Forms.Button();
+            label2 = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Enabled = false;
+            button1.Location = new System.Drawing.Point(515, 399);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(148, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Make Project Specific";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new System.Drawing.Point(12, 22);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new System.Drawing.Size(405, 400);
+            checkedListBox1.TabIndex = 1;
+            checkedListBox1.ItemCheck += OnItemCheck;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 4);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(49, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Projects";
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(423, 399);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Validate";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(423, 22);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(0, 15);
+            label2.TabIndex = 4;
+            // 
+            // SetProjectsForCatalogueUI
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(checkedListBox1);
+            Controls.Add(button1);
+            Name = "SetProjectsForCatalogueUI";
+            Text = "Set Project For Catalogue";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
