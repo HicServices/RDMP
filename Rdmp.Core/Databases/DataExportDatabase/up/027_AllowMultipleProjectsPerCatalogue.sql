afabea1993c4a2883028cb40662f681ff70ea5e6
James Friel <JFriel001@dundee.ac.uk>
2025-07-01 16:47:53 +0100

    add db change

---
+++ Rdmp.Core/Databases/DataExportDatabase/up/027_AllowMultipleProjectsPerCatalogue.sql
@@ -0,0 +1,16 @@
+﻿----Version: 9.0.0
+----Description: Allow a catalogue to be linked to multiple projects
+
+if exists(select 1 from sys.indexes where name='PreventDoubleAddingCatalogueIdx' and OBJECT_NAME(object_id) = 'ExtractableDataSet')
+BEGIN
+ DROP INDEX PreventDoubleAddingCatalogueIdx on [dbo].[ExtractableDataSet]
+END
+
+if not exists(select 1 from sys.indexes where name='PreventDoubleAddingCatalogueProjectIdx' and OBJECT_NAME(object_id) = 'ExtractableDataSet')
+BEGIN
+CREATE UNIQUE NONCLUSTERED INDEX [PreventDoubleAddingCatalogueIdx] ON [dbo].[ExtractableDataSet]
+(
+	[Catalogue_ID] ASC,
+	[Project_ID] ASC
+)
+END
