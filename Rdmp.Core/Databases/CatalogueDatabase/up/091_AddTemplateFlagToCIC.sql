20d56804ca5d6a5307c71ce876cb0d46970b4203
James Friel <JFriel001@dundee.ac.uk>
2025-11-10 16:30:41 +0000

    add db patch

---
+++ Rdmp.Core/Databases/CatalogueDatabase/up/091_AddTemplateFlagToCIC.sql
@@ -0,0 +1,11 @@
+﻿--Version: 8.4.3
+--Description: Add new metadata fields for catalogues
+if not exists (select 1 from sys.columns where name = 'IsTemplate' and OBJECT_NAME(object_id) = 'CohortIdentificationConfiguration')
+BEGIN
+ALTER TABLE [dbo].[CohortIdentificationConfiguration]
+ADD
+[IsTemplate] [bit] NOT NULL DEFAULT 0
+END
+GO
+
+
