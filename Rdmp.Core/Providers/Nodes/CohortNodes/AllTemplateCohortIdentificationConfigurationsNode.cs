8c154c70a227cb8acf3f63fac59d4ff0cf63040e
James Friel <JFriel001@dundee.ac.uk>
2025-11-10 10:31:42 +0000

    add node

---
+++ Rdmp.Core/Providers/Nodes/CohortNodes/AllTemplateCohortIdentificationConfigurationsNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.Core.Providers.Nodes.CohortNodes
+{
+    public class AllTemplateCohortIdentificationConfigurationsNode : Node
+    {
+        public AllTemplateCohortIdentificationConfigurationsNode()
+        {
+        }
+        public override string ToString() => "Template Cohort Configurations";
+
+    }
+}
