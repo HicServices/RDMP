f53d182509c82c5ba376d4c057b1f3e6062344a4
James Friel <JFriel001@dundee.ac.uk>
2025-11-27 13:55:42 +0000

    fix renderer

--- Rdmp.UI/Collections/Renderers/StatusRenderer.cs
+++ Rdmp.UI/Collections/Renderers/StatusRenderer.cs
@@ -10,9 +10,6 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Globalization;
 using System.Linq;
-using System.Runtime.InteropServices.Marshalling;
-using System.Windows.Forms;
-using static ScintillaNET.Style;
 
 namespace Rdmp.UI.Collections.Renderers
 {
@@ -136,7 +133,6 @@ namespace Rdmp.UI.Collections.Renderers
             r = ApplyCellPadding(r);
             SizeF size = g.MeasureString(_text, this.Font, r.Width, fmt);
             Rectangle inner = new Rectangle(xOffset + r.X, r.Y+2, (int)Math.Ceiling(size.Width),16);
-            //inner = AlignRectangle(r, inner);
             if (!(base.Aspect is IConvertible convertible))
             {
                 return xOffset;
@@ -145,164 +141,27 @@ namespace Rdmp.UI.Collections.Renderers
             double num = convertible.ToDouble(NumberFormatInfo.InvariantInfo);
             Rectangle rect = Rectangle.Inflate(inner, -1, -1);
 
-            g.FillRectangle(new SolidBrush(backgroundColour), inner);
+            var rounded = GetRoundedRect(inner, inner.Height/2);
+            g.FillPath(new SolidBrush(backgroundColour), rounded);
             g.DrawString(_text, this.Font, new SolidBrush(textColor), inner, fmt);
-            g.DrawRectangle(new Pen(new SolidBrush(backgroundColour)), inner);
+            g.DrawPath(new Pen(new SolidBrush(backgroundColour)), rounded);
             return xOffset + inner.Width;
         }
+        private static GraphicsPath GetRoundedRect(RectangleF rect, float diameter)
+        {
+            GraphicsPath path = new GraphicsPath();
 
-        //public override bool RenderSubItem(DrawListViewSubItemEventArgs e, Graphics g, Rectangle cellBounds, object rowObject)
-        //{
-        //    if (rowObject is Catalogue c)
-        //    {
-        //        bool isInternal = c.IsInternalDataset;
-        //        bool isProjectSpecific = c.IsProjectSpecific(_activator.RepositoryLocator.DataExportRepository);
-        //        bool isDeprecated = c.IsDeprecated;
-        //        DrawBackground(g, cellBounds);
-        //        int xOffset = 0;
-        //        if (isInternal)
-        //        {
-        //            xOffset += RenderStatus("Internal", StatusColours.Internal, StatusColours.InternalCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        if (isProjectSpecific)
-        //        {
-        //            xOffset += RenderStatus("Project Specific", StatusColours.ProjectSpecific, StatusColours.ProjectSpecificCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        if (isDeprecated)
-        //        {
-        //            xOffset += RenderStatus("Deprecated", StatusColours.Deprecated, StatusColours.DeprecatedCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        return true;
-        //    }
-        //    if (rowObject is CatalogueItem ci)
-        //    {
-        //        DrawBackground(g, cellBounds);
-        //        int xOffset = 0;
+            RectangleF arc = new RectangleF(rect.X, rect.Y, diameter, diameter);
+            path.AddArc(arc, 180, 90);
+            arc.X = rect.Right - diameter;
+            path.AddArc(arc, 270, 90);
+            arc.Y = rect.Bottom - diameter;
+            path.AddArc(arc, 0, 90);
+            arc.X = rect.Left;
+            path.AddArc(arc, 90, 90);
+            path.CloseFigure();
 
-        //        if (ci.ExtractionInformation.IsExtractionIdentifier)
-        //        {
-        //            xOffset += RenderStatus("Extraction Identifier", StatusColours.ExtractionIdentifier, StatusColours.ExtractionIdentifierCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        if (ci.ExtractionInformation.IsPrimaryKey)
-        //        {
-        //            xOffset += RenderStatus("Primary Key", StatusColours.PrimaryKey, StatusColours.PrimaryKeyCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        if (ci.ExtractionInformation.HashOnDataRelease)
-        //        {
-        //            xOffset += RenderStatus("Hash on Release", StatusColours.HashOnRelease, StatusColours.HashOnReleaseCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        if(ci.ExtractionInformation.ExtractionCategory == ExtractionCategory.Supplemental)
-        //        {
-        //            xOffset += RenderStatus("Supplemental", StatusColours.Supplemental, StatusColours.SupplementalCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        if (ci.ExtractionInformation.ExtractionCategory == ExtractionCategory.SpecialApprovalRequired)
-        //        {
-        //            xOffset += RenderStatus("Special Approval", StatusColours.SpecialistApproval, StatusColours.SpecialistApprovalCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        if (ci.ExtractionInformation.ExtractionCategory == ExtractionCategory.Internal)
-        //        {
-        //            xOffset += RenderStatus("Internal", StatusColours.Internal, StatusColours.InternalCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        if (ci.ExtractionInformation.ExtractionCategory == ExtractionCategory.Deprecated)
-        //        {
-        //            xOffset += RenderStatus("Deprecated", StatusColours.Deprecated, StatusColours.DeprecatedCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        //if (ci.ExtractionInformation.ExtractionCategory == ExtractionCategory.ProjectSpecific)
-        //        //{
-        //        //    xOffset += RenderStatus("Project Specific", StatusColours.ProjectSpecific, StatusColours.ProjectSpecificCompliment, cellBounds, xOffset, g);
-        //        //    xOffset += 5;
-        //        //}
-        //        return true;
-        //    }
-        //    if (rowObject is CohortIdentificationConfiguration cic)
-        //    {
-        //        DrawBackground(g, cellBounds);
-        //        int xOffset = 0;
-        //        if (cic.IsTemplate)
-        //        {
-        //            xOffset += RenderStatus("Template", StatusColours.Template, StatusColours.TemplateCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        if (cic.Frozen)
-        //        {
-        //            xOffset += RenderStatus("Frozen", StatusColours.Frozen, StatusColours.FrozenCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        if (cic.IsAssociatedToAProject(_activator.CoreChildProvider as DataExportChildProvider))
-        //        {
-        //            xOffset += RenderStatus("Project Specific", StatusColours.ProjectSpecific, StatusColours.ProjectSpecificCompliment, cellBounds, xOffset, g);
-        //            xOffset += 5;
-        //        }
-        //        return true;
-        //    }
-        //    return true;
-        //}
-
-        //private static void DrawBackground(Graphics g, Rectangle r)
-        //{
-        //    //Color backgroundColor = Color.White;
-
-        //    //using (Brush brush = new SolidBrush(backgroundColor))
-        //    //{
-        //    //    g.FillRectangle(brush, r.X - 1, r.Y - 1, r.Width + 2, r.Height + 2);
-        //    //}
-        //}
-
-
-        //private int RenderStatus(string text, Color colour, Color textColour, Rectangle cellBounds, int offset, Graphics g)
-        //{
-        //    string _text = text;
-        //    if (_useShortStrings)
-        //    {
-        //        _text = String.Join("", _text.Split(' ').Select(s => s[0]));
-        //    }
-        //    using Font font = new Font("Roboto", 8);
-        //    RectangleF textBoxRect = cellBounds;
-        //    textBoxRect.X += offset;
-        //    textBoxRect.Width = 50;
-
-        //    SizeF size = g.MeasureString(_text, font, cellBounds.Width, fmt);
-        //    textBoxRect.Height = 16;
-        //    textBoxRect.Width = size.Width + 4;
-        //    textBoxRect.Y += (cellBounds.Height - size.Height) / 2;
-        //    var textArc = GetRoundedRect(textBoxRect, textBoxRect.Height / 2);
-        //    textArc.CloseFigure();
-
-        //    g.DrawPath(new Pen(colour), textArc);
-        //    g.FillPath(new SolidBrush(colour), textArc);
-        //    g.Clip = new Region(cellBounds);
-        //    textBoxRect.Y += 2;
-        //    textBoxRect.Height -= 4;
-        //    g.DrawString(_text, font, new SolidBrush(textColour), textBoxRect, fmt);
-
-        //    return (int)Math.Ceiling(textBoxRect.Width);
-        //}
-
-        //private GraphicsPath GetRoundedRect(RectangleF rect, float diameter)
-        //{
-        //    GraphicsPath path = new GraphicsPath();
-
-        //    RectangleF arc = new RectangleF(rect.X, rect.Y, diameter, diameter);
-        //    path.AddArc(arc, 180, 90);
-        //    arc.X = rect.Right - diameter;
-        //    path.AddArc(arc, 270, 90);
-        //    arc.Y = rect.Bottom - diameter;
-        //    path.AddArc(arc, 0, 90);
-        //    arc.X = rect.Left;
-        //    path.AddArc(arc, 90, 90);
-        //    path.CloseFigure();
-
-        //    return path;
-        //}
+            return path;
+        }
     }
 }
