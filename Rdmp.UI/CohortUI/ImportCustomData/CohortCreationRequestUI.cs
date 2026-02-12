f310479a70291fc872770098e8c6f05a6a19fff8
James Friel <JFriel001@dundee.ac.uk>
2026-02-12 08:11:50 +0000

    add changelog

--- Rdmp.UI/CohortUI/ImportCustomData/CohortCreationRequestUI.cs
+++ Rdmp.UI/CohortUI/ImportCustomData/CohortCreationRequestUI.cs
@@ -60,6 +60,7 @@ public partial class CohortCreationRequestUI : RDMPForm
         lblExternalCohortTable.Text = _target.ToString();
 
         SetProject(project);
+
         pbProject.Image = CatalogueIcons.Project.ImageToBitmap();
         pbCohortSource.Image = CatalogueIcons.ExternalCohortTable.ImageToBitmap();
         taskDescriptionLabel1.SetupFor(new DialogArgs
@@ -121,8 +122,6 @@ public partial class CohortCreationRequestUI : RDMPForm
             return;
         }
 
-
-        //todo if cb is checked, copy over the project specific catalogues
 
         //construct the result
         Result = new CohortCreationRequest(Project,
@@ -318,7 +317,6 @@ public partial class CohortCreationRequestUI : RDMPForm
         //if a project is selected and the project has no project number
         lblErrorNoProjectNumber.Visible = Project is { ProjectNumber: null };
         tbSetProjectNumber.Visible = Project is { ProjectNumber: null };
-        cbCopyProjectSpecificCatalogues.Visible = project != null?project.GetAllProjectCatalogues().Any():false;
     }
 
     private void tbName_TextChanged(object sender, EventArgs e)
