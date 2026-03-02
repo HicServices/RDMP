e9a5832e28860ebb1e608e500784031052fb9663
James Friel <JFriel001@dundee.ac.uk>
2026-03-02 15:11:03 +0000

    add missing files

---
+++ Rdmp.Core/EntityFramework/Helpers/DatabaseObject.cs
@@ -0,0 +1,40 @@
+﻿using Rdmp.Core.MapsDirectlyToDatabaseTable;
+using Rdmp.Core.MapsDirectlyToDatabaseTable.Revertable;
+using Rdmp.Core.ReusableLibraryCode.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.Core.EntityFramework.Helpers
+{
+    public  class DatabaseObject: IMapsDirectlyToDatabaseTable
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [NotMapped]
+        public virtual RDMPDbContext CatalogueDbContext { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(object oldValue, object newValue,
+[CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedExtendedEventArgs(propertyName, oldValue, newValue));
+        }
+
+
+        protected void SetField<T>(ref T field, T value)
+        {
+            OnPropertyChanged(field, value, null);
+            field = value;
+        }
+    }
+}
---
+++ Rdmp.Core/EntityFramework/Models/Dataset.cs
@@ -0,0 +1,24 @@
+﻿using Rdmp.Core.Curation.Data;
+using Rdmp.Core.EntityFramework.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdmp.Core.EntityFramework.Models
+{
+    public class Dataset : DatabaseObject, IHasFolder
+    {
+        public string Folder { get; set => SetField(ref field, value); }
+        public string Name { get; set => SetField(ref field, value); }
+
+        public string DigitalObjectIdentifier { get; set => SetField(ref field, value); }
+
+        public string Source { get; set => SetField(ref field, value); }
+
+        public override string ToString() => Name;
+
+    }
+}
