d04d13f93fbd2843a8d1065f8db96d5abe684bff
James Friel <jfriel001@dundee.ac.uk>
2026-04-16 10:29:11 +0100

    Enable writing PKs per extraction batch (#2329)
    * enable pk ordering
    
    * add to mssql
    
    * add space
    
    * fix test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [9.2.2] - Unreleased
+- Add ability to force primary keys at each batch of data when extracting to a database
+
 ## [9.2.1] - 2026-04-07
 - Update Sql Merge component to force destination column types
 - Update Sql merge to correctly match updates
--- Rdmp.Core.Tests/DataLoad/Engine/Integration/DataTableUploadDestinationTests.cs
+++ Rdmp.Core.Tests/DataLoad/Engine/Integration/DataTableUploadDestinationTests.cs
@@ -1310,7 +1310,6 @@ ALTER TABLE DroppedColumnsTable add color varchar(1)
         var destination = new DataTableUploadDestination();
         destination.PreInitialize(null,db, toConsole);
         destination.AllowResizingColumnsAtUploadTime = true;
-
         var dt1 = new DataTable();
         dt1.Columns.Add("TestedCol", typeof(string));
         dt1.Rows.Add(new[] { v1 });
@@ -1328,6 +1327,7 @@ ALTER TABLE DroppedColumnsTable add color varchar(1)
 
         var tt = db.Server.GetQuerySyntaxHelper().TypeTranslater;
         var tbl = db.ExpectTable("DataTableUploadDestinationTests");
+
 
         try
         {
