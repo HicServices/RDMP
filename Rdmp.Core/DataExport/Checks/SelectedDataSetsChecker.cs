3c171d01a16c52ccadb5c3df4973f18fb9a01982
James Friel <JFriel001@dundee.ac.uk>
2026-06-09 10:46:08 +0100

    update changelog

--- Rdmp.Core/DataExport/Checks/SelectedDataSetsChecker.cs
+++ Rdmp.Core/DataExport/Checks/SelectedDataSetsChecker.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Rdmp.Core.CommandExecution;
 using Rdmp.Core.Curation;
 using Rdmp.Core.Curation.Data;
