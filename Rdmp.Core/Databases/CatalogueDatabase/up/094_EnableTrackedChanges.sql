0090e01d03724465022eee5869d5909bf350c3d3
James Friel <JFriel001@dundee.ac.uk>
2026-08-17 11:40:11 +0100

    revert

--- Rdmp.Core/Databases/CatalogueDatabase/up/094_EnableTrackedChanges.sql
+++
@@ -1,6 +0,0 @@
-﻿--Version: 9.3.0
---Description: Add new metadata fields for catalogues
-IF NOT EXISTS (SELECT 1 FROM sys.change_tracking_tables WHERE object_id = OBJECT_ID('Dataset')) 
-	ALTER TABLE Dataset								   ENABLE CHANGE_TRACKING
-IF NOT EXISTS (SELECT 1 FROM sys.change_tracking_tables WHERE object_id = OBJECT_ID('Favourite')) 
-	ALTER TABLE Favourite								   ENABLE CHANGE_TRACKING
